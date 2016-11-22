2beb80162a9e5373394c06ab6b9e4f7917d00179
jgaffuri <julien.gaffuri@ec.europa.eu>
2016-11-22 15:35:31 +0100

    unzip

--- src/main/java/org/opencarto/io/CompressUtil.java
+++ src/main/java/org/opencarto/io/CompressUtil.java
@@ -3,9 +3,13 @@
  */
 package org.opencarto.io;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.zip.GZIPInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
  * @author julien Gaffuri
@@ -27,4 +31,32 @@ public class CompressUtil {
 			e.printStackTrace();
 		}
 	}
+
+
+	public static void unZip(String zipFile, String outputFolder){
+		try{
+			//output directory
+			File folder = new File(outputFolder);
+			if(!folder.exists()) folder.mkdir();
+
+			ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
+			ZipEntry ze = zis.getNextEntry();
+			byte[] buffer = new byte[1024];
+			while(ze!=null){
+				String fileName = ze.getName();
+				File newFile = new File(outputFolder + File.separator + fileName);
+				new File(newFile.getParent()).mkdirs();
+				FileOutputStream fos = new FileOutputStream(newFile);
+				int len;
+				while ((len = zis.read(buffer)) > 0) fos.write(buffer, 0, len);
+				fos.close();
+				ze = zis.getNextEntry();
+			}
+			zis.closeEntry();
+			zis.close();
+		}catch(IOException ex){
+			ex.printStackTrace();
+		}
+	}
+
 }
