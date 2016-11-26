8903cd3ce04de581b05b35725ab207a83e503433
julien Gaffuri <julien.gaffuri@gmail.com>
2016-11-26 09:29:20 +0100

    styles features

--- src/main/java/org/opencarto/datamodel/Feature.java
+++ src/main/java/org/opencarto/datamodel/Feature.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.opencarto.style.Style;
+
 import com.vividsolutions.jts.geom.Geometry;
 
 /**
@@ -22,12 +24,20 @@ public class Feature{
 	//geometries
 	public static String GEOM = "the_geom";
 	private Map<String, Geometry> geoms = new HashMap<String, Geometry>();
-
 	public Geometry getGeom(){ return geoms.get(GEOM); }
 	public void setGeom(Geometry geom){ geoms.put(GEOM,geom); }
-	public Geometry getGeom(int z){ return (Geometry) geoms.get(GEOM+z); }
+	public Geometry getGeom(int z){ return geoms.get(GEOM+z); }
 	public Geometry getGeom(String z) {if(z==null || z=="") return getGeom(); else return getGeom(Integer.parseInt(z));}
 	public void setGeom(Geometry geom, int z){ geoms.put(GEOM+z,geom); }
+
+	//styles
+	public static String STYLE = "style";
+	private Map<String, Style> styles = new HashMap<String, Style>();
+	public Style getStyle(){ return styles.get(STYLE); }
+	public void setStyle(Style st){ styles.put(STYLE,st); }
+	public Style getStyle(int z){ return styles.get(STYLE+z); }
+	public Style getStyle(String z) {if(z==null || z=="") return getStyle(); else return getStyle(Integer.parseInt(z));}
+	public void setStyle(Style st, int z){ styles.put(STYLE+z,st); }
 
 	//properties
 	public Map<String, Object> props = new HashMap<String, Object>();
