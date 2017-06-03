5fa542fc500dd5e308f71aa4d006196c8cd32f74
julien Gaffuri <julien.gaffuri@gmail.com>
2017-06-03 23:59:51 +0200

    a

--- src/main/java/org/opencarto/algo/resolutionise/Resolutionise.java
+++ src/main/java/org/opencarto/algo/resolutionise/Resolutionise.java
@@ -5,6 +5,7 @@ package org.opencarto.algo.resolutionise;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashSet;
 
@@ -12,11 +13,13 @@ import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.LinearRing;
 import com.vividsolutions.jts.geom.MultiLineString;
 import com.vividsolutions.jts.geom.MultiPoint;
 import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.operation.linemerge.LineMerger;
 
 /**
  * @author julien Gaffuri
@@ -28,22 +31,36 @@ public class Resolutionise {
 	public Geometry aeral = null;
 
 	public Resolutionise(Geometry g, double resolution){
+		GeometryFactory gf = g.getFactory();
+
 		if(g instanceof Point){
-			punctual = new GeometryFactory().createPoint(get(g.getCoordinate(), resolution));
-		} else if(g instanceof MultiPoint){
-			Coordinate[] cs = removeDuplicate( get(g.getCoordinates(), resolution) );
-			punctual = new GeometryFactory().createMultiPoint(cs);
-		} else if(g instanceof LineString){
-			System.out.println("Resolutionise non implemented yet for LineString");
-			//think about effect on linearring
-		} else if(g instanceof MultiLineString){
+			punctual = gf.createPoint(get(g.getCoordinate(), resolution));
+		} else if(g instanceof MultiPoint) {
+			punctual = gf.createMultiPoint( removeDuplicates( get(g.getCoordinates(), resolution) ));
+		} else if(g instanceof LineString) {
+			Coordinate[] cs = removeConsecutiveDuplicates(get(g.getCoordinates(), resolution));
+			if(cs.length == 1)
+				punctual = gf.createPoint(cs[0]);
+			else {
+				//TODO use linemerger adding all segments one by one
+				if((samePosition(cs[0], cs[cs.length-1]) || g instanceof LinearRing) && cs.length>=4)
+					linear = gf.createLinearRing(cs);
+				else
+					linear = gf.createLineString(cs);
+/*
+				LineMerger merger = new LineMerger();
+				merger.add(linear);
+				linear = gf.buildGeometry( merger.getMergedLineStrings() );*/
+			}
+		} else if(g instanceof MultiLineString) {
 			System.out.println("Resolutionise non implemented yet for MultiLineString");
-		} else if(g instanceof Polygon){
+		} else if(g instanceof Polygon) {
 			System.out.println("Resolutionise non implemented yet for Polygon");
-		} else if(g instanceof MultiPolygon){
+		} else if(g instanceof MultiPolygon) {
 			System.out.println("Resolutionise non implemented yet for MultiPolygon");
+		} else {
+			System.out.println("Resolutionise non implemented yet for geometry type: "+g.getGeometryType());
 		}
-		System.out.println("Resolutionise non implemented yet for geometry type: "+g.getGeometryType());
 	}
 
 	//return result as a geometry collection
@@ -73,7 +90,7 @@ public class Resolutionise {
 	}
 	public static void apply(Coordinate[] cs, double resolution){ for(Coordinate c : cs) apply(c, resolution); }
 
-	public static Coordinate[] removeDuplicate(Coordinate[] cs){
+	public static Coordinate[] removeDuplicates(Coordinate[] cs){
 		ArrayList<Coordinate> csSorted = new ArrayList<Coordinate>(Arrays.asList(cs));
 		csSorted.sort(new Comparator<Coordinate>() {
 			public int compare(Coordinate c1, Coordinate c2) { return c1.x>c2.x?1:c1.y>c2.y?1:0; }
@@ -81,6 +98,16 @@ public class Resolutionise {
 		HashSet<Coordinate> cs_ = new HashSet<Coordinate>();
 		Coordinate cPrev = null;
 		for(Coordinate c : csSorted){
+			if(cPrev==null || !samePosition(c,cPrev)) cs_.add(c);
+			cPrev=c;
+		}
+		return cs_.toArray(new Coordinate[cs_.size()]);
+	}
+
+	public static Coordinate[] removeConsecutiveDuplicates(Coordinate[] cs){
+		ArrayList<Coordinate> cs_ = new ArrayList<Coordinate>();
+		Coordinate cPrev = null;
+		for(Coordinate c : cs){
 			if(cPrev==null || !samePosition(c,cPrev)) cs_.add(c);
 			cPrev=c;
 		}
@@ -115,6 +142,20 @@ public class Resolutionise {
 		System.out.println(new Resolutionise(pt,10).punctual);
 		System.out.println(new Resolutionise(pt,100).punctual);
 		System.out.println(new Resolutionise(pt,1000).punctual);*/
+
+		//linestring
+		LineString ls;
+		/*ls = gf.createLineString(new Coordinate[] {new Coordinate(107.4, 502.78), new Coordinate(117.4, 500), new Coordinate(487.4, 1402.78)});
+		System.out.println(ls);
+		System.out.println(new Resolutionise(ls,1).linear);
+		System.out.println(new Resolutionise(ls,10).linear);
+		System.out.println(new Resolutionise(ls,100).linear);
+		System.out.println(new Resolutionise(ls,1000).punctual);*/
+		//TODO test linearring
+		ls = gf.createLineString(new Coordinate[] {new Coordinate(107.4, 502.78), new Coordinate(117.4, 504), new Coordinate(120.4, 490), new Coordinate(107.4, 504)});
+		System.out.println(ls);
+		System.out.println(new Resolutionise(ls,1).linear);
+		System.out.println(new Resolutionise(ls,10).linear);
 
 	}
 
