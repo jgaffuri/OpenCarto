36974ef095cf65d3ba627b241e94eb998ec9489a
julien Gaffuri <julien.gaffuri@gmail.com>
2017-06-09 19:56:55 +0200

    a

--- src/main/java/org/opencarto/MainNUTSResolutionise.java
+++ src/main/java/org/opencarto/MainNUTSResolutionise.java
@@ -3,7 +3,15 @@
  */
 package org.opencarto;
 
+import org.geotools.feature.DefaultFeatureCollection;
+import org.geotools.feature.FeatureIterator;
+import org.opencarto.algo.resolutionise.Resolutionise;
 import org.opencarto.io.ShapeFile;
+import org.opengis.feature.simple.SimpleFeature;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
 
 /**
  * 
@@ -16,13 +24,28 @@ public class MainNUTSResolutionise {
 
 	public static void main(String[] args) {
 		//load nuts regions and boundaries from shapefile
-		ShapeFile rg = new ShapeFile("data/NUTS_2013_01M_SH/NUTS_RG_01M_2013.shp", true);
-		System.out.println(rg.count());
-		System.out.println(rg.getSchema());
+		//ShapeFile rg = new ShapeFile("data/NUTS_2013_01M_SH/NUTS_RG_01M_2013.shp", true);
+		ShapeFile bn = new ShapeFile("data/NUTS_2013_01M_SH/NUTS_BN_01M_2013.shp", true);
+
 
 		//compute generalisation
+		FeatureIterator<SimpleFeature> it = bn.getFeatures();
+		DefaultFeatureCollection fs = new DefaultFeatureCollection("geneBN", bn.getSchema());
+		while(it.hasNext()){
+			SimpleFeature f = it.next();
+			MultiLineString geom = (MultiLineString) f.getDefaultGeometry();
+			System.out.println(geom);
+			if(geom == null) continue;
+			Geometry lineal = (Geometry) new Resolutionise(geom, 0.001).lineal;
+			if(lineal == null ) continue;
+			Geometry geom_ = lineal instanceof MultiLineString? lineal : geom.getFactory().createMultiLineString(new LineString[]{(LineString)lineal});
+			f.setDefaultGeometry(geom_);
+			fs.add(f);
+		}
 
 		//save
+		ShapeFile shpOut = new ShapeFile(bn.getSchema(), "~/Bureau", "out.shp", true, true, true);
+		shpOut.add(fs);
 	}
 
 }
