496726e8c83d545cc762ee88b4df7972fb4e5507
julien Gaffuri <julien.gaffuri@gmail.com>
2017-06-09 19:11:28 +0200

    a

---
+++ src/main/java/org/opencarto/MainNUTSResolutionise.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.opencarto;
+
+/**
+ * 
+ * Test of NUTS generalisation based on resolutionise operation
+ * 
+ * @author julien Gaffuri
+ *
+ */
+public class MainNUTSResolutionise {
+
+	public MainNUTSResolutionise() {
+
+		//load nuts regions and boundaries from shapefile
+
+		//compute generalisation
+
+		//save
+
+	}
+
+}
