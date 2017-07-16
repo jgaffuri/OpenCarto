8b3b6ed6ad6a68b1c645f6574ac75049a78951b7
julien Gaffuri <julien.gaffuri@gmail.com>
2017-07-16 20:56:41 +0200

    a

---
+++ src/main/java/org/opencarto/MainGeneGISCO.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.opencarto;
+
+import org.opencarto.datamodel.graph.Graph;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class MainGeneGISCO {
+
+	public static void main(String[] args) {
+
+		//load statistical units
+
+		//structure dataset into topological map
+		Graph topoMap;
+
+		//simplify edges one by one checking the units are ok
+		//if units are not ok, try another edge OR reduce simplification OR collapse edge if too small
+
+
+
+
+
+	}
+
+}
