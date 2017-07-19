6b4fee1d061e4443c4c91d757bd400810bdad382
julien Gaffuri <julien.gaffuri@gmail.com>
2017-07-19 11:06:02 +0200

    a

---
+++ src/main/java/org/opencarto/transfoengine/TransformationEngine.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.opencarto.transfoengine;
+
+import java.util.ArrayList;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class TransformationEngine {
+
+	public ArrayList<Object> queue = new ArrayList<Object>();
+
+	void run(){
+		for(Object o:queue){
+		}
+	}
+
+
+
+}
