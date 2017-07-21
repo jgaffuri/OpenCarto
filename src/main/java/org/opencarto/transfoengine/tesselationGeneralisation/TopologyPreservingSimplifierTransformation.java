148331375e31a8855d734091e1dd3fa345658b88
julien Gaffuri <julien.gaffuri@gmail.com>
2017-07-21 11:28:56 +0200

    a

---
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/TopologyPreservingSimplifierTransformation.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.opencarto.transfoengine.tesselationGeneralisation;
+
+import org.opencarto.datamodel.graph.Edge;
+import org.opencarto.transfoengine.Agent;
+import org.opencarto.transfoengine.Transformation;
+
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.simplify.TopologyPreservingSimplifier;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class TopologyPreservingSimplifierTransformation extends Transformation {
+
+	private double resolution;
+
+	public TopologyPreservingSimplifierTransformation(double resolution){
+		this.resolution = resolution;
+	}
+
+	@Override
+	public void apply(Agent agent) {
+		Edge e = (Edge) agent.getObject();
+		LineString lsIni = e.getGeometry();
+
+		TopologyPreservingSimplifier tr = new TopologyPreservingSimplifier(lsIni);
+		tr.setDistanceTolerance(resolution);
+		LineString lsFin = (LineString) tr.getResultGeometry();
+
+		e.setGeom(lsFin);
+	}
+
+}
