de0993a0d1d8a186b2070c30ca1248c8dba9e787
julien Gaffuri <julien.gaffuri@gmail.com>
2017-08-10 18:27:44 +0200

    a

---
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/AUnit.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.opencarto.transfoengine.tesselationGeneralisation;
+
+/**
+ * A tesselation unit, which consists of one or several ADomains.
+ * 
+ * @author julien Gaffuri
+ *
+ */
+public class AUnit {
+
+}
