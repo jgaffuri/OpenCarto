bbdc8bafe7b155dd35dc5d3794a036f2835676e0
julien Gaffuri <julien.gaffuri@gmail.com>
2017-08-11 11:34:43 +0200

    a

--- src/main/java/org/opencarto/MainGeneGISCO.java
+++ src/main/java/org/opencarto/MainGeneGISCO.java
@@ -44,7 +44,7 @@ public class MainGeneGISCO {
 
 		//TODO create 'top' units
 		//TODO see transformation.cancel();
-		//TODO make graph elements features? link agents to feature (and not object)? Merge feature and agent?
+		//TODO ... make graph elements features? link agents to feature (and not object)? Merge feature and agent?
 		//TODO delete too short edges with only two vertices. edge collapse. length below threshold
 		//TODO create logging mechanism
 		//TODO enclave deletion
