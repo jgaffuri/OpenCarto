b3a164ea74f1c4bcf821143ac51fddb0795c2927
julien Gaffuri <julien.gaffuri@gmail.com>
2017-08-11 11:32:40 +0200

    a

---
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/CEdgeNoTriangle.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package org.opencarto.transfoengine.tesselationGeneralisation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opencarto.datamodel.graph.Edge;
+import org.opencarto.transfoengine.Agent;
+import org.opencarto.transfoengine.Constraint;
+import org.opencarto.transfoengine.Transformation;
+
+import com.vividsolutions.jts.geom.LineString;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class CEdgeNoTriangle extends Constraint {
+
+	public CEdgeNoTriangle(Agent agent) {
+		super(agent);
+	}
+
+	boolean isTriangle = false;
+
+	@Override
+	public void computeCurrentValue() {
+		LineString g = ((Edge)getAgent().getObject()).getGeometry();
+		if(g.isClosed() && g.getNumPoints()<=4)
+			isTriangle = true;
+		else
+			isTriangle = false;
+	}
+
+	@Override
+	public void computeSatisfaction() {
+		satisfaction = isTriangle? 0 : 10;
+	}
+
+	@Override
+	public boolean isHard() { return true; }
+
+	@Override
+	public List<Transformation> getTransformations() {
+		return new ArrayList<Transformation>();
+	}
+
+}
