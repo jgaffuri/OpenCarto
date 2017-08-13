1e3e25ec6f9b9de68b21078fe23574dcc6a8eb07
julien Gaffuri <julien.gaffuri@gmail.com>
2017-08-13 14:12:56 +0200

    a

--- src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/TEnclaveDomainDeletion.java
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/TEnclaveDomainDeletion.java
@@ -28,18 +28,20 @@ public class TEnclaveDomainDeletion extends Transformation<ADomain> {
 		new TIslandDomainDeletion(agent).apply();
 
 		//get edge
-		//TODO check nb=1
+		if(dom.getEdges().size()!=1) System.err.println("Unexpected number of edges for enclave domain "+dom.getId());
 		Edge e = dom.getEdges().iterator().next();
-		//get othe domain
-		//TODO check nb=1
+		//get other domain
+		if(e.getDomains().size()!=1) System.err.println("Unexpected number of domains for edge "+e.getId()+". It should be one but "+e.getDomains().size()+" were found.");
 		Domain dom_ = e.getDomains().iterator().next();
 
-		dom_.getEdges().remove(e);
+		//remove edge
+		b = dom_.getEdges().remove(e);
+		if(!b) System.err.println("Error when removing edge "+e.getId()+" from domain "+dom_.getId()+". Not in domain edges list.");
 		e.d1=null; e.d2=null;
 		g.remove(e);
-		g.remove(dom.getNodes().iterator().next());
 
-		//TODO finish that - remove hole in surrounding domain.
+		//remove node
+		g.remove(dom.getNodes().iterator().next());
 	}
 
 	@Override
