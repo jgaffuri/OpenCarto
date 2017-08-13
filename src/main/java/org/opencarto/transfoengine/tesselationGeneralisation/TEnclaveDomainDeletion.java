457164b5aaaf2f8ef3e41757a40fb2ec0546308e
julien Gaffuri <julien.gaffuri@gmail.com>
2017-08-13 13:55:58 +0200

    a

--- src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/TEnclaveDomainDeletion.java
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/TEnclaveDomainDeletion.java
@@ -3,6 +3,9 @@
  */
 package org.opencarto.transfoengine.tesselationGeneralisation;
 
+import org.opencarto.datamodel.graph.Domain;
+import org.opencarto.datamodel.graph.Edge;
+import org.opencarto.datamodel.graph.Graph;
 import org.opencarto.transfoengine.Transformation;
 
 /**
@@ -18,11 +21,25 @@ public class TEnclaveDomainDeletion extends Transformation<ADomain> {
 	public void apply() {
 		boolean b;
 
+		Domain dom = agent.getObject();
+		Graph g = dom.getGraph();
+
+		//delete domain, making a hole
 		new TIslandDomainDeletion(agent).apply();
 
+		//get edge
+		//TODO check nb=1
+		Edge e = dom.getEdges().iterator().next();
+		//get othe domain
+		//TODO check nb=1
+		Domain dom_ = e.getDomains().iterator().next();
+
+		dom_.getEdges().remove(e);
+		e.d1=null; e.d2=null;
+		g.remove(e);
+		g.remove(dom.getNodes().iterator().next());
+
 		//TODO finish that - remove hole in surrounding domain.
-		//Domain dom = agent.getObject();
-		//Graph g = dom.getGraph();
 	}
 
 	@Override
