f7c8816e1877b9d8b0f2f59e0bc7d25e86439d7f
julien Gaffuri <julien.gaffuri@gmail.com>
2017-08-15 12:36:54 +0200

    a

--- src/main/java/org/opencarto/MainGeneGISCO.java
+++ src/main/java/org/opencarto/MainGeneGISCO.java
@@ -25,8 +25,8 @@ public class MainGeneGISCO {
 	public static void main(String[] args) {
 		System.out.println("Start");
 
-		//TODO test all scales
 		//TODO gene evaluation - pb detection
+		//TODO test all scales
 		//TODO focus on activation strategy
 		//TODO create logging mechanism
 		//TODO data enrichment step: narrow straights/corridors detection. Archipelagos detection
