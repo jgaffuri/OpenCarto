8f8603a008fb4d9a99daa2d0536e48e2366e7a85
julien Gaffuri <julien.gaffuri@gmail.com>
2017-08-15 11:45:11 +0200

    a

--- src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/CFaceSize.java
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/CFaceSize.java
@@ -6,6 +6,7 @@ package org.opencarto.transfoengine.tesselationGeneralisation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.opencarto.datamodel.graph.Edge;
 import org.opencarto.datamodel.graph.Face;
 import org.opencarto.transfoengine.Agent;
 import org.opencarto.transfoengine.Constraint;
@@ -61,37 +62,54 @@ public class CFaceSize extends Constraint {
 	@Override
 	public List<Transformation<?>> getTransformations() {
 		ArrayList<Transformation<?>> out = new ArrayList<Transformation<?>>();
-		
+
 		//deletion case
 		if(goalValue == 0){
 			AFace aFace = (AFace)getAgent();
 			Face f = aFace.getObject();
 
+
 			//islands case
-			if(f.isIsland())
-				if( aFace.isTheLastUnitPatchToRemove() ){}
-				else{
+			if(f.isIsland()){
+				if( ! aFace.isTheLastUnitPatchToRemove() ) {
 					//propose deletion
 					out.add(new TIslandFaceDeletion(aFace));
 					//TODO propose also amalgamation for islands sharing a straight
 				}
+			}
 
-			
+
 			//enclave case
-			else if(f.isEnclave())
-				if( aFace.isTheLastUnitPatchToRemove() ){}
-				else{
+			else if(f.isEnclave()){
+				if( ! aFace.isTheLastUnitPatchToRemove() ) {
 					//propose deletion
 					out.add(new TEnclaveFaceDeletion(aFace));
 					//TODO propose also amalgamation for enclaves with narrow corridor
 				}
+			}
 
 
 			//other case
 			else {
-				//TODO
-				//check if good aggregation candidate exists. If yes, aggregate else collapse
+				if( ! aFace.isTheLastUnitPatchToRemove() ) {
+					//check if good aggregation candidate exists. If yes, aggregate else collapse
+
+					//best edge for aggregation is the one having the maximum length and a having another face. Maybe the face area could be also considered?
+					Edge maxEdge=null; double maxLength=-1;
+					for(Edge e:f.getEdges()){
+						if(e.getFaces().size()<2) continue;
+						double len = e.getGeometry().getLength();
+						if(len<maxLength) continue;
+						maxEdge = e; maxLength = len;
+					}
+
+					if(maxEdge==null) System.err.println("Could not find good candidate face for aggregation of face "+f.getId()+". Maybe this face is an island?");
+
+					//TODO propose aggregation
+					//TODO improve candidate selection method and propose also face collapse if several equivalent candidates are found.
+				}
 			}
+
 		}
 		return out;
 	}
