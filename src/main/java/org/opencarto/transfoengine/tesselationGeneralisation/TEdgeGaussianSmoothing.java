9fa9d4da2caedc79863ef679f8d5e034fc8d29b5
julien Gaffuri <julien.gaffuri@gmail.com>
2017-08-18 21:29:12 +0200

    a

---
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/TEdgeGaussianSmoothing.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package org.opencarto.transfoengine.tesselationGeneralisation;
+
+import org.opencarto.algo.line.GaussianSmoothing;
+import org.opencarto.datamodel.graph.Edge;
+import org.opencarto.transfoengine.Transformation;
+
+import com.vividsolutions.jts.geom.LineString;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class TEdgeGaussianSmoothing extends Transformation<AEdge> {
+
+	private double resolution;
+
+	public TEdgeGaussianSmoothing(AEdge agent, double resolution) {
+		super(agent);
+		this.resolution = resolution;
+	}
+
+	@Override
+	public void apply() {
+		Edge e = (Edge) agent.getObject();
+		LineString lsIni = e.getGeometry(), lsFin;
+
+		lsFin = GaussianSmoothing.get(lsIni, 2*resolution, resolution);
+
+		e.setGeom(lsFin);
+	}
+
+
+
+
+
+	@Override
+	public boolean isCancelable() { return true; }
+
+	private LineString geomStore= null;
+
+	@Override
+	public void storeState() {
+		geomStore = ((Edge)agent.getObject()).getGeometry();
+	}
+
+	@Override
+	public void cancel() {
+		((Edge)agent.getObject()).setGeom(geomStore);
+	}
+
+}
