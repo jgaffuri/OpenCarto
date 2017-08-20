9d5618f1d1f65d6eee123fff73ea487e9b572a2d
julien Gaffuri <julien.gaffuri@gmail.com>
2017-08-20 18:43:49 +0200

    a

--- src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/AUnit.java
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/AUnit.java
@@ -6,6 +6,7 @@ package org.opencarto.transfoengine.tesselationGeneralisation;
 import java.util.Collection;
 import java.util.HashSet;
 
+import org.opencarto.algo.base.Union;
 import org.opencarto.datamodel.Feature;
 import org.opencarto.transfoengine.Agent;
 import org.opencarto.util.JTSGeomUtil;
@@ -55,6 +56,15 @@ public class AUnit extends Agent {
 		int n=0;
 		for(AFace aFace : aFaces) if(!aFace.isDeleted()) n++;
 		return n;
+	}
+
+	public void absorbStraits() {
+		if(straits == null || straits.size() == 0) return;
+		Collection<Geometry> geoms = new HashSet<Geometry>();
+		geoms.add(getObject().getGeom());
+		for(Polygon strait : straits) geoms.add(strait);
+		getObject().setGeom(Union.get(geoms));
+		straits = null;
 	}
 
 }
