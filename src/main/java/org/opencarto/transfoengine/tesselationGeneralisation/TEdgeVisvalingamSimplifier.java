ca1f71ca13838ad2db80f1ce965827100594c6ee
julien Gaffuri <julien.gaffuri@gmail.com>
2017-08-21 10:05:47 +0200

    a

--- src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/TEdgeVisvalingamSimplifier.java
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/TEdgeVisvalingamSimplifier.java
@@ -3,6 +3,7 @@
  */
 package org.opencarto.transfoengine.tesselationGeneralisation;
 
+import org.opencarto.algo.line.GaussianSmoothing;
 import org.opencarto.algo.line.VWSimplifier;
 import org.opencarto.datamodel.graph.Edge;
 import org.opencarto.transfoengine.Transformation;
@@ -15,21 +16,24 @@ import com.vividsolutions.jts.geom.LineString;
  */
 public class TEdgeVisvalingamSimplifier extends Transformation<AEdge> {
 
-	private double resolution;
+	private double resolution, gaussianSmoothingParameter=-1;
 
-	public TEdgeVisvalingamSimplifier(AEdge agent, double resolution) {
+	public TEdgeVisvalingamSimplifier(AEdge agent, double resolution) { this(agent, resolution, -1); }
+	public TEdgeVisvalingamSimplifier(AEdge agent, double resolution, double gaussianSmoothingParameter) {
 		super(agent);
 		this.resolution = resolution;
+		this.gaussianSmoothingParameter = gaussianSmoothingParameter;
 	}
 
 	@Override
 	public void apply() {
 		Edge e = (Edge) agent.getObject();
-		LineString lsIni = e.getGeometry(), lsFin;
 
-		lsFin = (LineString) VWSimplifier.simplify(lsIni, resolution);
+		LineString out = (LineString) VWSimplifier.simplify(e.getGeometry(), resolution);
+		if(gaussianSmoothingParameter > 0)
+			out = GaussianSmoothing.get(out, gaussianSmoothingParameter, resolution);
 
-		e.setGeom(lsFin);
+		e.setGeom(out);
 	}
 
 
