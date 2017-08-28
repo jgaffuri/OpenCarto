99f025971bfb71cec4204426c30393b670b30519
julien Gaffuri <julien.gaffuri@gmail.com>
2017-08-28 20:13:17 +0200

    a

--- src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/CFaceSize.java
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/CFaceSize.java
@@ -5,7 +5,6 @@ package org.opencarto.transfoengine.tesselationGeneralisation;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import org.opencarto.datamodel.graph.Edge;
 import org.opencarto.datamodel.graph.Face;
