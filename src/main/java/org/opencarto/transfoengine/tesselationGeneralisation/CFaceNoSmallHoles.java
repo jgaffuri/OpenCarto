7962714cb21db5f5643e054e8074aaeb02a62ef4
julien Gaffuri <julien.gaffuri@gmail.com>
2017-08-29 11:20:34 +0200

    a

--- src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/CFaceNoSmallHoles.java
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/CFaceNoSmallHoles.java
@@ -43,15 +43,7 @@ public class CFaceNoSmallHoles extends Constraint {
 		//get exterior ring area
 		Polygon poly = f.getGeometry();
 		double outArea = 0;
-		//try {
-			outArea = poly.getFactory().createPolygon(poly.getExteriorRing().getCoordinates()).getArea();
-		/*} catch (Exception e1) {
-			e1.printStackTrace();
-			System.out.println(f.getId());
-			System.out.println(f.getType());
-			System.out.println(((AFace)getAgent()).aUnit.getId());
-			System.out.println(poly);
-		}*/
+		outArea = poly.getFactory().createPolygon(poly.getExteriorRing().getCoordinates()).getArea();
 
 		//find edges corresponding to holes
 		//holes are closed and coastal edges which are not the outer ring
