72f2cd6302750d6959707a6e8bd8d2d74f77e2e2
julien Gaffuri <julien.gaffuri@gmail.com>
2017-08-30 11:40:49 +0200

    a

--- src/main/java/org/opencarto/MainGeneGISCO.java
+++ src/main/java/org/opencarto/MainGeneGISCO.java
@@ -29,7 +29,6 @@ public class MainGeneGISCO {
 	public static void main(String[] args) {
 		System.out.println("Start");
 
-		//TODO check b? in edge stuff + scaling
 		//TODO change logging message style
 		//TODO bug with face aggregation in 1M->60M.
 		//TODO fix CEdgeMinimumSize and edge collapse: move nodes, check polygon validity and if all valids, collapse it.
