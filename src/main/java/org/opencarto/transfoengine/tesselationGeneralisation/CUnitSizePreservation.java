2b8e7fa55603f681f92142dea963d117f5f59824
julien Gaffuri <julien.gaffuri@gmail.com>
2017-09-03 20:59:36 +0200

    a

---
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/CUnitSizePreservation.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package org.opencarto.transfoengine.tesselationGeneralisation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opencarto.datamodel.graph.Face;
+import org.opencarto.transfoengine.Agent;
+import org.opencarto.transfoengine.Constraint;
+import org.opencarto.transfoengine.Transformation;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class CUnitSizePreservation extends Constraint {
+
+	public CUnitSizePreservation(Agent agent) {
+		super(agent);
+	}
+
+
+
+	double initialValue, currentValue, goalValue;
+
+	@Override
+	public void computeInitialValue() {
+		computeCurrentValue();
+		initialValue = currentValue;
+	}
+
+	@Override
+	public void computeCurrentValue() {
+		Face d = (Face)(getAgent().getObject());
+		currentValue = d.getGeometry()==null? 0 : d.getGeometry().getArea();
+	}
+
+	@Override
+	public void computeGoalValue() { goalValue = initialValue; }
+
+
+
+
+	@Override
+	public void computeSatisfaction() {
+		if(getAgent().isDeleted())
+			if(goalValue == 0) satisfaction=10; else satisfaction=0;
+		else
+			if(goalValue == 0) satisfaction=0;
+			else satisfaction = 10 - 10*Math.abs(goalValue-currentValue)/goalValue;
+	}
+
+	@Override
+	public List<Transformation<?>> getTransformations() {
+		return new ArrayList<Transformation<?>>();
+	}
+
+}
