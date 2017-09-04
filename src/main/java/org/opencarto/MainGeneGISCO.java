26eea76259d472a218ff429d94e6aeea82da2046
julien Gaffuri <julien.gaffuri@gmail.com>
2017-09-04 14:11:42 +0200

    a

--- src/main/java/org/opencarto/MainGeneGISCO.java
+++ src/main/java/org/opencarto/MainGeneGISCO.java
@@ -96,22 +96,22 @@ public class MainGeneGISCO {
 		}*/
 
 		//evaluation
-		//1spatial
-		for(int targetScaleM : new int[]{3,20,60}) {
-			System.out.println("--- Evaluation: NUTS/1spatial generalisation "+targetScaleM+"M");
-			runNUTSGeneralisationEvaluation(basePath+"1spatial/1Generalise_Result"+targetScaleM+"M.shp", 3857, targetScaleM*resolution1M, basePath+"evaluation/1spatial/"+targetScaleM+"M/");
-		}
-		/*/GISCOgene
+		//GISCOgene
 		for(String inputScale : new String[]{"1M","100k"})
 			for(int targetScaleM : new int[]{1,3,10,20,60}) {
 				System.out.println("--- Evaluation: NUTS generalisation "+inputScale+"-"+targetScaleM+"M");
 				runNUTSGeneralisationEvaluation(outPath+inputScale+"_input/"+targetScaleM+"M/units.shp", 3035, targetScaleM*resolution1M, basePath+"evaluation/GISCOgene/"+inputScale+"_"+targetScaleM+"M/");
 			}
-		//2010 versions
+		//1spatial
+		for(int targetScaleM : new int[]{3,20,60}) {
+			System.out.println("--- Evaluation: NUTS/1spatial generalisation "+targetScaleM+"M");
+			runNUTSGeneralisationEvaluation(basePath+"1spatial/1Generalise_Result"+targetScaleM+"M.shp", 3857, targetScaleM*resolution1M, basePath+"evaluation/1spatial/"+targetScaleM+"M/");
+		}
+		//2013 versions
 		for(int targetScaleM : new int[]{1,3,10,20,60}) {
 			System.out.println("--- Evaluation: NUTS 2010 generalisation "+targetScaleM+"M");
 			runNUTSGeneralisationEvaluation(basePath+"/nuts_2013/"+targetScaleM+"M/LAEA/lvl3/RG.shp", 3035, targetScaleM*resolution1M, basePath+"evaluation/nuts2010/"+targetScaleM+"M/");
-		}*/
+		}
 		//TODO comm 1M/100k?
 		//TODO nuts 2010 100k too?
 
