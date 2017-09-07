779c9a3baf63bed6a471b6ef21eb8f398a9a060c
julien Gaffuri <julien.gaffuri@gmail.com>
2017-09-07 17:17:51 +0200

    a

--- src/main/java/org/opencarto/MainGeneGISCO.java
+++ src/main/java/org/opencarto/MainGeneGISCO.java
@@ -35,6 +35,8 @@ public class MainGeneGISCO {
 
 		//TODO handle case of face deletion/aggregation when it has holes (!)
 
+		//TODO strong logging!
+
 		//TODO improve CEdgeFacesValid with overlaps operation
 		//TODO finalise edge face validity constraint - other constraints obsolete? improve performence with better face spatial index?
 		//TODO evaluation: check partition is a true partition
