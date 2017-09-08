19a55025f13c6381ca2fb8cba3117379feea7299
julien Gaffuri <julien.gaffuri@gmail.com>
2017-09-08 18:23:49 +0200

    a

---
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/CUnitNoNarrowPartsAndCorridors.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package org.opencarto.transfoengine.tesselationGeneralisation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opencarto.transfoengine.Constraint;
+import org.opencarto.transfoengine.Transformation;
+
+/**
+ * 
+ * Constraint ensuring a unit's size is equal to a goal one, typically the initial value.
+ * 
+ * @author julien Gaffuri
+ *
+ */
+public class CUnitNoNarrowPartsAndCorridors extends Constraint<AUnit> {
+
+	public CUnitNoNarrowPartsAndCorridors(AUnit agent) {
+		super(agent);
+	}
+
+	private int initialNumber, number = 0;
+
+	@Override
+	public void computeInitialValue() {
+		computeCurrentValue();
+		initialNumber = number;
+	}
+
+	@Override
+	public void computeCurrentValue() {
+		number = getAgent().straits.size();
+	}
+
+	@Override
+	public void computeSatisfaction() {
+		if(getAgent().isDeleted()) satisfaction = 10;
+		else if(initialNumber == 0) satisfaction = 10;
+		else satisfaction = 10*number/initialNumber;
+	}
+
+	@Override
+	public List<Transformation<AUnit>> getTransformations() {
+		ArrayList<Transformation<AUnit>> out = new ArrayList<Transformation<AUnit>>();
+
+		out.add(new Transformation<AUnit>((AUnit)getAgent()) {
+
+			@Override
+			public void apply() {
+				//TODO absorb
+			}
+
+			@Override
+			public boolean isCancelable() { return false; }
+			@Override
+			public void storeState() {}
+			@Override
+			public void cancel() { System.err.println("cancel() not implemented for "+this.getClass().getSimpleName()); }
+
+		});
+
+		return out;
+	}
+
+}
