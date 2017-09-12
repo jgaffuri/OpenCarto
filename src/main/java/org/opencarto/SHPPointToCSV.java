5a35eb92814dc327e5f4949cf59af674ba56300d
julien Gaffuri <julien.gaffuri@gmail.com>
2017-09-12 09:11:17 +0200

    a

--- src/main/java/org/opencarto/SHPPointToCSV.java
+++ src/main/java/org/opencarto/SHPPointToCSV.java
@@ -1,5 +1,8 @@
 package org.opencarto;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Map;
@@ -19,28 +22,41 @@ public class SHPPointToCSV {
 
 	}
 
-	private void export(String shpFileFolder, String shpFile, int epsg, String outPath){
+	private void export(String shpFileFolder, String shpFile, int epsg, String outPath) throws Exception{
 		LOGGER.info("Load "+shpFile);
 		ArrayList<Feature> fs = SHPUtil.loadSHP(shpFileFolder+shpFile+".shp",epsg).fs;
 
+		LOGGER.info("Prepare file");
 		new File(outPath).mkdirs();
 		File f = new File(outPath+shpFile+".csv");
 		if(f.exists()) f.delete();
 		f.createNewFile();
-		PrintWriter w = new PrintWriter(f);
+		BufferedWriter bw = new BufferedWriter(new FileWriter(f, true));
 
-		
-		LOGGER.info("Build CSV data");
-		ArrayList<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
-		for(Feature f : fs) {
-			Map<String, Object> p = f.getProperties();
-			p.put("geom", f.getGeom().toText()); f.setGeom(null);
-			data.add(p);
+		LOGGER.info("Write header");
+		ArrayList<String> keys = new ArrayList<String>(fs.get(0).getProperties().keySet());
+		int i=0;
+		for(String key : keys ){
+			bw.write(key);
+			if(i<keys.size()-1) bw.write(",");
+			i++;
+		}
+		bw.write(",geom\n");
+
+		LOGGER.info("Write data");
+		for(Feature f_ : fs) {
+			i=0;
+			for(String key : keys){
+				bw.write(f_.getProperties().get(key).toString());
+				if(i<keys.size()-1) bw.write(",");
+				i++;
+			}
+			bw.write(",");
+			bw.write(f_.getGeom().toText());
+			bw.write("\n");
 		}
 
-		LOGGER.info("Save as CSV");
-		CSVUtil.save(data, outPath, shpFile+".csv");
-
+		bw.close();
 
 	}
 }
