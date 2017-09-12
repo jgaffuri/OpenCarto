11020d1610c6de13f6ec903889fb05216166c1df
julien Gaffuri <julien.gaffuri@gmail.com>
2017-09-12 09:13:49 +0200

    a

--- src/main/java/org/opencarto/SHPPointToCSV.java
+++ src/main/java/org/opencarto/SHPPointToCSV.java
@@ -2,14 +2,10 @@ package org.opencarto;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.opencarto.datamodel.Feature;
-import org.opencarto.io.CSVUtil;
 import org.opencarto.io.SHPUtil;
 
 public class SHPPointToCSV {
@@ -18,7 +14,6 @@ public class SHPPointToCSV {
 	public static void main(String[] args) {
 
 
-		XXXX
 
 	}
 
@@ -28,31 +23,29 @@ public class SHPPointToCSV {
 
 		LOGGER.info("Prepare file");
 		new File(outPath).mkdirs();
-		File f = new File(outPath+shpFile+".csv");
-		if(f.exists()) f.delete();
-		f.createNewFile();
-		BufferedWriter bw = new BufferedWriter(new FileWriter(f, true));
+		File file = new File(outPath+shpFile+".csv");
+		if(file.exists()) file.delete();
+		file.createNewFile();
+		BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
 
 		LOGGER.info("Write header");
 		ArrayList<String> keys = new ArrayList<String>(fs.get(0).getProperties().keySet());
 		int i=0;
 		for(String key : keys ){
-			bw.write(key);
-			if(i<keys.size()-1) bw.write(",");
-			i++;
+			bw.write(key.replaceAll(",", ";"));
+			if(i<keys.size()-1) bw.write(","); i++;
 		}
 		bw.write(",geom\n");
 
 		LOGGER.info("Write data");
-		for(Feature f_ : fs) {
+		for(Feature f : fs) {
 			i=0;
 			for(String key : keys){
-				bw.write(f_.getProperties().get(key).toString());
-				if(i<keys.size()-1) bw.write(",");
-				i++;
+				bw.write(f.getProperties().get(key).toString().replaceAll(",", ";"));
+				if(i<keys.size()-1) bw.write(","); i++;
 			}
 			bw.write(",");
-			bw.write(f_.getGeom().toText());
+			bw.write(f.getGeom().toText());
 			bw.write("\n");
 		}
 
