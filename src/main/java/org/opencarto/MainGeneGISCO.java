50f6d0706572078f90a9d7699997a317e40d4a31
julien Gaffuri <julien.gaffuri@gmail.com>
2017-09-14 18:32:40 +0200

    a

--- src/main/java/org/opencarto/MainGeneGISCO.java
+++ src/main/java/org/opencarto/MainGeneGISCO.java
@@ -33,6 +33,8 @@ public class MainGeneGISCO {
 	public static void main(String[] args) {
 		System.out.println("Start");
 
+
+		//TODO for the enclave/islands, make scale after simplification
 		//TODO better handle holes: define hole removal parameter + same for islands
 
 		//TODO handle narrow gaps and parts unit narrow parts.		
