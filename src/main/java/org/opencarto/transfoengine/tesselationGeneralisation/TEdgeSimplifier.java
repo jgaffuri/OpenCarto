c6b83f0b535537db60ab31bfdb3e49f6913cbfba
julien Gaffuri <julien.gaffuri@gmail.com>
2017-09-15 14:32:23 +0200

    a

--- src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/TEdgeSimplifier.java
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/TEdgeSimplifier.java
@@ -6,7 +6,6 @@ package org.opencarto.transfoengine.tesselationGeneralisation;
 import org.opencarto.datamodel.graph.Edge;
 import org.opencarto.transfoengine.Transformation;
 
-import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.LineString;
 
 /**
@@ -23,21 +22,21 @@ public abstract class TEdgeSimplifier extends Transformation<AEdge> {
 	public boolean isCancelable() { return true; }
 
 	private LineString geomStore= null;
-	private Coordinate closedEdgeNodePosition = null;
-	private double scaleRatio = 1;
+	//private Coordinate closedEdgeNodePosition = null;
+	//private double scaleRatio = 1;
 
 	@Override
 	public void storeState() {
 		Edge e = agent.getObject();
 		geomStore = e.getGeometry();
-		if(e.isClosed()) closedEdgeNodePosition = new Coordinate(e.getN1().getC().x, e.getN1().getC().y);
+		//if(e.isClosed()) closedEdgeNodePosition = new Coordinate(e.getN1().getC().x, e.getN1().getC().y);
 	}
 
 	@Override
 	public void cancel() {
 		Edge e = agent.getObject();
 		e.setGeom(geomStore);
-		if(e.isClosed()) e.getN1().moveTo(closedEdgeNodePosition.x, closedEdgeNodePosition.y);;
+		//if(e.isClosed()) e.getN1().moveTo(closedEdgeNodePosition.x, closedEdgeNodePosition.y);;
 	}
 
 }
