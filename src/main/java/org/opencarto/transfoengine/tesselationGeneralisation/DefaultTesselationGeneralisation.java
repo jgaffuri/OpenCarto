992c0e2b5b21dfd977c07a401624c00d06fa939f
julien Gaffuri <julien.gaffuri@gmail.com>
2017-09-22 13:50:53 +0200

    a

--- src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/DefaultTesselationGeneralisation.java
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/DefaultTesselationGeneralisation.java
@@ -28,7 +28,7 @@ public class DefaultTesselationGeneralisation {
 		double resSqu = resolution*resolution;
 		for(AUnit a : t.aUnits) {
 			//a.addConstraint(new CUnitNoNarrowPartsAndGapsXXX(a).setPriority(10));
-			a.addConstraint(new CUnitNoNarrowGaps(a, resolution, 0.2*resSqu, 4).setPriority(10));
+			a.addConstraint(new CUnitNoNarrowGaps(a, resolution, 0.1*resSqu, 4).setPriority(10));
 		}
 	}
 
