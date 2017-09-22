f8394404671fadffa6b1ef3d8a9df2b1b77e48df
julien Gaffuri <julien.gaffuri@gmail.com>
2017-09-22 09:13:39 +0200

    a

---
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/DefaultStatisticalUnitsGeneralisation.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.opencarto.transfoengine.tesselationGeneralisation;
+
+/**
+ * 
+ * Default procedure for basic generalisation of statistical units tesselations.
+ * 
+ * @author julien Gaffuri
+ *
+ */
+public class DefaultStatisticalUnitsGeneralisation {
+
+}
