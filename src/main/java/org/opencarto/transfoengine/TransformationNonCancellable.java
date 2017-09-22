54125698345e01f3ffa68f9d1f9735c4a49488fb
julien Gaffuri <julien.gaffuri@gmail.com>
2017-09-22 16:01:13 +0200

    a

---
+++ src/main/java/org/opencarto/transfoengine/TransformationNonCancellable.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.opencarto.transfoengine;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public abstract class TransformationNonCancellable<T extends Agent> extends Transformation<T> {
+
+	public TransformationNonCancellable(T agent) { super(agent); }
+	@Override
+	public boolean isCancelable() { return false; }
+
+}
