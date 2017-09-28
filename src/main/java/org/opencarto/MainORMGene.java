0a66c9839f4723f41db712020fdc7b94cd65b496
julien Gaffuri <julien.gaffuri@gmail.com>
2017-09-28 12:09:52 +0200

    a

--- src/main/java/org/opencarto/MainORMGene.java
+++ src/main/java/org/opencarto/MainORMGene.java
@@ -15,11 +15,16 @@ public class MainORMGene {
 	public static void main(String[] args) {
 		System.out.println("Start");
 
-		//target: 1:50k
+		//http://overpass-api.de/api/status
+		//http://overpass-api.de/api/map?bbox=6.2828,49.6598,6.3165,49.6714&data=node[railway];out;
+		//http://overpass-api.de/api/map?bbox=6.2828,49.6598,6.3165,49.6714&data=way[railway];out;
+
+		//target: 1:50k -> Resolution 0.2mm -> 10m
 		//filtering: remove by type, etc.
 		//test connection
-		//detect  by type.
-
+		//distinct mainlines from local. By type? Using stroke?
+		//build areas from locals
+		//symplify main lines: collapse dual mainlines, simplify junctions, etc.
 
 		System.out.println("End");
 	}
