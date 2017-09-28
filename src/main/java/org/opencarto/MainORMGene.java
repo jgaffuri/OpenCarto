5df9c6afd0811fd5e7f6e091782a13a19bfb1867
julien Gaffuri <julien.gaffuri@gmail.com>
2017-09-28 11:44:15 +0200

    a

--- src/main/java/org/opencarto/MainORMGene.java
+++ src/main/java/org/opencarto/MainORMGene.java
@@ -15,6 +15,10 @@ public class MainORMGene {
 	public static void main(String[] args) {
 		System.out.println("Start");
 
+		//target: 1:50k
+		//filtering: remove by type, etc.
+		//test connection
+		//detect  by type.
 
 
 		System.out.println("End");
