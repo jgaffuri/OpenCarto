ab33b6eb828295256454e9d906a5c8b8954d6d86
julien Gaffuri <julien.gaffuri@gmail.com>
2017-09-28 15:02:49 +0200

    a

--- src/main/java/org/opencarto/MainORMGene.java
+++ src/main/java/org/opencarto/MainORMGene.java
@@ -15,6 +15,10 @@ public class MainORMGene {
 	public static void main(String[] args) {
 		System.out.println("Start");
 
+		//See
+		//https://blog-en.openalfa.com/how-to-query-openstreetmap-using-the-overpass-api
+		//http://overpass-api.de/command_line.html
+
 		//http://overpass.osm.rambler.ru
 		//http://www.overpass-api.de
 		//http://api.openstreetmap.fr
@@ -23,12 +27,37 @@ public class MainORMGene {
 		//http://overpass-api.de/api/interpreter?data=node[name=\"Gielgen\"];out;
 		//http://overpass-api.de/api/map?bbox=6.2828,49.6598,6.3165,49.6714&data=node[railway];out;
 		//http://overpass-api.de/api/map?bbox=6.2828,49.6598,6.3165,49.6714&data=way[railway];out;
-		//http://overpass-api.de/api/interpreter?data=node(1422314245);out;
 
-		//BBOX
-		//node(50.745,7.17,50.75,7.18);out;
+		
+		//{{geocodeArea:luxembourg}}->.searchArea;
+		//way["railway"="rail"](area.searchArea);
 
+		/*BBOX
+		node(50.745,7.17,50.75,7.18);out;
+		(
+				node(50.745,7.17,50.75,7.18);
+				way(50.745,7.17,50.75,7.18);
+				relation(50.745,7.17,50.75,7.18);
+				);
+				out;
+		 */
+/*
+(49.6598,6.2828,49.6714,6.3165)
+(49,5,51,8)
+http://overpass-api.de/api/map?data=[out:xml];(node[railway](49,5,51,8);way[railway](49,5,51,8);relation[railway](49,5,51,8););>;out;
+*/
 
+		
+		/*
+ * node
+  ["highway"="bus_stop"]
+  ["shelter"]
+  ["shelter"!="no"]
+  (50.7,7.1,50.8,7.25);
+out;
+ * 
+*/	
+		
 		//target: 1:50k -> Resolution 0.2mm -> 10m
 		//filtering: remove by type, etc.
 		//test connection
