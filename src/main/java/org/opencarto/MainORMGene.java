b9346aeda72105da112b94f64f0cd482ebdbe9a5
julien Gaffuri <julien.gaffuri@gmail.com>
2017-09-28 11:42:20 +0200

    a

---
+++ src/main/java/org/opencarto/MainORMGene.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.opencarto;
+
+/**
+ * 
+ * Some tests on ORM generalisation
+ * 
+ * @author julien Gaffuri
+ *
+ */
+public class MainORMGene {
+
+	public static void main(String[] args) {
+		System.out.println("Start");
+
+
+
+		System.out.println("End");
+	}
+
+}
