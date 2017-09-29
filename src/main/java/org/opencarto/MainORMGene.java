4f3a7ed26c9ee9e749b019e7b15bf434da2ebd0b
julien Gaffuri <julien.gaffuri@gmail.com>
2017-09-29 10:39:31 +0200

    a

--- src/main/java/org/opencarto/MainORMGene.java
+++ src/main/java/org/opencarto/MainORMGene.java
@@ -12,30 +12,35 @@ package org.opencarto;
  */
 public class MainORMGene {
 
+
+	//get ORM data using openpass API:
+
+	/*
+(46,1,52,7)
+wget -O orm.osm "http://overpass-api.de/api/map?data=[out:xml];(node[railway](46,1,52,7);way[railway](46,1,52,7);relation[railway](46,1,52,7););(._;>;);out;"
+ogr2ogr --config OSM_USE_CUSTOM_INDEXING NO -skipfailures -f "ESRI Shapefile" shp orm.osm  -overwrite
+	 */
+
+	//doc:
+	//http://wiki.openstreetmap.org/wiki/OpenRailwayMap  ---  http://wiki.openstreetmap.org/wiki/OpenRailwayMap/Tagging
+	//https://blog-en.openalfa.com/how-to-query-openstreetmap-using-the-overpass-api
+	//https://stackoverflow.com/questions/31879288/overpass-api-way-coordinates
+
+	//http://www.overpass-api.de/api/
+	//http://overpass.osm.rambler.ru/cgi/
+	//http://api.openstreetmap.fr/api/
+
+	//http://www.overpass-api.de/api/status
+
+
 	public static void main(String[] args) {
 		System.out.println("Start");
 
-		//http://wiki.openstreetmap.org/wiki/OpenRailwayMap  ---  http://wiki.openstreetmap.org/wiki/OpenRailwayMap/Tagging
-		//https://blog-en.openalfa.com/how-to-query-openstreetmap-using-the-overpass-api
-
-		//http://www.overpass-api.de/api/
-		//http://overpass.osm.rambler.ru/cgi/
-		//http://api.openstreetmap.fr/api/
-
-		//http://www.overpass-api.de/api/status
-
-		/*
-(49,5,52,8)
-wget -O orm.osm "http://overpass-api.de/api/map?data=[out:xml];(node[railway](49,5,52,8);way[railway](49,5,52,8);relation[railway](49,5,52,8););>;out;"
-ogr2ogr --config OSM_USE_CUSTOM_INDEXING NO -skipfailures -f "ESRI Shapefile" shp orm.osm  -overwrite
-
-wget -O rail.osm "http://www.overpass-api.de/api/map?data=[out:xml];way["railway"="rail"](49,5,52,8);>;out;"
-ogr2ogr --config OSM_USE_CUSTOM_INDEXING NO -skipfailures -f "ESRI Shapefile" rshp rail.osm  -overwrite
-		 */
-
-		//
-
 		//target: 1:50k -> Resolution 0.2mm -> 10m
+
+		//TODO see input data abd filter it
+
+
 		//filtering: remove by type, etc.
 		//test connection
 		//distinct mainlines from local. By type? Using stroke?
