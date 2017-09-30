c50cfbf77d5a970d8dfaf4fc99b160417ff9f89d
julien Gaffuri <julien.gaffuri@gmail.com>
2017-09-30 20:30:23 +0200

    a

--- src/main/java/org/opencarto/MainORMGene.java
+++ src/main/java/org/opencarto/MainORMGene.java
@@ -15,12 +15,16 @@ public class MainORMGene {
 
 	//get ORM data using openpass API:
 
+	//improve request mechanism
 	/*
-(46,1.5,53,8.5)
-wget -O orm.osm "http://overpass-api.de/api/map?data=[out:xml];(node[railway](46,1.5,53,8.5);way[railway](46,1.5,53,8.5);relation[railway](46,1.5,53,8.5););(._;>;);out;"
+(46,1,53,9)
+wget -O orm.osm "http://overpass-api.de/api/map?data=[out:xml];(node[railway](46,1,53,9);way[railway](46,1,53,9);relation[railway](46,1,53,9););(._;>;);out;"
 ogr2ogr --config OSM_USE_CUSTOM_INDEXING NO -skipfailures -f "ESRI Shapefile" shp orm.osm  -overwrite
 	 */
 
+//wget -O orm2.osm "http://overpass-api.de/api/map?data=[out:xml];(node[railway](46,1,53,9);way[railway](46,1,53,9);relation[railway](46,1,53,9););(._;>;);out;"
+
+	
 	//doc:
 	//http://wiki.openstreetmap.org/wiki/OpenRailwayMap  ---  http://wiki.openstreetmap.org/wiki/OpenRailwayMap/Tagging
 	//https://blog-en.openalfa.com/how-to-query-openstreetmap-using-the-overpass-api
@@ -37,9 +41,12 @@ ogr2ogr --config OSM_USE_CUSTOM_INDEXING NO -skipfailures -f "ESRI Shapefile" sh
 		System.out.println("Start");
 
 		//TODO define specs based on ORM model and generalisation process in mind (at least ERM specs should be covered)
-		//main railway lines + railway areas + stations (points and surfaces)
-		//leveling crossing (points)
+		//specs for input dataset (1:5k): tracks selected, with proper attributes, well structured. basic ETL process.
+		//specs for generalised dataset (1:50k)
+		//   main railway lines + railway areas + stations (points and surfaces)
+		//   leveling crossing (points)
 		//target: 1:50k -> Resolution 0.2mm -> 10m
+
 
 		//TODO see input data
 		//define filtering based on attribute
