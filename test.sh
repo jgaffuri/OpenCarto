46a325f545c1be26b8b1a76b1bd9f758c125e789
julien Gaffuri <julien.gaffuri@gmail.com>
2017-10-02 18:30:42 +0200

    a

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 #http://wiki.openstreetmap.org/wiki/OpenRailwayMap  ---  http://wiki.openstreetmap.org/wiki/OpenRailwayMap/Tagging
 #https://blog-en.openalfa.com/how-to-query-openstreetmap-using-the-overpass-api
 #https://stackoverflow.com/questions/31879288/overpass-api-way-coordinates
@@ -9,7 +11,14 @@
 #http://www.overpass-api.de/api/status
 
 
-cd ~/Bureau/gisco_rail/orm
-wget -O orm_main.osm "http://overpass-api.de/api/map?data=[out:xml];(node[railway][usage=main](43,5,44,6);way[railway][usage=main](43,5,44,6);relation[railway][usage=main](43,5,44,6););(._;>;);out;"
-ogr2ogr --config OSM_USE_CUSTOM_INDEXING NO -skipfailures -f "ESRI Shapefile" shp_main orm_main.osm  -overwrite
 
+
+
+cd ~/Bureau/gisco_rail/orm
+
+for usage in "main" "branch" "industrial" "military" "tourism"
+do
+	echo $usage
+	wget -O orm_$usage.osm "http://overpass-api.de/api/map?data=[out:xml];(node[railway][usage=$usage](43,5,44,6);way[railway][usage=$usage](43,5,44,6);relation[railway][usage=$usage](43,5,44,6););(._;>;);out;"
+	ogr2ogr --config OSM_USE_CUSTOM_INDEXING NO -skipfailures -f "ESRI Shapefile" shp_$usage orm_$usage.osm  -overwrite
+done
