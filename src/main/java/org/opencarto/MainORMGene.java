4cb443075bb6bc6805c4f73ea25a463d1b6040a0
julien Gaffuri <julien.gaffuri@gmail.com>
2017-10-02 18:16:22 +0200

    a

--- src/main/java/org/opencarto/MainORMGene.java
+++ src/main/java/org/opencarto/MainORMGene.java
@@ -13,23 +13,9 @@ package org.opencarto;
 public class MainORMGene {
 
 
-	//TODO make import script
 	//TODO do filtering at import level
-	//http://www.overpass-api.de/query_form.html
-
-	//improve request mechanism
-
 
 	//doc:
-	//http://wiki.openstreetmap.org/wiki/OpenRailwayMap  ---  http://wiki.openstreetmap.org/wiki/OpenRailwayMap/Tagging
-	//https://blog-en.openalfa.com/how-to-query-openstreetmap-using-the-overpass-api
-	//https://stackoverflow.com/questions/31879288/overpass-api-way-coordinates
-
-	//http://www.overpass-api.de/api/
-	//http://overpass.osm.rambler.ru/cgi/
-	//http://api.openstreetmap.fr/api/
-
-	//http://www.overpass-api.de/api/status
 
 
 	public static void main(String[] args) {
--- test.sh
+++ test.sh
@@ -1,3 +1,14 @@
+#http://wiki.openstreetmap.org/wiki/OpenRailwayMap  ---  http://wiki.openstreetmap.org/wiki/OpenRailwayMap/Tagging
+#https://blog-en.openalfa.com/how-to-query-openstreetmap-using-the-overpass-api
+#https://stackoverflow.com/questions/31879288/overpass-api-way-coordinates
+
+#http://www.overpass-api.de/api/
+#http://overpass.osm.rambler.ru/cgi/
+#http://api.openstreetmap.fr/api/
+
+#http://www.overpass-api.de/api/status
+
+
 cd ~/Bureau/gisco_rail/orm
 wget -O orm.osm "http://overpass-api.de/api/map?data=[out:xml];(node[railway](45,5,46,6);way[railway](45,5,46,6);relation[railway](45,5,46,6););(._;>;);out;"
 ogr2ogr --config OSM_USE_CUSTOM_INDEXING NO -skipfailures -f "ESRI Shapefile" shp orm.osm  -overwrite
