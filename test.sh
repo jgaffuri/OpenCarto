bb2a36c1e775cc3646502c7a348a2f2a1c2b7d50
julien Gaffuri <julien.gaffuri@gmail.com>
2017-10-03 11:05:38 +0200

    a

--- test.sh
+++ test.sh
@@ -21,17 +21,17 @@ cd ~/Bureau/gisco_rail/orm
 
 for usage in "main" "branch" "industrial" "military" "tourism" "test"
 do
-	echo $usage
+	echo Get data for usage $usage
 	wget -O orm_$usage.osm "http://overpass-api.de/api/map?data=[out:xml];(node[railway][usage=$usage](42,2,47,9);way[railway][usage=$usage](42,2,47,9);relation[railway][usage=$usage](42,2,47,9););(._;>;);out;"
 	ogr2ogr --config OSM_USE_CUSTOM_INDEXING NO -skipfailures -f "ESRI Shapefile" shp_$usage orm_$usage.osm  -overwrite
 	rm orm_$usage.osm
 done
+
+echo Get data for other usage
 wget -O orm_other.osm "http://overpass-api.de/api/map?data=[out:xml];(node[railway][!usage](42,2,47,9);way[railway][!usage](42,2,47,9);relation[railway][!usage](42,2,47,9););(._;>;);out;"
 ogr2ogr --config OSM_USE_CUSTOM_INDEXING NO -skipfailures -f "ESRI Shapefile" shp_other orm_other.osm  -overwrite
 rm orm_other.osm
 
+echo Get attribute data
+wget -O orm.csv "http://overpass-api.de/api/map?data=[out:csv(::id,name,description,railway,gauge,usage,'railway:traffic_mode',service,'railway:track_class',maxspeed,direction,highspeed,historic,bridge,'bridge:name',tunnel,'tunnel:name',electrified,'electrified:rail',voltage,incline,ele,start_date,end_date,operator,::timestamp,::version,::user,::user,::uid)];(node[railway][usage](42,2,47,9);way[railway][usage](42,2,47,9);relation[railway][usage](42,2,47,9););(._;>;);out;"
 
-wget -O orm.csv "http://overpass-api.de/api/map?data=[out:csv(::id,name,description,railway)];(node[railway][usage=main](46,8,47,9);way[railway][usage=main](46,8,47,9);relation[railway][usage=main](46,8,47,9););(._;>;);out;"
-#::id,name,description,railway,::gauge,usage,"railway:traffic_mode",service,"railway:track_class",maxspeed,direction,highspeed,historic,bridge,"bridge:name",tunnel,"tunnel:name",electrified,electrified:rail,::voltage,incline,::ele,start_date,end_date,operator
-#[out:csv(::id, ::lat, ::lon, name)];
-#[!"subway"]
