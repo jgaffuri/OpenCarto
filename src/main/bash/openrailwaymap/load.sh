34c1946aa077248409447ba9c324ceb1c1fd179d
julien Gaffuri <julien.gaffuri@gmail.com>
2017-10-08 11:06:38 +0200

    a

--- src/main/bash/openrailwaymap/load.sh
+++ src/main/bash/openrailwaymap/load.sh
@@ -30,7 +30,7 @@ do
 	echo "****** $cnt ******"
 	echo Get raw ORM data for $cnt$
 	#wget -O ormxml/orm_$cnt.osm "http://overpass-api.de/api/map?data=[out:xml];(area['ISO3166-1:alpha2'=$cnt][admin_level=2];)->.a;(node[railway](area.a);way[railway](area.a);relation[railway](area.a););(._;>;);out;"
-	wget -O osmxml/orm_$cnt_node.osm "http://overpass-api.de/api/map?data=[out:xml];(area['ISO3166-1:alpha2'=$cnt][admin_level=2];)->.a;(node[railway](area.a););(._;>;);out;"
-	wget -O osmxml/orm_$cnt_way.osm "http://overpass-api.de/api/map?data=[out:xml];(area['ISO3166-1:alpha2'=$cnt][admin_level=2];)->.a;(way[railway](area.a););(._;>;);out;"
-	wget -O osmxml/orm_$cnt_relation.osm "http://overpass-api.de/api/map?data=[out:xml];(area['ISO3166-1:alpha2'=$cnt][admin_level=2];)->.a;(relation[railway](area.a););(._;>;);out;"
+	wget -O osmxml/orm_node_$cnt.osm "http://overpass-api.de/api/map?data=[out:xml];(area['ISO3166-1:alpha2'=$cnt][admin_level=2];)->.a;(node[railway](area.a););(._;>;);out;"
+	wget -O osmxml/orm_way_$cnt.osm "http://overpass-api.de/api/map?data=[out:xml];(area['ISO3166-1:alpha2'=$cnt][admin_level=2];)->.a;(way[railway](area.a););(._;>;);out;"
+	wget -O osmxml/orm_relation_$cnt.osm "http://overpass-api.de/api/map?data=[out:xml];(area['ISO3166-1:alpha2'=$cnt][admin_level=2];)->.a;(relation[railway](area.a););(._;>;);out;"
 done
