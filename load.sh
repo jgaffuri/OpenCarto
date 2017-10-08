350514268bcad7ffbfa2dbf8e5a87469da6cf7e6
julien Gaffuri <julien.gaffuri@gmail.com>
2017-10-08 10:50:01 +0200

    a

---
+++ load.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+#https://blog-en.openalfa.com/how-to-query-openstreetmap-using-the-overpass-api
+#https://stackoverflow.com/questions/31879288/overpass-api-way-coordinates
+
+#http://www.overpass-api.de/api/
+#http://overpass.osm.rambler.ru/cgi/
+#http://api.openstreetmap.fr/api/
+
+#http://www.overpass-api.de/api/status
+#[timeout:25];
+
+#  ["key"]            /* filter objects tagged with this key and any value */
+#  [!"key"]           /* filter objects not tagged with this key and any value */
+#  ["key"="value"]    /* filter objects tagged with this key and this value */
+#  ["key"!="value"]   /* filter objects tagged with this key but not this value */
+#  ["key"~"value"]    /* filter objects tagged with this key and a value matching a regular expression */
+#  ["key"!~"value"    /* filter objects tagged with this key but a value not matching a regular expression */
+#  [~"key"~"value"]   /* filter objects tagged with a key and a value matching regular expressions */
+#  [~"key"~"value",i] /* filter objects tagged with a key and a case-insensitive value matching regular expressions */
+
+
+cd ~/Bureau/gisco_rail/orm
+
+
+echo "Load data from with overpass API"
+mkdir -p ormxml
+for cnt in "DE" "FR"
+do
+	echo "****** $cnt ******"
+	echo Get raw ORM data for $cnt$
+	#wget -O ormxml/orm_$cnt.osm "http://overpass-api.de/api/map?data=[out:xml];(area['ISO3166-1:alpha2'=$cnt][admin_level=2];)->.a;(node[railway](area.a);way[railway](area.a);relation[railway](area.a););(._;>;);out;"
+	wget -O ormxml/orm_$cnt_node.osm "http://overpass-api.de/api/map?data=[out:xml];(area['ISO3166-1:alpha2'=$cnt][admin_level=2];)->.a;(node[railway](area.a););(._;>;);out;"
+	wget -O ormxml/orm_$cnt_way.osm "http://overpass-api.de/api/map?data=[out:xml];(area['ISO3166-1:alpha2'=$cnt][admin_level=2];)->.a;(way[railway](area.a););(._;>;);out;"
+	wget -O ormxml/orm_$cnt_relation.osm "http://overpass-api.de/api/map?data=[out:xml];(area['ISO3166-1:alpha2'=$cnt][admin_level=2];)->.a;(relation[railway](area.a););(._;>;);out;"
+done
