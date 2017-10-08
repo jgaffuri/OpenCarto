72aa35bb7362260928c019edcec4817f4ba16325
julien Gaffuri <julien.gaffuri@gmail.com>
2017-10-08 19:50:52 +0200

    a

--- src/main/bash/openrailwaymap/transform.sh
+++ src/main/bash/openrailwaymap/transform.sh
@@ -7,7 +7,7 @@ cd ~/Bureau/gisco_rail/orm
 echo Convert OSM-XML country files into a single set of shapefiles
 mkdir -p shp
 ogr2ogr --config OSM_USE_CUSTOM_INDEXING NO -oo CONFIG_FILE=/home/juju/workspace/OpenCarto/src/main/bash/openrailwaymap/GDALormconf.ini -skipfailures -f "ESRI Shapefile" shp osmxml/orm_AT.osm  -overwrite -lco ENCODING=UTF-8
-for file in "BE" "BG" "CH" "CY" "CZ" "DK" "EE" "ES" "FI" "GB" "GR" "HU" "IE" "IS" "IT" "LT" "LU" "LV" "MT" "NL" "NO" "PL" "PT" "RO" "SE" "SI" "SK"
+for file in "BE" "BG" "CH" "CY" "CZ" "DK" "EE" "ES" "FI" "GB" "GR" "HU" "IE" "IS" "IT" "LT" "LU" "LV" "MT" "NL" "NO" "PL" "PT" "RO" "SE" "SI" "SK" "node_DE" "way_DE" "relation_DE" "node_FR" "way_FR" "relation_FR"
 do
 	echo "****** $file ******"
 	ogr2ogr --config OSM_USE_CUSTOM_INDEXING NO -oo CONFIG_FILE=/home/juju/workspace/OpenCarto/src/main/bash/openrailwaymap/GDALormconf.ini -skipfailures -f "ESRI Shapefile" shp osmxml/orm_$file.osm -append -lco ENCODING=UTF-8
