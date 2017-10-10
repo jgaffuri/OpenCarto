f8a75f02a9233b8cf29c9547fa88afb6b5019a5e
julien Gaffuri <julien.gaffuri@gmail.com>
2017-10-10 11:54:10 +0200

    a

--- src/main/bash/openrailwaymap/transform.sh
+++ src/main/bash/openrailwaymap/transform.sh
@@ -4,9 +4,9 @@
 
 cd ~/Bureau/gisco_rail/orm
 
-echo Convert OSM-XML country files into a single set of shapefiles
+echo Convert OSM-XML files into a single set of shapefiles
 mkdir -p shp
-ogr2ogr --config OSM_USE_CUSTOM_INDEXING NO -oo CONFIG_FILE=/home/juju/workspace/OpenCarto/src/main/bash/openrailwaymap/GDALormconf.ini -skipfailures -f "ESRI Shapefile" shp osmxml/orm_AT.osm  -overwrite -lco ENCODING=UTF-8
+ogr2ogr --config OSM_USE_CUSTOM_INDEXING NO -oo CONFIG_FILE=/home/juju/workspace/OpenCarto/src/main/bash/openrailwaymap/GDALormconf.ini -skipfailures -f "ESRI Shapefile" shp osmxml/orm_AT.osm  -overwrite -lco SHPT=POINT/ARC/POLYGON/MULTIPOINT/POINTZ/ARCZ/POLYGONZ/MULTIPOINTZ ENCODING=UTF-8
 for file in "BE" "BG" "CH" "CY" "CZ" "DK" "EE" "ES" "FI" "GB" "GR" "HU" "IE" "IS" "IT" "LT" "LV" "MT" "NL" "NO" "PL" "PT" "RO" "SE" "SI" "SK" "node_DE" "way_DE" "relation_DE" "node_FR" "way_FR" "relation_FR" "node_LU" "way_LU" "relation_LU"
 #for file in "node_LU" "way_LU" "relation_LU"
 do
