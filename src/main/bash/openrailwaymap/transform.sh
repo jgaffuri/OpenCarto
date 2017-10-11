1324d9552847536410a36302ee1f8ab0bc48f5aa
julien Gaffuri <julien.gaffuri@gmail.com>
2017-10-11 14:35:15 +0200

    a

--- src/main/bash/openrailwaymap/transform.sh
+++ src/main/bash/openrailwaymap/transform.sh
@@ -4,6 +4,11 @@
 
 cd ~/Bureau/gisco_rail/orm
 
+#finish filtering/selection. check tags one by one !
+#remove duplicates - See https://gitlab.com/snippets/34883
+#TODO: Fix all warnings
+#
+
 echo Convert OSM-XML files into a single set of shapefiles
 mkdir -p shp
 ogr2ogr --config OSM_USE_CUSTOM_INDEXING NO -oo CONFIG_FILE=/home/juju/workspace/OpenCarto/src/main/bash/openrailwaymap/GDALormconf.ini -skipfailures -f "ESRI Shapefile" shp osmxml/orm_AT.osm -overwrite -lco ENCODING=UTF-8
@@ -21,7 +26,9 @@ ogr2ogr shp/multilinestrings.shp shp/multilinestrings.shp -sql "SELECT osm_id, n
 ogr2ogr shp/multipolygons.shp shp/multipolygons.shp -sql "SELECT osm_id, name, descriptio AS descrip, railway, gauge, usage, railway_tr AS traff_mode, service, railway__1 AS track_cl, maxspeed, direction, highspeed, historic, bridge, bridge_nam, tunnel, tunnel_nam, electrifie AS electrif, electrif_1 AS elec_rai, voltage, incline, start_date, end_date, operator FROM multipolygons" ENCODING=UTF-8
 
 echo Filter by attribute value
-ogr2ogr shp/points.shp shp/points.shp -sql "SELECT * FROM points WHERE railway NOT IN ('','turntable','stop','service_station','tram_stop','blockpost','crossing_box','interlocking','signal_box','milestone','derail','buffer_stop','switch','railway_crossing','level_crossing','crossing')"
+ogr2ogr shp/points.shp shp/points.shp -sql "SELECT * FROM points WHERE railway NOT IN ('','tram','turntable','stop','service_station','tram_stop','blockpost','crossing_box','interlocking','signal_box','milestone','derail','buffer_stop','switch','railway_crossing','level_crossing','crossing')"
+ogr2ogr shp/lines.shp shp/lines.shp -sql "SELECT * FROM lines WHERE railway NOT IN ('tram','turntable','stop','service_station','tram_stop','blockpost','crossing_box','interlocking','signal_box','milestone','derail','buffer_stop','switch','railway_crossing','level_crossing','crossing')"
+ogr2ogr shp/multilinestrings.shp shp/multilinestrings.shp -sql "SELECT * FROM multilinestrings WHERE railway NOT IN ('tram','turntable','stop','service_station','tram_stop','blockpost','crossing_box','interlocking','signal_box','milestone','derail','buffer_stop','switch','railway_crossing','level_crossing','crossing')"
 
 #echo Select by attribute value
 #ogr2ogr shp/points.shp shp/points.shp -sql "SELECT * FROM points WHERE railway IN ('station','halt','stop','station-site','station site','historic_station')"
@@ -34,8 +41,3 @@ do
 	ogr2ogr -t_srs EPSG:3035 -s_srs EPSG:4326 shp/$type.shp shp/$type.shp
 	ogrinfo -sql "CREATE SPATIAL INDEX ON $type" shp/$type.shp
 done
-
-#make corrections for attribute values
-#remove duplicates - See https://gitlab.com/snippets/34883
-#TODO: Fix all warnings
-#get FR and DE. DO filtering at query level? Decompose by zone/region?
