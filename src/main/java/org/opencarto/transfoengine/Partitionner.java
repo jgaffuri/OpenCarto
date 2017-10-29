8ad22196318a83958c0259b5eb002f5a44b05a36
julien Gaffuri <julien.gaffuri@gmail.com>
2017-10-29 11:29:53 +0100

    a

--- src/main/java/org/opencarto/transfoengine/Partitionner.java
+++ src/main/java/org/opencarto/transfoengine/Partitionner.java
@@ -4,6 +4,7 @@
 package org.opencarto.transfoengine;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 import org.opencarto.datamodel.Feature;
 
@@ -14,26 +15,41 @@ import org.opencarto.datamodel.Feature;
  * @author julien Gaffuri
  *
  */
-public class Partitionner {
+public abstract class Partitionner {
+
+	//TODO need fo partition object?
 
 	public Object runRecurssively(Collection<Feature> features) {
 		if (partitionningNeeded(features)) {
-			//decompose in four parts
+			//partiionning
+			Collection<Collection<Feature>> partitions = partition(features);
 			//launch job on parts
+			Collection<Object> results = new HashSet<Object>();
+			for(Collection<Feature> partition : partitions) results.add(run(partition));
 			//return reconciliated results
-			return null;
+			return reconciliate(results);
 		} else {
 			return run(features);
 		}
 	}
 
-
 	private boolean partitionningNeeded(Collection<Feature> features) {
+		//TODO
 		return false;
 	}
 
+	private Collection<Collection<Feature>> partition(Collection<Feature> features) {
+		//TODO
+		return null;
+	}
 
 	public Object run(Collection<Feature> features) {
+		//TODO
+		return null;
+	}
+
+	private Object reconciliate(Collection<Object> results) {
+		//TODO
 		return null;
 	}
 
