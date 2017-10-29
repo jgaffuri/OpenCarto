2956cf5e3ca1badbb33b6f450d44273b0fffbebe
julien Gaffuri <julien.gaffuri@gmail.com>
2017-10-29 10:44:50 +0100

    a

--- src/main/java/org/opencarto/TestInterRounding.java
+++ src/main/java/org/opencarto/TestInterRounding.java
@@ -8,18 +8,22 @@ public class TestInterRounding {
 
 	public static void main(String[] args) {
 
-		Polygon cell1 = JTSGeomUtil.createPolygon(0,0, 100,0, 100,100, 0,100, 0,0);
-		Polygon cell2 = JTSGeomUtil.createPolygon(100,0, 200,0, 200,100, 100,100, 100,0);
 
+		//not OK
 		Polygon test = JTSGeomUtil.createPolygon(10,10, 190,10+(1.0/3.0), 190,90, 10,90, 10,10);
+		//OK
 		//Polygon test = JTSGeomUtil.createPolygon(10,10, 190,10+Math.sqrt(2), 190,90, 10,90, 10,10);
+		//OK
+		//Polygon test = JTSGeomUtil.createPolygon(10,10, 190,10+Math.PI, 190,90, 10,90, 10,10);
+
+		//conclusion: not exact for some cases, but should be enough for partitioning work from quadtree structure
 
 		System.out.println(test);
 		System.out.println(test.getArea());
 		System.out.println(test.getLength());
-		//System.out.println(cell1);
-		//System.out.println(cell2);
 
+		Polygon cell1 = JTSGeomUtil.createPolygon(0,0, 100,0, 100,100, 0,100, 0,0);
+		Polygon cell2 = JTSGeomUtil.createPolygon(100,0, 200,0, 200,100, 100,100, 100,0);
 		Polygon inter1 = (Polygon) test.intersection(cell1);
 		Polygon inter2 = (Polygon) test.intersection(cell2);
 
@@ -34,8 +38,9 @@ public class TestInterRounding {
 		System.out.println(union.getLength());
 
 		System.out.println( union.equals(test) );
-		System.out.println( union.symDifference(test) );
-		System.out.println( test.symDifference(union) );
+		System.out.println( union.symDifference(test) + " " +union.symDifference(test).getArea() );
+		System.out.println( test.symDifference(union) + " " +test.symDifference(union).getArea() );
+
 	}
 
 }
