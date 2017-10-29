0a71ac3b9bcef449fb4866d11dc009ce3a15fd78
julien Gaffuri <julien.gaffuri@gmail.com>
2017-10-29 10:01:25 +0100

    a

---
+++ src/main/java/org/opencarto/TestInterRounding.java
@@ -0,0 +1,15 @@
+package org.opencarto;
+
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Polygon;
+
+public class TestInterRounding {
+
+	public static void main(String[] args) {
+
+		GeometryFactory gf = new GeometryFactory();
+		Polygon cell;
+
+	}
+
+}
