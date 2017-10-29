49defaed0f0bbbe46cba5de50b1c123b4d843228
julien Gaffuri <julien.gaffuri@gmail.com>
2017-10-29 10:33:20 +0100

    a

--- src/main/java/org/opencarto/TestInterRounding.java
+++ src/main/java/org/opencarto/TestInterRounding.java
@@ -11,9 +11,31 @@ public class TestInterRounding {
 		Polygon cell1 = JTSGeomUtil.createPolygon(0,0, 100,0, 100,100, 0,100, 0,0);
 		Polygon cell2 = JTSGeomUtil.createPolygon(100,0, 200,0, 200,100, 100,100, 100,0);
 
-		System.out.println(cell1);
-		System.out.println(cell2);
+		//Polygon test = JTSGeomUtil.createPolygon(Math.PI,Math.PI/2, 200-Math.sqrt(2),Math.sqrt(2), 150, , Math.PI,Math.PI/2);
+		Polygon test = JTSGeomUtil.createPolygon(10,10, 190, 10+(1/3), 190,90, 10,90, 10,10);
 
+		System.out.println(test);
+		System.out.println(test.getArea());
+		System.out.println(test.getLength());
+		//System.out.println(cell1);
+		//System.out.println(cell2);
+
+		Polygon inter1 = (Polygon) test.intersection(cell1);
+		Polygon inter2 = (Polygon) test.intersection(cell2);
+
+		System.out.println(inter1);
+		System.out.println(inter2);
+		System.out.println(inter1.getArea()+inter2.getArea());
+
+		Polygon union = (Polygon) inter1.union(inter2);
+		//union = (Polygon) union.buffer(0);
+		System.out.println(union);
+		System.out.println(union.getArea());
+		System.out.println(union.getLength());
+
+		System.out.println( union.equals(test) );
+		System.out.println( union.symDifference(test) );
+		System.out.println( test.symDifference(union) );
 	}
 
 }
