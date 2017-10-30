9160a34e6e131df470afa395a036b028c687ae5a
julien Gaffuri <julien.gaffuri@gmail.com>
2017-10-30 10:04:48 +0100

    a

--- src/main/java/org/opencarto/transfoengine/GeneralisationPartitionner.java
+++ src/main/java/org/opencarto/transfoengine/GeneralisationPartitionner.java
@@ -33,36 +33,14 @@ public class GeneralisationPartitionner {
 		pIni.setFeatures(features, false);
 
 		//launch process
-		runRecursively(pIni);
-	}
-
-	public void runRecursively(Partition p) {
-		if(! p.isTooLarge(maxCoordinatesNumber)) run(p);
-		else {
-			//decompose in sub-partitions
-			Collection<Partition> sps = p.getSubPartitions();
-			//run process on sub-partitions
-			for(Partition sp : sps) runRecursively(sp);
-			//recompose result
-			//TODO
-		}
-	}
-
-
-	public Object run(Partition p) {
-		//TODO launch generalisation here
-		return null;
-	}
-
-	private Object reconciliate(Collection<Partition> ps) {
-		//TODO
-		return null;
+		pIni.runRecursively();
 	}
 
 	public class Partition{
 		Envelope env;
 		Polygon extend = null;
 		Collection<Feature> features = null;
+		Collection<Partition> subPartitions;
 
 		Partition(double xMin, double xMax, double yMin, double yMax){ this(new Envelope(xMin,xMax,yMin,yMax)); }
 		Partition(Envelope env){
@@ -70,26 +48,30 @@ public class GeneralisationPartitionner {
 			extend = JTS.toGeometry(this.env);
 		}
 
-		public void setFeatures(Collection<Feature> fs, boolean computeIntersections) {
+		private void setFeatures(Collection<Feature> fs, boolean computeIntersections) {
 			if(!computeIntersections) {
 				features = fs;
 				return;
 			}
+
 			features = new HashSet<Feature>();
 			for(Feature f : fs) {
 				Geometry g = f.getGeom();
 				Envelope env_ = g.getEnvelopeInternal();
 				if(!env.intersects(env_)) continue;
 
+				//feature fully in the envelope
 				if(env.contains(env_)) {
 					features.add(f);
 					continue;
 				}
 
+				//check if feature intersects envelope
 				Geometry inter = g.intersection(extend);
 				if(inter.isEmpty()) continue;
 				if(inter.getArea()==0) continue;
 
+				//create intersection feature
 				Feature f_ = new Feature();
 				f_.setGeom(inter);
 				f_.id = f.id;
@@ -97,15 +79,43 @@ public class GeneralisationPartitionner {
 			}
 		}
 
-		public boolean isTooLarge(double maxCoordinatesNumber) {
+		//determine if a partition is to large
+		private boolean isTooLarge(double maxCoordinatesNumber) {
 			int coordinatesNumber = 0;
 			for(Feature f : features) coordinatesNumber += f.getGeom().getNumPoints();
 			return coordinatesNumber > maxCoordinatesNumber;
 		}
 
-		public Collection<Partition> getSubPartitions() {
-			//TODO create four sub-partitions and return them
-			return null;
+		//run process on the partition, decomposing it recursively if it is too large.
+		private void runRecursively() {
+			if(! isTooLarge(maxCoordinatesNumber)) run();
+			else {
+				//decompose in sub-partitions
+				decompose();
+				//run process on sub-partitions
+				for(Partition sp : subPartitions) sp.runRecursively();
+				//recompose sub-partitions
+				recompose();
+			}
+		}
+
+		//decompose the partiotion into four partitions
+		private void decompose() {
+			//TODO create four sub-partitions
+			//clean top partition to avoid duplication of objects
+			features.clear(); features=null;
+		}
+
+		//recompose partition
+		private void recompose() {
+			//TODO recompose
+			//clean sub partitions
+			subPartitions.clear(); subPartitions = null;
+		}
+
+		//run process on the partition
+		private void run() {
+			//TODO run generalisation on features
 		}
 
 	}
