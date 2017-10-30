067028e4d72e14fe7bf84fdfedef82a46ebda096
julien Gaffuri <julien.gaffuri@gmail.com>
2017-10-30 10:53:24 +0100

    a

--- src/main/java/org/opencarto/transfoengine/GeneralisationPartitionner.java
+++ src/main/java/org/opencarto/transfoengine/GeneralisationPartitionner.java
@@ -29,23 +29,25 @@ public class GeneralisationPartitionner {
 		for(Feature f : features) env.expandToInclude(f.getGeom().getEnvelopeInternal());
 
 		//create initial partition
-		Partition pIni = new Partition(env);
+		Partition pIni = new Partition(env,"0");
 		pIni.setFeatures(features, false);
 
 		//launch process
 		pIni.runRecursively();
 	}
 
-	public class Partition{
+	public class Partition {
 		Envelope env;
 		Polygon extend = null;
 		Collection<Feature> features = null;
 		Collection<Partition> subPartitions;
+		String code;
 
-		Partition(double xMin, double xMax, double yMin, double yMax){ this(new Envelope(xMin,xMax,yMin,yMax)); }
-		Partition(Envelope env){
+		Partition(double xMin, double xMax, double yMin, double yMax, String code){ this(new Envelope(xMin,xMax,yMin,yMax), code); }
+		Partition(Envelope env, String code){
 			this.env = env;
 			extend = JTS.toGeometry(this.env);
+			this.code = code;
 		}
 
 		private void setFeatures(Collection<Feature> fs, boolean computeIntersections) {
@@ -99,9 +101,29 @@ public class GeneralisationPartitionner {
 			}
 		}
 
-		//decompose the partiotion into four partitions
+		//decompose the partition into four partitions
 		private void decompose() {
-			//TODO create four sub-partitions
+			//create four sub-partitions
+			double xMid = 0.5*(env.getMinX() + env.getMaxX()), yMid = 0.5*(env.getMinY() + env.getMaxY());
+			Partition
+			p1 = new Partition(env.getMinX(), xMid, env.getMinY(), yMid, this.code+"1"),
+			p2 = new Partition(xMid, env.getMaxX(), env.getMinY(), yMid, this.code+"2"),
+			p3 = new Partition(env.getMinX(), xMid, yMid, env.getMaxY(), this.code+"3"),
+			p4 = new Partition(xMid, env.getMaxX(), yMid, env.getMaxY(), this.code+"4")
+			;
+
+			//fill it
+			p1.setFeatures(features, true);
+			p2.setFeatures(features, true);
+			p3.setFeatures(features, true);
+			p4.setFeatures(features, true);
+
+			subPartitions = new HashSet<GeneralisationPartitionner.Partition>();
+			if(p1.features.size()>0) subPartitions.add(p1);
+			if(p2.features.size()>0) subPartitions.add(p2);
+			if(p3.features.size()>0) subPartitions.add(p3);
+			if(p4.features.size()>0) subPartitions.add(p4);
+
 			//clean top partition to avoid duplication of objects
 			features.clear(); features=null;
 		}
