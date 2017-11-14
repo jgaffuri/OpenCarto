026e7677d2e8b32f0a1c877cbbce63794907b3a2
julien Gaffuri <julien.gaffuri@gmail.com>
2017-11-14 16:29:59 +0100

    a

---
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/CUnitValid.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.opencarto.transfoengine.tesselationGeneralisation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opencarto.transfoengine.Constraint;
+import org.opencarto.transfoengine.Transformation;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class CUnitValid  extends Constraint<AUnit> {
+
+	boolean valid;
+
+	public CUnitValid(AUnit agent) { super(agent); }
+
+	@Override
+	public void computeCurrentValue() {
+		valid = getAgent().getObject().getGeom().isValid();
+	}
+
+	@Override
+	public void computeSatisfaction() {
+		satisfaction = valid? 10 : 0;
+	}
+
+	@Override
+	public List<Transformation<AUnit>> getTransformations() {
+		return new ArrayList<Transformation<AUnit>>();
+	}
+
+}
