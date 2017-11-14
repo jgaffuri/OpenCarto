ebe7fcffe41a7853745ac877acd0afc6e90f78e8
julien Gaffuri <julien.gaffuri@gmail.com>
2017-11-14 14:23:01 +0100

    a

--- src/main/java/org/opencarto/MainGISCOQuality.java
+++ src/main/java/org/opencarto/MainGISCOQuality.java
@@ -7,6 +7,7 @@ import org.opencarto.io.SHPUtil;
 import org.opencarto.transfoengine.tesselationGeneralisation.ATesselation;
 import org.opencarto.transfoengine.tesselationGeneralisation.AUnit;
 import org.opencarto.transfoengine.tesselationGeneralisation.CUnitNoOverlap;
+import org.opencarto.transfoengine.tesselationGeneralisation.CUnitValid;
 import org.opencarto.transfoengine.tesselationGeneralisation.DefaultTesselationGeneralisation;
 
 import com.vividsolutions.jts.index.SpatialIndex;
@@ -25,8 +26,9 @@ public class MainGISCOQuality {
 
 		//final int epsg = 3035; ArrayList<Feature> fs = SHPUtil.loadSHP(basePath+ "nuts_2013/RG_LAEA_1M.shp",epsg).fs;
 		//final int epsg = 3035; ArrayList<Feature> fs = SHPUtil.loadSHP(basePath+ "nuts_2013/RG_LAEA_100k.shp",epsg).fs;
-		final int epsg = 3035; ArrayList<Feature> fs = SHPUtil.loadSHP(basePath+"comm_2013/COMM_RG_100k_2013_LAEA.shp",epsg).fs;
+		//final int epsg = 3035; ArrayList<Feature> fs = SHPUtil.loadSHP(basePath+"comm_2013/COMM_RG_100k_2013_LAEA.shp",epsg).fs;
 		//final int epsg = 3857; ArrayList<Feature> fs = SHPUtil.loadSHP(basePath+"commplus_100k/COMMPLUS_0404_WM.shp", epsg).fs;
+		final int epsg = 4258; ArrayList<Feature> fs = SHPUtil.loadSHP(basePath+"gaul/FINAL_GAUL_MINUS.shp", epsg).fs;
 		for(Feature f : fs)
 			if(f.getProperties().get("NUTS_ID") != null) f.id = ""+f.getProperties().get("NUTS_ID");
 			else if(f.getProperties().get("COMM_ID") != null) f.id = ""+f.getProperties().get("COMM_ID");
@@ -38,10 +40,12 @@ public class MainGISCOQuality {
 		for(AUnit a : t.aUnits) index.insert(a.getObject().getGeom().getEnvelopeInternal(), a.getObject());
 
 		//LOGGER.info("   Set units constraints");
-		for(AUnit a : t.aUnits)
+		for(AUnit a : t.aUnits) {
 			a.addConstraint(new CUnitNoOverlap(a, index));
+			a.addConstraint(new CUnitValid(a));
+		}
 
-		DefaultTesselationGeneralisation.runEvaluation(t, "/home/juju/Bureau/qual_cont/", 5);
+		DefaultTesselationGeneralisation.runEvaluation(t, "/home/juju/Bureau/qual_cont/", 10);
 
 		System.out.println("End");
 	}
