fe357e23e28f650ed071bc05b1480d6e819f6a6c
julien Gaffuri <julien.gaffuri@gmail.com>
2017-11-14 11:01:51 +0100

    a

---
+++ src/main/java/org/opencarto/MainGISCOQuality.java
@@ -0,0 +1,20 @@
+package org.opencarto;
+/**
+ * 
+ */
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class MainGISCOQuality {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
