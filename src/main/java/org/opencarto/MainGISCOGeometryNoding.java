be05a451820e40a40c75945ab6b1a3e13d4dbd58
julien Gaffuri <julien.gaffuri@gmail.com>
2017-11-25 10:59:01 +0100

    a

--- src/main/java/org/opencarto/MainGISCOGeometryNoding.java
+++ src/main/java/org/opencarto/MainGISCOGeometryNoding.java
@@ -3,8 +3,6 @@ package org.opencarto;
 import java.util.Collection;
 import java.util.HashSet;
 
-import org.opencarto.util.JTSGeomUtil;
-
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 
@@ -15,8 +13,14 @@ public class MainGISCOGeometryNoding {
 		//make example on simple geometries + on real geo file
 
 		Collection<Geometry> lineCol = new HashSet<Geometry>();
-		lineCol.add( JTSGeomUtil.createLineString(0,0, 1,0, 0,1, 0,0) );
-		lineCol.add( JTSGeomUtil.createLineString(1,0, 0.5000000000000001,0.5) );
+		//lineCol.add( JTSGeomUtil.createLineString(0,2, 0,0) );
+		//lineCol.add( JTSGeomUtil.createLineString(0,2, 0.000000000000000055,1, 1,0) );
+		
+		//LINESTRING ( 3225160.713144858 1840681.7306859163, 3225180.9437183817 1840603.6479523852 )
+		//LINESTRING ( 3225161.5657030023 1840649.4736821207, 3225160.713144858 1840681.730685917 )
+		//[ (3225160.713144858, 1840681.7306859163, NaN) ]
+
+		
 		Geometry union = new GeometryFactory().buildGeometry(lineCol).union();
 
 		System.out.println(union);
