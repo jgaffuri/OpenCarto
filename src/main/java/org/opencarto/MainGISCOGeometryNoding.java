a968aeb533992d7ad8735f53bd447d5957125941
julien Gaffuri <julien.gaffuri@gmail.com>
2017-11-25 10:59:12 +0100

    a

--- src/main/java/org/opencarto/MainGISCOGeometryNoding.java
+++ src/main/java/org/opencarto/MainGISCOGeometryNoding.java
@@ -15,16 +15,16 @@ public class MainGISCOGeometryNoding {
 		Collection<Geometry> lineCol = new HashSet<Geometry>();
 		//lineCol.add( JTSGeomUtil.createLineString(0,2, 0,0) );
 		//lineCol.add( JTSGeomUtil.createLineString(0,2, 0.000000000000000055,1, 1,0) );
-		
+
 		//LINESTRING ( 3225160.713144858 1840681.7306859163, 3225180.9437183817 1840603.6479523852 )
 		//LINESTRING ( 3225161.5657030023 1840649.4736821207, 3225160.713144858 1840681.730685917 )
 		//[ (3225160.713144858, 1840681.7306859163, NaN) ]
 
-		
+
 		Geometry union = new GeometryFactory().buildGeometry(lineCol).union();
 
 		System.out.println(union);
-		
+
 		//detect noding pb
 		//correct noding pb
 
