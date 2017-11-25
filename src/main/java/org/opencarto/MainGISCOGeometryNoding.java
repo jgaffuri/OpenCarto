f3fc7554f6ddaf00d7551a0b54a31fddd074a7d9
julien Gaffuri <julien.gaffuri@gmail.com>
2017-11-25 10:28:39 +0100

    a

--- src/main/java/org/opencarto/MainGISCOGeometryNoding.java
+++ src/main/java/org/opencarto/MainGISCOGeometryNoding.java
@@ -1,17 +1,21 @@
 package org.opencarto;
 
+import org.opencarto.util.JTSGeomUtil;
+
+import com.vividsolutions.jts.geom.Polygon;
+
 public class MainGISCOGeometryNoding {
 
 	public static void main(String[] args) {
-		
+
 		//make example on simple geometries + on real geo file
-		
-		
-		
+
+		Polygon poly1 = JTSGeomUtil.createPolygon(0,0, 1,0, 0,1, 0,0);
+
 		//detect noding pb
 		//correct noding pb
-		
-		
+
+
 	}
 
 }
