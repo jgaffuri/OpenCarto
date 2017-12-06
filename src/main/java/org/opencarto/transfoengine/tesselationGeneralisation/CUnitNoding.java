db7fc3cbb892b3b34877a8fdd6edeb2d45c5e02d
julien Gaffuri <julien.gaffuri@gmail.com>
2017-12-06 13:07:29 +0100

    a

--- src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/CUnitNoding.java
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/CUnitNoding.java
@@ -3,6 +3,11 @@
  */
 package org.opencarto.transfoengine.tesselationGeneralisation;
 
+import org.apache.log4j.Logger;
+import org.opencarto.transfoengine.Constraint;
+
+import com.vividsolutions.jts.index.SpatialIndex;
+
 /**
  * 
  * Check a unit is correctly noded to its touching ones.
@@ -10,6 +15,14 @@ package org.opencarto.transfoengine.tesselationGeneralisation;
  * @author julien
  *
  */
-public class CUnitNoding {
+public class CUnitNoding  extends Constraint<AUnit> {
+	private final static Logger LOGGER = Logger.getLogger(CUnitNoding.class);
+
+	SpatialIndex index;
+
+	public CUnitNoding(AUnit agent, SpatialIndex index) {
+		super(agent);
+		this.index = index;
+	}
 
 }
