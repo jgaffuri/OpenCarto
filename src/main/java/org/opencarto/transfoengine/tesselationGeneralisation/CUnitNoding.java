fd3b0029a6f76054ddab46f0cb5c56b941c29c0c
julien Gaffuri <julien.gaffuri@gmail.com>
2017-12-06 13:09:57 +0100

    a

--- src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/CUnitNoding.java
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/CUnitNoding.java
@@ -3,9 +3,17 @@
  */
 package org.opencarto.transfoengine.tesselationGeneralisation;
 
-import org.apache.log4j.Logger;
-import org.opencarto.transfoengine.Constraint;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.log4j.Logger;
+import org.opencarto.datamodel.Feature;
+import org.opencarto.transfoengine.Constraint;
+import org.opencarto.transfoengine.Transformation;
+import org.opencarto.transfoengine.tesselationGeneralisation.CUnitOverlap.Overlap;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.index.SpatialIndex;
 
 /**
@@ -19,10 +27,55 @@ public class CUnitNoding  extends Constraint<AUnit> {
 	private final static Logger LOGGER = Logger.getLogger(CUnitNoding.class);
 
 	SpatialIndex index;
+	Object nodingIssue = null;
 
 	public CUnitNoding(AUnit agent, SpatialIndex index) {
 		super(agent);
 		this.index = index;
+	}
+
+	@Override
+	public void computeCurrentValue() {
+		LOGGER.info("CUnitNoding "+getAgent().getObject().id);
+
+		/*/retrieve all units overlapping, with spatial index
+		MultiPolygon geom = (MultiPolygon) getAgent().getObject().getGeom();
+		for(Feature unit : (List<Feature>)index.query(geom.getEnvelopeInternal())) {
+			if(unit == getAgent().getObject()) continue;
+			if(!geom.getEnvelopeInternal().intersects(unit.getGeom().getEnvelopeInternal())) continue;
+
+			//check overlap
+			boolean overlap = false;
+			try {
+				overlap = geom.overlaps(unit.getGeom());
+			} catch (Exception e) {
+				//overlaps.add(new Overlap(unit.id, null, -1, -1));
+				continue;
+			}
+			if(!overlap) continue;
+
+			Geometry inter = geom.intersection(unit.getGeom());
+			double interArea = inter.getArea();
+			if(interArea == 0) continue;
+			//overlaps.add(new Overlap(unit.id, inter.getCoordinate(), interArea, 100.0*interArea/geom.getArea()));
+		}*/
+	}
+
+	@Override
+	public void computeSatisfaction() {
+		if(nodingIssue == null) satisfaction = 10;
+		else satisfaction = 0;
+	}
+
+	@Override
+	public List<Transformation<AUnit>> getTransformations() {
+		return new ArrayList<Transformation<AUnit>>();
+	}
+
+	public String getMessage(){
+		StringBuffer sb = new StringBuffer(super.getMessage());
+		sb.append(",").append(nodingIssue);
+		return sb.toString();
 	}
 
 }
