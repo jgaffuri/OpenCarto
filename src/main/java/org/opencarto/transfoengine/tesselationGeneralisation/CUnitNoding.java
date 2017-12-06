85c6bbb0b975c5e973f9fda2eb60b7d1e35a90c4
julien Gaffuri <julien.gaffuri@gmail.com>
2017-12-06 13:12:02 +0100

    a

--- src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/CUnitNoding.java
+++ src/main/java/org/opencarto/transfoengine/tesselationGeneralisation/CUnitNoding.java
@@ -4,16 +4,23 @@
 package org.opencarto.transfoengine.tesselationGeneralisation;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.opencarto.algo.base.Union;
 import org.opencarto.datamodel.Feature;
 import org.opencarto.transfoengine.Constraint;
 import org.opencarto.transfoengine.Transformation;
 
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.TopologyException;
 import com.vividsolutions.jts.index.SpatialIndex;
+import com.vividsolutions.jts.operation.linemerge.LineMerger;
 
 /**
  * 
@@ -37,6 +44,30 @@ public class CUnitNoding  extends Constraint<AUnit> {
 	public void computeCurrentValue() {
 		LOGGER.info("CUnitNoding "+getAgent().getObject().id);
 
+		
+		
+		LOGGER.info("   Run linemerger on rings");
+		Collection<Geometry> lineCol = new HashSet<Geometry>();
+		for(MultiPolygon unit : units) lineCol.add(unit.getBoundary());
+		LOGGER.info("     compute union of boundaries...");
+		//TODO find smarter ways to union lines?
+		Geometry union = null;
+		try {
+			union = new GeometryFactory().buildGeometry(lineCol).union();
+		} catch (TopologyException e1) {
+			LOGGER.error("     Geometry.union failed. "+e1.getMessage());
+			e1.printStackTrace();
+			//TODO if error related to non noded geometries, node it and try again.
+			union = Union.get(lineCol);
+		}
+		LOGGER.info("     linemerger...");
+		LineMerger lm = new LineMerger();
+		lm.add(union);
+		union = null;
+		Collection<LineString> lines = lm.getMergedLineStrings();
+		lm = null;
+
+		
 		//retrieve all units touching, with spatial index
 		MultiPolygon geom = (MultiPolygon) getAgent().getObject().getGeom();
 		for(Feature unit : (List<Feature>)index.query(geom.getEnvelopeInternal())) {
