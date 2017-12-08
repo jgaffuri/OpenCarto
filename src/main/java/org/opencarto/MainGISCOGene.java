8e8f680e3ea642ec34e823bae700e07f2fc01560
julien Gaffuri <julien.gaffuri@gmail.com>
2017-12-08 09:28:10 +0100

    a

--- src/main/java/org/opencarto/MainGISCOGene.java
+++ src/main/java/org/opencarto/MainGISCOGene.java
@@ -91,7 +91,7 @@ public class MainGISCOGene {
 
 
 
-		/*/narrow gaps removal
+		//narrow gaps removal
 		//final int epsg = 3035; ArrayList<Feature> fs = SHPUtil.loadSHP(basePath+ "nuts_2013/RG_LAEA_1M.shp", epsg).fs;
 		//final int epsg = 3035; ArrayList<Feature> fs = SHPUtil.loadSHP(basePath+ "nuts_2013/RG_LAEA_100k.shp", epsg).fs;
 		//final int epsg = 3857; ArrayList<Feature> fs = SHPUtil.loadSHP(basePath+"gaul/GAUL_CLEAN_WM.shp", epsg).fs;
@@ -116,11 +116,11 @@ public class MainGISCOGene {
 		SHPUtil.saveSHP(fs_, outPath+ "100k_1M/comm/", "out_narrow_gaps_removed.shp");
 		//SHPUtil.saveSHP(fs_, outPath+ "100k_1M/gaul/", "out_narrow_gaps_removed.shp");
 		//SHPUtil.saveSHP(fs_, outPath+ "test/", "out_narrow_gaps_removed.shp");
-*/
 
 
 
-		//generalisation (partitionned)
+
+		/*/generalisation (partitionned)
 		LOGGER.info("Load data");
 		//final int epsg = 3035; ArrayList<Feature> fs = SHPUtil.loadSHP(outPath+ "test/out_narrow_gaps_removed.shp", epsg).fs;
 		//final int epsg = 3035; ArrayList<Feature> fs = SHPUtil.loadSHP(basePath+"comm_2013/COMM_RG_100k_2013_LAEA.shp", epsg).fs;
@@ -145,7 +145,7 @@ public class MainGISCOGene {
 			}}, fs, 1500000, 25000);
 		SHPUtil.saveSHP(fs_, outPath+ "100k_1M/comm/", "out.shp");
 		//SHPUtil.saveSHP(fs_, outPath+ "test/", "out.shp");
-		
+*/
 
 
 
