57ada1b54aa30c9632964020e61b6129b696d50e
julien Gaffuri <julien.gaffuri@gmail.com>
2017-12-10 19:27:17 +0100

    a

--- src/main/java/org/opencarto/algo/noding/NodingUtil.java
+++ src/main/java/org/opencarto/algo/noding/NodingUtil.java
@@ -6,6 +6,8 @@ package org.opencarto.algo.noding;
 import java.util.Collection;
 import java.util.HashSet;
 
+import org.opencarto.transfoengine.tesselationGeneralisation.AUnit;
+
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
@@ -13,6 +15,8 @@ import com.vividsolutions.jts.geom.LineSegment;
 import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.MultiPoint;
 import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.index.SpatialIndex;
+import com.vividsolutions.jts.index.strtree.STRtree;
 
 /**
  * @author julien Gaffuri
@@ -25,11 +29,34 @@ public class NodingUtil {
 		out.addAll(analyseNoding(g1,g2)); out.addAll(analyseNoding(g2,g1));
 		return out;
 	}
+
 	public static Collection<NodingIssue> analyseNoding(Geometry g1, Geometry g2) {
+		//check if points of g1 are noded to points of g2.
+		GeometryFactory gf = new GeometryFactory();
+		Collection<NodingIssue> out = new HashSet<NodingIssue>();
+		MultiPoint g2_pt = gf.createMultiPoint(g2.getCoordinates());
+		//improve efficiency !
+		for(Coordinate c : g1.getCoordinates()) {
+			Point pt = gf.createPoint(c);
+			//noded case ok
+			if( pt.distance(g2_pt) == 0 ) continue;
+			//not noded case ok
+			if( pt.distance(g2) > 0 ) continue;
+			//issue detected
+			//System.out.println(c);
+			out.add( new NodingIssue(c) );
+		}
+		return out;
+	}
+
+	public static Collection<NodingIssue> analyseNoding(LineString g1, LineString g2) {
 		//check if points of g1 are noded to points of g2.
 		GeometryFactory gf = new GeometryFactory();
 
 		//build index of g2 segments
+		SpatialIndex segIndex = new STRtree();
+		for(AUnit a : t.aUnits) index.insert(a.getObject().getGeom().getEnvelopeInternal(), a.getObject());
+
 		//go through points of g1
 		//get segments close to it
 		//go through segments
