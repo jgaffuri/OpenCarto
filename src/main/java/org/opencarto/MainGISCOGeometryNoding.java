452cbde275b9cece03baba908f0bdf6de41d5900
julien Gaffuri <julien.gaffuri@gmail.com>
2017-12-11 12:56:20 +0100

    a

--- src/main/java/org/opencarto/MainGISCOGeometryNoding.java
+++ src/main/java/org/opencarto/MainGISCOGeometryNoding.java
@@ -46,26 +46,6 @@ public class MainGISCOGeometryNoding {
 		new File(outPath).mkdirs();
 		uEng.runEvaluation(outPath + "eval_units_noding.csv", true);
 
-
-		/*/make example on simple geometries + on real geo file
-
-		Collection<Geometry> lineCol = new HashSet<Geometry>();
-		//lineCol.add( JTSGeomUtil.createLineString(0,2, 0,0) );
-		//lineCol.add( JTSGeomUtil.createLineString(0,2, 0.000000000000000055,1, 1,0) );
-
-		lineCol.add( JTSGeomUtil.createLineString(60.713144858, 81.730685917, 60.713144858, 81.7306859163, 80.9437183817, 03.6479523852) );
-		lineCol.add( JTSGeomUtil.createLineString(61.5657030023, 49.4736821207, 60.713144858, 81.730685917, 60.713144858, 81.730685917) );
-
-		//LINESTRING ( 60.713144858 81.7306859163, 80.9437183817 03.6479523852 )
-		//LINESTRING ( 61.5657030023 49.4736821207, 60.713144858 81.730685917 )
-		//[ (60.713144858, 81.7306859163, NaN) ]
-		//60.713144858 81.730685917
-
-		Geometry union = new GeometryFactory().buildGeometry(lineCol).union();
-
-		System.out.println(union);
-		 */
-
 		System.out.println("End");
 	}
 
