d1fb037732bae5f65829e5e2d4980ec8719f2636
julien Gaffuri <julien.gaffuri@gmail.com>
2017-12-22 17:57:18 +0100

    a

--- src/main/java/org/opencarto/MainGISCOGene.java
+++ src/main/java/org/opencarto/MainGISCOGene.java
@@ -143,7 +143,7 @@ public class MainGISCOGene {
 				p.features = t.getUnits(epsg);
 
 				//SHPUtil.saveSHP(p.getFeatures(), outPath+ "100k_1M/comm/", "Z_out_"+p.getCode()+".shp");
-			}}, fs, 1500000, 25000);
+			}}, fs, 2500000, 30000);
 		SHPUtil.saveSHP(fs_, outPath+ "100k_1M/comm/", "out.shp");
 
 
