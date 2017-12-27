f3b10023d4736db487e14cbcfb785a8b0b0ac8a6
julien Gaffuri <julien.gaffuri@gmail.com>
2017-12-27 17:45:57 +0100

    a

--- src/main/java/org/opencarto/MainGISCOGene.java
+++ src/main/java/org/opencarto/MainGISCOGene.java
@@ -3,10 +3,8 @@
  */
 package org.opencarto;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -28,9 +26,6 @@ import org.opencarto.transfoengine.tesselationGeneralisation.CFaceValidity;
 import org.opencarto.transfoengine.tesselationGeneralisation.DefaultTesselationGeneralisation;
 import org.opencarto.transfoengine.tesselationGeneralisation.TesselationGeneralisationSpecifications;
 import org.opencarto.util.JTSGeomUtil;
-
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.Polygon;
 
 /**
  * @author julien Gaffuri
@@ -142,182 +137,9 @@ public class MainGISCOGene {
 		for(Feature f : fs_) f.setGeom(JTSGeomUtil.toMulti(f.getGeom()));
 		SHPUtil.saveSHP(fs_, outPath+ rep+"/", "out.shp");
 
-
-
-
-		/*/nuts regions generalisation
-		for(String inputScale : new String[]{"1M"}){
-			String inputDataPath = basePath+ "nuts_2013/RG_LAEA_"+inputScale+".shp";
-			String straitDataPath = basePath + "out/straits_with_input_"+inputScale+"/straits_";
-			for(int targetScaleM : new int[]{1,3,10,20,60}){
-				LOGGER.info("--- NUTS generalisation from "+inputScale+" to "+targetScaleM+"M");
-				runGeneralisation(inputDataPath, straitDataPath+targetScaleM+"M.shp", NUTSFrom1MSpecs, 3035, targetScaleM*resolution1M, outPath+inputScale+"_input/"+targetScaleM+"M/");
-			}
-		}*/
-
-		/*/communes generalisation
-		for(String inputScale : new String[]{"100k"}){
-			String inputDataPathComm = basePath+"comm_2013/COMM_RG_"+inputScale+"_2013_LAEA.shp";
-			runNUTSGeneralisation(inputDataPathComm, null, communesFrom100kSpecs, 3035, resolution1M, outPath+"comm_with_input_"+inputScale+"/");
-		}*/
-		/*/commune 100k extracts
-		for(String commDS : new String[]{"finland","france","germany","london","slovenia","spain"}){ //isgreenland
-			LOGGER.info("--- COMM generalisation "+commDS);
-			String inputDataPathComm = basePath+"comm_2013/extract/COMM_RG_100k_2013_LAEA_"+commDS+".shp";
-			runGeneralisation(inputDataPathComm, null, communesFrom100kSpecs, 3035, resolution1M, outPath+"comm_100k_extract/"+commDS+"/");
-		}*/
-
-
-
-
-
-		/*/straits detections
-		for(String inputScale : new String[]{"1M","100k"}){
-			for(int scaleM : new int[]{1,3,10,20,60}){
-				double resolution = scaleM*resolution1M;
-				LOGGER.info("--- Straits detection ("+inputScale+" -> "+scaleM+"M, resolution="+resolution+"m)");
-
-				LOGGER.info("Load data");
-				ArrayList<Feature> fs = SHPUtil.loadSHP(basePath+ "nuts_2013/RG_LAEA_"+inputScale+".shp", 3035).fs;
-				for(Feature f : fs) f.id = ""+f.getProperties().get("NUTS_ID");
-
-				LOGGER.info("Run straits detection");
-				Collection<Feature> fsOut = MorphologicalAnalysis.runStraitAndBaysDetection(fs, resolution , 1.0 * resolution*resolution, 4);
-
-				LOGGER.info("Save");
-				for(Feature f:fsOut) f.setProjCode(3035);
-				SHPUtil.saveSHP(fsOut, outPath+"straits_with_input_"+inputScale+"/", "straits_"+scaleM+"M.shp");
-			}
-		}*/
-
-		/*/narrow parts and gaps (NPG) detection
-		for(String inputScale : new String[]{"1M","100k"}){
-			for(int scaleM : new int[]{1,3,10,20,60}){
-				double resolution = scaleM*resolution1M;
-				LOGGER.info("--- NPG detection ("+inputScale+" -> "+scaleM+"M, resolution="+resolution+"m)");
-
-				LOGGER.info("Load data");
-				ArrayList<Feature> fs = SHPUtil.loadSHP(basePath+ "nuts_2013/RG_LAEA_"+inputScale+".shp", 3035).fs;
-				for(Feature f : fs) f.id = ""+f.getProperties().get("NUTS_ID");
-
-				LOGGER.info("Run NPG detection");
-				Collection<Feature> fsOut = MorphologicalAnalysis.getNarrowPartsAndGaps(fs, resolution , 0.5 * resolution*resolution, 4);
-
-				LOGGER.info("Save");
-				for(Feature f:fsOut) f.setProjCode(3035);
-				SHPUtil.saveSHP(fsOut, outPath+"NPG_with_input_"+inputScale+"/", "NPG_"+scaleM+"M.shp");
-			}
-		}*/
-
-		/*static void runNUTSGeneralisationEvaluation(String inputDataPath, int epsg, double resolution, String outPath) {
-				new File(outPath).mkdirs();
-
-				LOGGER.info("Load data");
-				ArrayList<Feature> fs = SHPUtil.loadSHP(inputDataPath,epsg).fs;
-				for(Feature f : fs) f.id = ""+f.getProperties().get("NUTS_ID");
-
-				LOGGER.info("Create tesselation");
-				ATesselation t = new ATesselation(fs);
-				fs = null;
-				for(AUnit uAg : t.aUnits) uAg.setId(uAg.getObject().id);
-
-				//TODO run straigths detection
-
-				LOGGER.info("create tesselation's topological map");
-				t.buildTopologicalMap();
-
-				LOGGER.info("Set generalisation constraints");
-				DefaultTesselationGeneralisation.defaultSpecs.setTopologicalConstraints(t, resolution);
-				DefaultTesselationGeneralisation.defaultSpecs.setUnitConstraints(t, resolution); //TODO check that
-
-				//LOGGER.info("Remove generalisation constraint on face size");
-				for(AFace af : t.aFaces) af.removeConstraint(af.getConstraint(CFaceSize.class));
-				LOGGER.info("Add constraint on unit's size");
-				HashMap<String, Double> nutsAreas = loadNutsArea100k();
-				for(AUnit au : t.aUnits){
-					Double area = nutsAreas.get(au.getId());
-					if(area==null) {
-						//System.err.println("Could not find area value for nuts "+id);
-						continue;
-					}
-					//LOGGER.info(id+" "+area);
-					au.addConstraint(new CUnitSizePreservation(au, area.doubleValue()));
-				}
-
-				LOGGER.info("Run evaluation");
-				DefaultTesselationGeneralisation.runEvaluation(t, outPath, 7);
-			}
-
-
-			public static HashMap<String,Double> loadNutsArea100k(){
-				String inputPath = "/home/juju/Bureau/nuts_gene_data/nuts_2013/100k/NUTS_RG_LVL3_100K_2013_LAEA.shp";
-				ArrayList<Feature> fs = SHPUtil.loadSHP(inputPath,3035).fs;
-				HashMap<String,Double> out = new HashMap<String,Double>();
-				for(Feature f : fs)
-					out.put(""+f.getProperties().get("NUTS_ID"), f.getGeom().getArea());
-				return out;
-			}*/
-
-		//evaluation
-		/*/GISCOgene
-		for(String inputScale : new String[]{"1M","100k"})
-			for(int targetScaleM : new int[]{1,3,10,20,60}) {
-				LOGGER.info("--- Evaluation: NUTS generalisation "+inputScale+"-"+targetScaleM+"M");
-				runNUTSGeneralisationEvaluation(outPath+inputScale+"_input/"+targetScaleM+"M/units.shp", 3035, targetScaleM*resolution1M, outPath+inputScale+"_input/"+targetScaleM+"M/");
-			}
-		//1spatial
-		for(int targetScaleM : new int[]{3,20,60}) {
-			LOGGER.info("--- Evaluation: NUTS/1spatial generalisation "+targetScaleM+"M");
-			runNUTSGeneralisationEvaluation(basePath+"1spatial/1Generalise_Result"+targetScaleM+"M.shp", 3857, targetScaleM*resolution1M, basePath+"1spatial/eval"+targetScaleM+"M/");
-		}
-		//2013 versions
-		for(int targetScaleM : new int[]{1,3,10,20,60}) {
-			LOGGER.info("--- Evaluation: NUTS 2010 generalisation "+targetScaleM+"M");
-			runNUTSGeneralisationEvaluation(basePath+"/nuts_2013/"+targetScaleM+"M/LAEA/lvl3/RG.shp", 3035, targetScaleM*resolution1M, basePath+"/nuts_2013/"+targetScaleM+"M/LAEA/lvl3/");
-		}*/
-		//TODO comm 1M/100k?
-		//TODO nuts 2010 100k too?
-
 		LOGGER.info("End");
 	}
 
-
-	static void runGeneralisation(String inputDataPath, String straitDataPath, TesselationGeneralisationSpecifications specs, int epsg, double resolution, String outPath) throws Exception {
-		new File(outPath).mkdirs();
-
-		LOGGER.info("Load data");
-		ArrayList<Feature> fs = SHPUtil.loadSHP(inputDataPath,epsg).fs;
-		for(Feature f : fs)
-			if(f.getProperties().get("NUTS_ID") != null) f.id = ""+f.getProperties().get("NUTS_ID");
-			else if(f.getProperties().get("COMM_ID") != null) f.id = ""+f.getProperties().get("COMM_ID");
-
-		LOGGER.info("Create tesselation object");
-		ATesselation t = new ATesselation(fs);
-		fs = null;
-		for(AUnit uAg : t.aUnits) uAg.setId(uAg.getObject().id);
-
-		if(straitDataPath != null){
-			LOGGER.info("Load straits and link them to units");
-			//index units by id
-			HashMap<String,AUnit> aUnitsI = new HashMap<String,AUnit>();
-			for(AUnit au : t.aUnits) aUnitsI.put(au.getId(), au);
-			//load straights and link them to units
-			ArrayList<Feature> straits = SHPUtil.loadSHP(straitDataPath,epsg).fs;
-			for(Feature s : straits){
-				AUnit au = aUnitsI.get(s.getProperties().get("unit_id"));
-				Collection<Geometry> polys = JTSGeomUtil.getGeometries(s.getGeom());
-				//TODO all polygons?
-			}
-		}
-
-		LOGGER.info("Run generalisation");
-		DefaultTesselationGeneralisation.run(t, specs, resolution, outPath);
-
-		LOGGER.info("Save output");
-		t.exportAsSHP(outPath, epsg);
-		LOGGER.info("Save report on agents satisfaction");
-		t.exportAgentReport(outPath);
-	}
 
 
 
@@ -325,11 +147,11 @@ public class MainGISCOGene {
 	static TesselationGeneralisationSpecifications specs = new TesselationGeneralisationSpecifications() {
 		public void setUnitConstraints(ATesselation t, double resolution){
 			//double resSqu = resolution*resolution;
-			for(AUnit a : t.aUnits) {
-				//a.addConstraint(new CUnitNoNarrowPartsAndGapsXXX(a).setPriority(10));
-				//a.addConstraint(new CUnitNoNarrowGaps(a, resolution, 0.1*resSqu, 4).setPriority(10));
-				//a.addConstraint(new ConstraintOneShot<AUnit>(a, new TUnitNarrowGapsFilling(a, resolution, 0.1*resSqu, 4)).setPriority(10));
-			}
+			//for(AUnit a : t.aUnits) {
+			//a.addConstraint(new CUnitNoNarrowPartsAndGapsXXX(a).setPriority(10));
+			//a.addConstraint(new CUnitNoNarrowGaps(a, resolution, 0.1*resSqu, 4).setPriority(10));
+			//a.addConstraint(new ConstraintOneShot<AUnit>(a, new TUnitNarrowGapsFilling(a, resolution, 0.1*resSqu, 4)).setPriority(10));
+			//}
 		}
 
 		public void setTopologicalConstraints(ATesselation t, double resolution){
