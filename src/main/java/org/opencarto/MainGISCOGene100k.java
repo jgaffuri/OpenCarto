c575ca8e1e6fe687c17123466621a2230d272b80
julien Gaffuri <julien.gaffuri@gmail.com>
2018-01-12 23:58:12 +0100

    a

--- src/main/java/org/opencarto/MainGISCOGene100k.java
+++ src/main/java/org/opencarto/MainGISCOGene100k.java
@@ -46,7 +46,9 @@ public class MainGISCOGene100k {
 		ATesselation.LOGGER.setLevel(Level.WARN);
 		//Agent.LOGGER.setLevel(Level.ALL);
 
-		//TODO partionning: improve recomposing efficiency, with better union
+		//TODO test generalisation without narrow gap deletions
+		//if fails, correct inconsticencies
+		
 		//gene to xM scales
 		//removal of large elongated faces/holes: face size constraint: take into account shape - use erosion ?
 		// + do not delete small isolated elements (detect them based on spatial index) - scale them only
