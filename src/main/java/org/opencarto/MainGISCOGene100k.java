aed700f56509739a1439cf800a54ed9e9cb07b57
julien Gaffuri <julien.gaffuri@gmail.com>
2018-01-15 15:22:30 +0100

    a

--- src/main/java/org/opencarto/MainGISCOGene100k.java
+++ src/main/java/org/opencarto/MainGISCOGene100k.java
@@ -46,8 +46,6 @@ public class MainGISCOGene100k {
 		ATesselation.LOGGER.setLevel(Level.WARN);
 		//Agent.LOGGER.setLevel(Level.ALL);
 
-		//TODO noding fix: fix first vertice positions !
-
 		//gene to xM scales
 		//removal of large elongated faces/holes: face size constraint: take into account shape - use erosion ?
 		// + do not delete small isolated elements (detect them based on spatial index) - scale them only
