831beb83686e424b7b8d9c7e26cbf5f6cf2f0ba4
julien Gaffuri <julien.gaffuri@gmail.com>
2018-01-26 20:20:42 +0100

    a

--- src/main/java/org/opencarto/MainNUTSExtraction.java
+++ src/main/java/org/opencarto/MainNUTSExtraction.java
@@ -1,21 +1,27 @@
 package org.opencarto;
 
+import java.util.ArrayList;
+
+import org.opencarto.datamodel.Feature;
+import org.opencarto.io.SHPUtil;
+
 public class MainNUTSExtraction {
 
 	public static void main(String[] args) {
-		String outPath = "~/Bureau/validation";
+		String outPath = "~/Bureau/drafts/cnts/";
 
 		//load nuts regions
+		ArrayList<Feature> fs = SHPUtil.loadSHP("~/Bureau/drafts/NUTS_RG_2016_RG_01M_DRAFT.shp", 4258).fs;
 
-		//
 		for(String cnt : new String[] {"FR","BE","DE"}) {
+			//make country folder
 			//filter - nuts 3 regions for cnt
 			//save as SHP
 			//make and save all other levels? make boundaries?
 			//make overview image
 			//zip everything
 		}
-		
+
 	}
 
 }
