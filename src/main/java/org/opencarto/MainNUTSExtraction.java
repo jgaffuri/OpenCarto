c3003f1db20594cc32a9b19dc811b1d9c33aba6a
julien Gaffuri <julien.gaffuri@gmail.com>
2018-01-26 20:14:47 +0100

    a

---
+++ src/main/java/org/opencarto/MainNUTSExtraction.java
@@ -0,0 +1,17 @@
+package org.opencarto;
+
+public class MainNUTSExtraction {
+
+	public static void main(String[] args) {
+		String outPath = "~/Bureau/";
+
+		//load nuts regions
+
+		//
+		for(String cnt : new String[] {"FR","BE","DE"}) {
+			
+		}
+		
+	}
+
+}
