4c58e4710915a9e4dbe5a26b9c1e9ea61e23bcf4
julien Gaffuri <julien.gaffuri@gmail.com>
2018-01-26 23:14:13 +0100

    a

--- src/main/java/org/opencarto/MainNUTSExtraction.java
+++ src/main/java/org/opencarto/MainNUTSExtraction.java
@@ -18,12 +18,10 @@ public class MainNUTSExtraction {
 
 		//load nuts regions
 		ArrayList<Feature> fs = SHPUtil.loadSHP("/home/juju/Bureau/drafts/NUTS_RG_2016_RG_01M_DRAFT.shp", 4258).fs;
-		System.out.println(fs.size());
 
 		//extract all cnt ids
 		HashSet<String> cnts = new HashSet<String>();
 		for(Feature f : fs) cnts.add(f.getProperties().get("CNTR_ID").toString());
-
 
 		for(String cnt : new String[] { "BE"/*"FR","BE","DE"*/}) {
 		//for(String cnt : cnts) {
@@ -38,8 +36,6 @@ public class MainNUTSExtraction {
 				if(f.getProperties().get("CNTR_ID").equals(cnt))
 					fs_.add(f);
 
-			System.out.println("Nb="+fs_.size());
-
 			//save as new shp file
 			SHPUtil.saveSHP(fs_, o, "NUTS_RG_2016_01M_DRAFT_"+cnt+".shp");
 
@@ -51,7 +47,7 @@ public class MainNUTSExtraction {
 			map.setNoDataColor(Color.LIGHT_GRAY);
 			//map.setBounds(x1, x2, y1, y2);
 			map.make();
-			map.saveAsImage(o+"sdfhkjshfkjsh.png", 2000, true, false);
+			map.saveAsImage(o+"NUTS_RG_2016_01M_DRAFT_"+cnt+".png", 2000, true, false);
 			map.dispose();
 
 			//zip everything
@@ -61,7 +57,8 @@ public class MainNUTSExtraction {
 					"NUTS_RG_2016_01M_DRAFT_"+cnt+".fix",
 					"NUTS_RG_2016_01M_DRAFT_"+cnt+".prj",
 					"NUTS_RG_2016_01M_DRAFT_"+cnt+".shp",
-					"NUTS_RG_2016_01M_DRAFT_"+cnt+".shx"
+					"NUTS_RG_2016_01M_DRAFT_"+cnt+".shx",
+					"NUTS_RG_2016_01M_DRAFT_"+cnt+".png"
 			});
 
 
