1158ef7daca0b7654d6a914188691ba90dd5e5b3
julien Gaffuri <julien.gaffuri@gmail.com>
2018-01-26 20:17:54 +0100

    a

--- src/main/java/org/opencarto/MainNUTSExtraction.java
+++ src/main/java/org/opencarto/MainNUTSExtraction.java
@@ -3,13 +3,17 @@ package org.opencarto;
 public class MainNUTSExtraction {
 
 	public static void main(String[] args) {
-		String outPath = "~/Bureau/";
+		String outPath = "~/Bureau/validation";
 
 		//load nuts regions
 
 		//
 		for(String cnt : new String[] {"FR","BE","DE"}) {
-			
+			//filter - nuts 3 regions for cnt
+			//save as SHP
+			//make and save all other levels? make boundaries?
+			//make overview image
+			//zip everything
 		}
 		
 	}
