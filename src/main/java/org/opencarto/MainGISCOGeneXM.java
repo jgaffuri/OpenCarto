21fb470b5d3923683cd53f29d1ef572756cb7a32
julien Gaffuri <julien.gaffuri@gmail.com>
2018-03-05 21:52:31 +0100

    a

--- src/main/java/org/opencarto/MainGISCOGeneXM.java
+++ src/main/java/org/opencarto/MainGISCOGeneXM.java
@@ -5,19 +5,10 @@ package org.opencarto;
 
 import java.util.Collection;
 
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.opencarto.algo.polygon.MorphologicalAnalysis;
 import org.opencarto.datamodel.Feature;
-import org.opencarto.datamodel.graph.GraphBuilder;
 import org.opencarto.io.SHPUtil;
-import org.opencarto.partitionning.Partition;
-import org.opencarto.partitionning.Partition.Operation;
-import org.opencarto.transfoengine.CartographicResolution;
-import org.opencarto.transfoengine.tesselationGeneralisation.ATesselation;
-import org.opencarto.transfoengine.tesselationGeneralisation.AUnit;
 import org.opencarto.transfoengine.tesselationGeneralisation.DefaultTesselationGeneralisation;
-import org.opencarto.util.JTSGeomUtil;
 
 /**
  * @author julien Gaffuri
@@ -29,40 +20,22 @@ public class MainGISCOGeneXM {
 	public static void main(String[] args) {
 		LOGGER.info("Start");
 
-		GraphBuilder.LOGGER.setLevel(Level.WARN);
-		DefaultTesselationGeneralisation.LOGGER.setLevel(Level.WARN);
-		ATesselation.LOGGER.setLevel(Level.WARN);
-
 		String basePath = "/home/juju/Bureau/nuts_gene_data/";
 		final String outPath = basePath+"out/";
 
 		for(double s : new double[]{1,3,10,20,60}) {
-
-			final CartographicResolution res = new CartographicResolution(s*1e6);
-
-			Collection<Feature> fs;
+			double scaleDenominator = s*1e6;
 
 			LOGGER.info("Load data");
 			final int epsg = 3857; final String rep="serbia"; String inFile = basePath+"serbia/NUTS_3_serbia_WM.shp";
+			Collection<Feature> units = SHPUtil.loadSHP(inFile, epsg).fs;
+			for(Feature f : units) for(String id : new String[] {"NUTS_CODE","COMM_ID","idgene","GISCO_ID"}) if(f.getProperties().get(id) != null) f.id = ""+f.getProperties().get(id);
 
-			fs = SHPUtil.loadSHP(inFile, epsg).fs;
-			for(Feature f : fs) for(String id : new String[] {"NUTS_CODE","COMM_ID","idgene","GISCO_ID"}) if(f.getProperties().get(id) != null) f.id = ""+f.getProperties().get(id);
+			LOGGER.info("Launch generalisation");
+			units = DefaultTesselationGeneralisation.runGeneralisation(units, scaleDenominator, 4);
 
-			Partition.runRecursively(fs, new Operation() {
-				public void run(Partition p) {
-					LOGGER.info(p);
-					MorphologicalAnalysis.removeNarrowGapsTesselation(p.getFeatures(), res.getSeparationDistanceMeter(), 5, 1e-5);
-
-					ATesselation t = new ATesselation(p.getFeatures(), p.getEnvelope());
-					for(AUnit uAg : t.aUnits) uAg.setId(uAg.getObject().id);
-
-					try {
-						DefaultTesselationGeneralisation.run(t, null, res, outPath+ rep);
-					} catch (Exception e) { e.printStackTrace(); }
-				}}, 1000000, 5000, false);
-			for(Feature f : fs) f.setGeom(JTSGeomUtil.toMulti(f.getGeom()));
-			SHPUtil.saveSHP(fs, outPath+ rep+"/", "NUTS_3_serbia_"+(int)s+"M_WM.shp");
-
+			LOGGER.info("Save output data");
+			SHPUtil.saveSHP(units, outPath+ rep+"/", "NUTS_3_serbia_"+(int)s+"M_WM.shp");
 		}
 
 		LOGGER.info("End");
