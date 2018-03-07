7fd39e518220f7af0cc93688c328aad078168132
julien Gaffuri <julien.gaffuri@gmail.com>
2018-03-07 09:22:05 +0100

    a

--- src/main/java/org/opencarto/MainTests.java
+++ src/main/java/org/opencarto/MainTests.java
@@ -1,5 +1,19 @@
 package org.opencarto;
 
+import java.util.Collection;
+
+import org.opencarto.datamodel.Feature;
+import org.opencarto.io.SHPUtil;
+import org.opencarto.util.FeatureUtil;
+
 public class MainTests {
+
+	public static void main(String[] args) {
+
+		String basePath = "/home/juju/Bureau/nuts_gene_data/";
+		int epsg = 4258; Collection<Feature> fs = SHPUtil.loadSHP(basePath+"commplus/COMM_PLUS.shp", epsg).fs;
+
+		cnts = FeatureUtil.
+	}	
 
 }
