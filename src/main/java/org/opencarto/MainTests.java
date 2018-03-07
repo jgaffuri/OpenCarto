758c4583a1327fb21baef255c6f70d6eca5309fd
julien Gaffuri <julien.gaffuri@gmail.com>
2018-03-07 14:42:01 +0100

    a

--- src/main/java/org/opencarto/MainTests.java
+++ src/main/java/org/opencarto/MainTests.java
@@ -1,7 +1,6 @@
 package org.opencarto;
 
 import java.util.Collection;
-import java.util.HashMap;
 
 import org.opencarto.datamodel.Feature;
 import org.opencarto.io.SHPUtil;
@@ -16,9 +15,13 @@ public class MainTests {
 
 		System.out.println("Load data");
 		int epsg = 4258; Collection<Feature> fs = SHPUtil.loadSHP(basePath+"commplus/COMM_PLUS_clean.shp", epsg).fs;
-		System.out.println("Compute id check");
-		HashMap<String, Integer> cnts = FeatureUtil.checkIdentfier(fs, "GISCO_ID");
-		System.out.println(cnts);
+
+		System.out.println(fs.size());
+		System.out.println(FeatureUtil.getNumberVertices(fs));
+
+		//System.out.println("Compute id check");
+		//HashMap<String, Integer> cnts = FeatureUtil.checkIdentfier(fs, "GISCO_ID");
+		//System.out.println(cnts);
 
 		System.out.println("End");
 	}	
