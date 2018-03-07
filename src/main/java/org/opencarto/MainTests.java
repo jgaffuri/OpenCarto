914140240cf735bd6234d9f090ccb99257e95755
julien Gaffuri <julien.gaffuri@gmail.com>
2018-03-07 09:41:38 +0100

    a

--- src/main/java/org/opencarto/MainTests.java
+++ src/main/java/org/opencarto/MainTests.java
@@ -1,6 +1,7 @@
 package org.opencarto;
 
 import java.util.Collection;
+import java.util.HashMap;
 
 import org.opencarto.datamodel.Feature;
 import org.opencarto.io.SHPUtil;
@@ -9,11 +10,17 @@ import org.opencarto.util.FeatureUtil;
 public class MainTests {
 
 	public static void main(String[] args) {
+		System.out.println("Start");
 
 		String basePath = "/home/juju/Bureau/nuts_gene_data/";
-		int epsg = 4258; Collection<Feature> fs = SHPUtil.loadSHP(basePath+"commplus/COMM_PLUS.shp", epsg).fs;
 
-		cnts = FeatureUtil.
+		System.out.println("Load data");
+		int epsg = 4258; Collection<Feature> fs = SHPUtil.loadSHP(basePath+"commplus/COMM_PLUS_clean.shp", epsg).fs;
+		System.out.println("Compute id check");
+		HashMap<String, Integer> cnts = FeatureUtil.checkIdentfier(fs, "GISCO_ID");
+		System.out.println(cnts);
+
+		System.out.println("End");
 	}	
 
 }
