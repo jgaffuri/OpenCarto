c5784fbefeccd2602d033ea81716a0e22d4c3dae
julien Gaffuri <julien.gaffuri@gmail.com>
2018-03-08 09:50:39 +0100

    a

--- src/main/java/org/opencarto/MainTests.java
+++ src/main/java/org/opencarto/MainTests.java
@@ -1,6 +1,7 @@
 package org.opencarto;
 
 import java.util.Collection;
+import java.util.HashMap;
 
 import org.opencarto.datamodel.Feature;
 import org.opencarto.io.SHPUtil;
@@ -14,14 +15,15 @@ public class MainTests {
 		String basePath = "/home/juju/Bureau/nuts_gene_data/";
 
 		System.out.println("Load data");
-		int epsg = 4258; Collection<Feature> fs = SHPUtil.loadSHP(basePath+"commplus/COMM_PLUS_clean.shp", epsg).fs;
+		//Collection<Feature> fs = SHPUtil.loadSHP(basePath+"commplus/COMM_PLUS_clean.shp").fs;
+		Collection<Feature> fs = SHPUtil.loadSHP(basePath+"out/100k_1M/commplus/COMM_PLUS_WM_1M_1.shp").fs;
 
 		System.out.println(fs.size());
 		System.out.println(FeatureUtil.getNumberVertices(fs));
 
-		//System.out.println("Compute id check");
-		//HashMap<String, Integer> cnts = FeatureUtil.checkIdentfier(fs, "GISCO_ID");
-		//System.out.println(cnts);
+		System.out.println("Compute id check");
+		HashMap<String, Integer> cnts = FeatureUtil.checkIdentfier(fs, "GISCO_ID");
+		System.out.println(cnts);
 
 		System.out.println("End");
 	}	
