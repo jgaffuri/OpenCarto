e4f17a6c0762cf6d19652ee5a92b93a488a545ae
julien Gaffuri <julien.gaffuri@gmail.com>
2018-03-08 11:04:09 +0100

    a

--- src/main/java/org/opencarto/MainTests.java
+++ src/main/java/org/opencarto/MainTests.java
@@ -1,11 +1,18 @@
 package org.opencarto;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.opencarto.datamodel.Feature;
+import org.opencarto.io.CSVUtil;
 import org.opencarto.io.SHPUtil;
+import org.opencarto.transfoengine.CartographicResolution;
 import org.opencarto.util.FeatureUtil;
+import org.opencarto.util.JTSGeomUtil;
+
+import com.vividsolutions.jts.geom.Geometry;
 
 public class MainTests {
 
@@ -18,21 +25,37 @@ public class MainTests {
 		//Collection<Feature> fs = SHPUtil.loadSHP(basePath+"commplus/COMM_PLUS_clean.shp").fs;
 		Collection<Feature> fs = SHPUtil.loadSHP(basePath+"out/100k_1M/commplus/COMM_PLUS_WM_1M_1.shp").fs;
 
+		/*
 		System.out.println(fs.size());
 		System.out.println(FeatureUtil.getNumberVertices(fs));
 
 		System.out.println("Compute id check");
 		HashMap<String, Integer> cnts = FeatureUtil.checkIdentfier(fs, "GISCO_ID");
 		System.out.println(cnts);
+		 */
+
+		final CartographicResolution res = new CartographicResolution(1e6);
+		ArrayList<Map<String, Object>> data = analysePolygonsSizes(fs, res.getPerceptionSizeSqMeter());
+		CSVUtil.save(data, "/home/juju/Bureau/", "area_analysis.csv");
 
 		System.out.println("End");
 	}	
 
+
 	//considering multi/polygonal features, get the patches that are smallest than an area threshold
-	public HashMap<String,Double> analysePolygonsSizes(Collection<Feature> fs, double areaThreshold) {
-		HashMap<String,Double> out = new HashMap<String,Double>();
+	public static ArrayList<Map<String, Object>> analysePolygonsSizes(Collection<Feature> fs, double areaThreshold) {
+		ArrayList<Map<String, Object>> out = new ArrayList<Map<String, Object>>();
 		for(Feature f : fs) {
-			
+			Collection<Geometry> polys = JTSGeomUtil.getGeometries( JTSGeomUtil.keepOnlyPolygonal(f.getGeom()) );
+			for(Geometry poly : polys) {
+				double area = poly.getArea();
+				if( area > areaThreshold ) continue;
+				Map<String, Object> m = new HashMap<String, Object>();
+				m.put("id", f.id);
+				m.put("area", area);
+				m.put("position", poly.getCentroid().getCoordinate());
+				out.add(m);
+			}
 		}
 		return out;
 	}
