1916f80ceb5d69e4aae479510a87f770d98979cb
julien Gaffuri <julien.gaffuri@gmail.com>
2018-03-12 19:50:13 +0100

    a

--- src/main/java/org/opencarto/MainGISCOGeneXM.java
+++ src/main/java/org/opencarto/MainGISCOGeneXM.java
@@ -21,21 +21,20 @@ public class MainGISCOGeneXM {
 		LOGGER.info("Start");
 
 		String basePath = "/home/juju/Bureau/nuts_gene_data/";
-		final String outPath = basePath+"out/";
 
 		for(double s : new double[]{1,3,10,20,60}) {
 			double scaleDenominator = s*1e6;
 
-			LOGGER.info("Load data");
-			final int epsg = 3857; final String rep="serbia"; String inFile = basePath+"nutsplus/NUTS_PLUS_1203_WM.shp";
+			LOGGER.info("Load data "+((int)s)+"M");
+			final int epsg = 3857; String inFile = basePath+"nutsplus/NUTS_PLUS_1203_WM.shp";
 			Collection<Feature> units = SHPUtil.loadSHP(inFile, epsg).fs;
 			for(Feature f : units) for(String id : new String[] {"NUTS_P_ID","NUTS_CODE","COMM_ID","idgene","GISCO_ID"}) if(f.getProperties().get(id) != null) f.id = ""+f.getProperties().get(id);
 
-			LOGGER.info("Launch generalisation");
+			LOGGER.info("Launch generalisation for "+((int)s)+"M");
 			units = DefaultTesselationGeneralisation.runGeneralisation(units, DefaultTesselationGeneralisation.defaultSpecs, scaleDenominator, 5, false);
 
-			LOGGER.info("Save output data");
-			SHPUtil.saveSHP(units, outPath+ rep+"/", "NUTS_PLUS_"+(int)s+"M_WM.shp");
+			LOGGER.info("Save output data for "+((int)s)+"M");
+			SHPUtil.saveSHP(units, basePath + "out/nutsplus/", "NUTS_PLUS_"+((int)s)+"M_WM.shp");
 		}
 
 		LOGGER.info("End");
