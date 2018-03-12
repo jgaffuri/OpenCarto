f8c6220ddc1bf5cb47f06d0fe52fe5c13cd0e441
julien Gaffuri <julien.gaffuri@gmail.com>
2018-03-12 19:45:57 +0100

    a

--- src/main/java/org/opencarto/MainGISCOGeneXM.java
+++ src/main/java/org/opencarto/MainGISCOGeneXM.java
@@ -32,7 +32,7 @@ public class MainGISCOGeneXM {
 			for(Feature f : units) for(String id : new String[] {"NUTS_P_ID","NUTS_CODE","COMM_ID","idgene","GISCO_ID"}) if(f.getProperties().get(id) != null) f.id = ""+f.getProperties().get(id);
 
 			LOGGER.info("Launch generalisation");
-			units = DefaultTesselationGeneralisation.runGeneralisation(units, DefaultTesselationGeneralisation.defaultSpecs, scaleDenominator, 4, false);
+			units = DefaultTesselationGeneralisation.runGeneralisation(units, DefaultTesselationGeneralisation.defaultSpecs, scaleDenominator, 5, false);
 
 			LOGGER.info("Save output data");
 			SHPUtil.saveSHP(units, outPath+ rep+"/", "NUTS_PLUS_"+(int)s+"M_WM.shp");
