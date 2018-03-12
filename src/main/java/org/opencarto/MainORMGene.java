e32b05ae19a8381617b358286ba320308ced150f
julien Gaffuri <julien.gaffuri@gmail.com>
2018-03-12 15:26:06 +0100

    a

--- src/main/java/org/opencarto/MainORMGene.java
+++ src/main/java/org/opencarto/MainORMGene.java
@@ -47,7 +47,7 @@ AND "railway" != 'subway'
 		//is it possible to use some tags to select main lines from xxx ?
 		//see tags - to make classification
 		//service? usage? maxspeed-highspeed? electrified-voltage-frequency? gauge? railway:traffic_mode? railway:preferred_direction-railway:bidirectional? name-description-ref?
-		//use service! - keep only tracks with service==null
+		//use service! - keep only tracks with service==null + voltage!=null
 
 		//TODO define specs based on ORM model and generalisation process in mind (at least ERM specs should be covered)
 		//specs for input dataset (1:5k): tracks selected, with proper attributes, well structured. basic ETL process.
