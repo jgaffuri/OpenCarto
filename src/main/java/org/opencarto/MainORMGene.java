23f824453e5ff90093c972f4d5259bcf254105e5
julien Gaffuri <julien.gaffuri@gmail.com>
2018-03-13 15:30:09 +0100

    a

--- src/main/java/org/opencarto/MainORMGene.java
+++ src/main/java/org/opencarto/MainORMGene.java
@@ -49,41 +49,19 @@ AND "railway" != 'subway'
 		//service? usage? maxspeed-highspeed? electrified-voltage? gauge? railway:traffic_mode? railway:preferred_direction-railway:bidirectional? name-description-ref?
 		//use service! - keep only tracks with service==null (+ voltage!=null ?)
 
+		//target: 1:50k -> Resolution 0.2mm -> 10m
+		
+		
+
+		
 		//TODO define specs based on ORM model and generalisation process in mind (at least ERM specs should be covered)
 		//specs for input dataset (1:5k): tracks selected, with proper attributes, well structured. basic ETL process.
 		//specs for generalised dataset (1:50k)
 		//   main railway lines + railway areas + stations (points and surfaces)
 		//   leveling crossing (points)
-		//target: 1:50k -> Resolution 0.2mm -> 10m
 
 		//see: https://gis.stackexchange.com/questions/20279/calculating-average-width-of-polygon
 
-		//TODO see input ORM data
-		//http://wiki.openstreetmap.org/wiki/OpenRailwayMap
-		//http://wiki.openstreetmap.org/wiki/OpenRailwayMap/Tagging
-		
-		//define filtering based on attribute
-		//Exclude:
-		//NO "railway"=> abandoned razed disused
-		//NO railway:preserved => no
-		//NO historic => railway
-		//"railway"=> subway funicular tram light_rail miniature
-		//"attraction"
-		//usage => test
-
-		//tags helping for generalisation:
-		//usage => main branch industrial military tourism
-		//on not main: service => yard spur siding crossover
-		//railway:track_class => https://en.wikipedia.org/wiki/Rail_speed_limits_in_the_United_States#Track_classes
-		//maxspeed
-
-		//direction: See railway:preferred_direction and railway:bidirectional
-
-		//test connection - build topology
-		//generalisation: derive railway lines from railway tracks
-		//distinct 'main' lines from 'branch' lines. By type? Using stroke?
-		//build railway areas from locals
-		//symplify main lines: collapse dual mainlines, simplify junctions, etc.
 		//algorithm to compute average of two lines, based on curvelinear abscissa
 
 		System.out.println("End");
