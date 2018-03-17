50038f89ee8a4ee4e11e1be0851092c422f75556
julien Gaffuri <julien.gaffuri@gmail.com>
2018-03-17 09:19:32 +0100

    a

--- src/main/java/org/opencarto/MainGISCOGene100k.java
+++ src/main/java/org/opencarto/MainGISCOGene100k.java
@@ -10,7 +10,19 @@ import org.apache.log4j.Logger;
 import org.opencarto.datamodel.Feature;
 import org.opencarto.datamodel.graph.GraphBuilder;
 import org.opencarto.io.SHPUtil;
+import org.opencarto.transfoengine.CartographicResolution;
+import org.opencarto.transfoengine.tesselationGeneralisation.AEdge;
+import org.opencarto.transfoengine.tesselationGeneralisation.AFace;
+import org.opencarto.transfoengine.tesselationGeneralisation.ATesselation;
+import org.opencarto.transfoengine.tesselationGeneralisation.CEdgeFaceSize;
+import org.opencarto.transfoengine.tesselationGeneralisation.CEdgeGranularity;
+import org.opencarto.transfoengine.tesselationGeneralisation.CEdgeTriangle;
+import org.opencarto.transfoengine.tesselationGeneralisation.CEdgeValidity;
+import org.opencarto.transfoengine.tesselationGeneralisation.CFaceSize;
+import org.opencarto.transfoengine.tesselationGeneralisation.CFaceValidity;
+import org.opencarto.transfoengine.tesselationGeneralisation.CTesselationMorphology;
 import org.opencarto.transfoengine.tesselationGeneralisation.DefaultTesselationGeneralisation;
+import org.opencarto.transfoengine.tesselationGeneralisation.TesselationGeneralisationSpecifications;
 
 /**
  * @author julien Gaffuri
@@ -63,7 +75,7 @@ public class MainGISCOGene100k {
 
 		for(int i=1; i<=100; i++) {
 			LOGGER.info("Launch generalisation " + i);
-			units = DefaultTesselationGeneralisation.runGeneralisation(units, DefaultTesselationGeneralisation.defaultSpecs, 1e6, 1, false);
+			units = DefaultTesselationGeneralisation.runGeneralisation(units, specs, 1e6, 1, false);
 
 			LOGGER.info("Run GC");
 			System.gc();
@@ -74,5 +86,36 @@ public class MainGISCOGene100k {
 
 		LOGGER.info("End");
 	}
+
+
+	public static TesselationGeneralisationSpecifications specs = new TesselationGeneralisationSpecifications() {
+		public void setTesselationConstraints(ATesselation t, CartographicResolution res) {
+			t.addConstraint(new CTesselationMorphology(t, res.getSeparationDistanceMeter(), 1e-5));
+		}
+		public void setUnitConstraints(ATesselation t, CartographicResolution res) {
+			/*for(AUnit a : t.aUnits) {
+				//a.addConstraint(new CUnitNoNarrowGaps(a, resolution, 0.1*resSqu, 4).setPriority(10));
+				//a.addConstraint(new ConstraintOneShot<AUnit>(a, new TUnitNarrowGapsFilling(a, resolution, 0.1*resSqu, 4)).setPriority(10));
+			}*/
+		}
+		public void setTopologicalConstraints(ATesselation t, CartographicResolution res) {
+			for(AFace a : t.aFaces) {
+				a.addConstraint(new CFaceSize(a, 0.2*res.getPerceptionSizeSqMeter(), 3*res.getPerceptionSizeSqMeter(), res.getPerceptionSizeSqMeter(), true).setPriority(2));
+				a.addConstraint(new CFaceValidity(a).setPriority(1));
+				a.addConstraint(new CFaceEEZInLand(a).setPriority(10));
+				//a.addConstraint(new CFaceNoSmallHoles(a, resSqu*5).setPriority(3));
+				//a.addConstraint(new CFaceNoEdgeToEdgeIntersection(a, graph.getSpatialIndexEdge()).setPriority(1));
+			}
+			for(AEdge a : t.aEdges) {
+				a.addConstraint(new CEdgeGranularity(a, 2*res.getResolutionM(), true));
+				a.addConstraint(new CEdgeFaceSize(a).setImportance(6));
+				a.addConstraint(new CEdgeValidity(a));
+				a.addConstraint(new CEdgeTriangle(a));
+				//a.addConstraint(new CEdgeSize(a, resolution, resolution*0.6));
+				//a.addConstraint(new CEdgeNoSelfIntersection(a));
+				//a.addConstraint(new CEdgeToEdgeIntersection(a, graph.getSpatialIndexEdge()));
+			}
+		}
+	};
 
 }
--- src/main/java/org/opencarto/MainGISCOGeneXM.java
+++ src/main/java/org/opencarto/MainGISCOGeneXM.java
@@ -8,7 +8,19 @@ import java.util.Collection;
 import org.apache.log4j.Logger;
 import org.opencarto.datamodel.Feature;
 import org.opencarto.io.SHPUtil;
+import org.opencarto.transfoengine.CartographicResolution;
+import org.opencarto.transfoengine.tesselationGeneralisation.AEdge;
+import org.opencarto.transfoengine.tesselationGeneralisation.AFace;
+import org.opencarto.transfoengine.tesselationGeneralisation.ATesselation;
+import org.opencarto.transfoengine.tesselationGeneralisation.CEdgeFaceSize;
+import org.opencarto.transfoengine.tesselationGeneralisation.CEdgeGranularity;
+import org.opencarto.transfoengine.tesselationGeneralisation.CEdgeTriangle;
+import org.opencarto.transfoengine.tesselationGeneralisation.CEdgeValidity;
+import org.opencarto.transfoengine.tesselationGeneralisation.CFaceSize;
+import org.opencarto.transfoengine.tesselationGeneralisation.CFaceValidity;
+import org.opencarto.transfoengine.tesselationGeneralisation.CTesselationMorphology;
 import org.opencarto.transfoengine.tesselationGeneralisation.DefaultTesselationGeneralisation;
+import org.opencarto.transfoengine.tesselationGeneralisation.TesselationGeneralisationSpecifications;
 
 /**
  * @author julien Gaffuri
@@ -17,6 +29,37 @@ import org.opencarto.transfoengine.tesselationGeneralisation.DefaultTesselationG
 public class MainGISCOGeneXM {
 	private final static Logger LOGGER = Logger.getLogger(MainGISCOGeneXM.class.getName());
 
+	public static TesselationGeneralisationSpecifications specs = new TesselationGeneralisationSpecifications() {
+		public void setTesselationConstraints(ATesselation t, CartographicResolution res) {
+			t.addConstraint(new CTesselationMorphology(t, res.getSeparationDistanceMeter(), 1e-5));
+		}
+		public void setUnitConstraints(ATesselation t, CartographicResolution res) {
+			/*for(AUnit a : t.aUnits) {
+				//a.addConstraint(new CUnitNoNarrowGaps(a, resolution, 0.1*resSqu, 4).setPriority(10));
+				//a.addConstraint(new ConstraintOneShot<AUnit>(a, new TUnitNarrowGapsFilling(a, resolution, 0.1*resSqu, 4)).setPriority(10));
+			}*/
+		}
+		public void setTopologicalConstraints(ATesselation t, CartographicResolution res) {
+			for(AFace a : t.aFaces) {
+				a.addConstraint(new CFaceSize(a, 0.2*res.getPerceptionSizeSqMeter(), 3*res.getPerceptionSizeSqMeter(), res.getPerceptionSizeSqMeter(), true).setPriority(2));
+				a.addConstraint(new CFaceValidity(a).setPriority(1));
+				a.addConstraint(new CFaceEEZInLand(a).setPriority(10));
+				//a.addConstraint(new CFaceNoSmallHoles(a, resSqu*5).setPriority(3));
+				//a.addConstraint(new CFaceNoEdgeToEdgeIntersection(a, graph.getSpatialIndexEdge()).setPriority(1));
+			}
+			for(AEdge a : t.aEdges) {
+				a.addConstraint(new CEdgeGranularity(a, 2*res.getResolutionM(), true));
+				a.addConstraint(new CEdgeFaceSize(a).setImportance(6));
+				a.addConstraint(new CEdgeValidity(a));
+				a.addConstraint(new CEdgeTriangle(a));
+				//a.addConstraint(new CEdgeSize(a, resolution, resolution*0.6));
+				//a.addConstraint(new CEdgeNoSelfIntersection(a));
+				//a.addConstraint(new CEdgeToEdgeIntersection(a, graph.getSpatialIndexEdge()));
+			}
+		}
+	};
+
+	
 	public static void main(String[] args) {
 		LOGGER.info("Start");
 
@@ -32,7 +75,7 @@ public class MainGISCOGeneXM {
 
 			for(int i=1; i<=8; i++) {
 				LOGGER.info("Launch generalisation " + i + " for "+((int)s)+"M");
-				units = DefaultTesselationGeneralisation.runGeneralisation(units, DefaultTesselationGeneralisation.defaultSpecs, scaleDenominator, 1, false);
+				units = DefaultTesselationGeneralisation.runGeneralisation(units, specs, scaleDenominator, 1, false);
 
 				LOGGER.info("Run GC");
 				System.gc();
