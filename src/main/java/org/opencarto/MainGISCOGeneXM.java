6a5f19b19480e8f1bb65194d079386832f43bf50
julien Gaffuri <julien.gaffuri@gmail.com>
2018-03-18 21:19:14 +0100

    a

--- src/main/java/org/opencarto/MainGISCOGeneXM.java
+++ src/main/java/org/opencarto/MainGISCOGeneXM.java
@@ -51,6 +51,40 @@ public class MainGISCOGeneXM {
 			for(Feature f : units) for(String id : new String[] {"NUTS_P_ID","NUTS_CODE","COMM_ID","idgene","GISCO_ID"}) if(f.getProperties().get(id) != null) f.id = ""+f.getProperties().get(id);
 
 			for(int i=1; i<=8; i++) {
+
+				LOGGER.info("Launch generalisation " + i + " for "+((int)s)+"M");
+				TesselationGeneralisationSpecifications specs = new TesselationGeneralisationSpecifications() {
+					public void setTesselationConstraints(ATesselation t, CartographicResolution res) {
+						t.addConstraint(new CTesselationMorphology(t, res.getSeparationDistanceMeter(), 1e-5));
+					}
+					public void setUnitConstraints(ATesselation t, CartographicResolution res) {
+						/*for(AUnit a : t.aUnits) {
+							//a.addConstraint(new CUnitNoNarrowGaps(a, resolution, 0.1*resSqu, 4).setPriority(10));
+							//a.addConstraint(new ConstraintOneShot<AUnit>(a, new TUnitNarrowGapsFilling(a, resolution, 0.1*resSqu, 4)).setPriority(10));
+						}*/
+					}
+					public void setTopologicalConstraints(ATesselation t, CartographicResolution res) {
+						for(AFace a : t.aFaces) {
+							a.addConstraint(new CFaceSize(a, 0.2*res.getPerceptionSizeSqMeter(), 3*res.getPerceptionSizeSqMeter(), res.getPerceptionSizeSqMeter(), true).setPriority(2));
+							a.addConstraint(new CFaceValidity(a).setPriority(1));
+							a.addConstraint(new CFaceEEZInLand(a).setPriority(10));
+							//a.addConstraint(new CFaceNoSmallHoles(a, resSqu*5).setPriority(3));
+							//a.addConstraint(new CFaceNoEdgeToEdgeIntersection(a, graph.getSpatialIndexEdge()).setPriority(1));
+						}
+						for(AEdge a : t.aEdges) {
+							a.addConstraint(new CEdgeGranularity(a, 2*res.getResolutionM(), true));
+							a.addConstraint(new CEdgeFaceSize(a).setImportance(6));
+							a.addConstraint(new CEdgeValidity(a));
+							a.addConstraint(new CEdgeTriangle(a));
+							//a.addConstraint(new CEdgeSize(a, resolution, resolution*0.6));
+							//a.addConstraint(new CEdgeNoSelfIntersection(a));
+							//a.addConstraint(new CEdgeToEdgeIntersection(a, graph.getSpatialIndexEdge()));
+						}
+					}
+				};
+
+				
+				
 				LOGGER.info("Launch generalisation " + i + " for "+((int)s)+"M");
 				units = DefaultTesselationGeneralisation.runGeneralisation(units, specs, scaleDenominator, 1, false);
 
@@ -70,39 +104,6 @@ public class MainGISCOGeneXM {
 
 		LOGGER.info("End");
 	}
-
-
-
-
-	public static TesselationGeneralisationSpecifications specs = new TesselationGeneralisationSpecifications() {
-		public void setTesselationConstraints(ATesselation t, CartographicResolution res) {
-			t.addConstraint(new CTesselationMorphology(t, res.getSeparationDistanceMeter(), 1e-5));
-		}
-		public void setUnitConstraints(ATesselation t, CartographicResolution res) {
-			/*for(AUnit a : t.aUnits) {
-				//a.addConstraint(new CUnitNoNarrowGaps(a, resolution, 0.1*resSqu, 4).setPriority(10));
-				//a.addConstraint(new ConstraintOneShot<AUnit>(a, new TUnitNarrowGapsFilling(a, resolution, 0.1*resSqu, 4)).setPriority(10));
-			}*/
-		}
-		public void setTopologicalConstraints(ATesselation t, CartographicResolution res) {
-			for(AFace a : t.aFaces) {
-				a.addConstraint(new CFaceSize(a, 0.2*res.getPerceptionSizeSqMeter(), 3*res.getPerceptionSizeSqMeter(), res.getPerceptionSizeSqMeter(), true).setPriority(2));
-				a.addConstraint(new CFaceValidity(a).setPriority(1));
-				a.addConstraint(new CFaceEEZInLand(a).setPriority(10));
-				//a.addConstraint(new CFaceNoSmallHoles(a, resSqu*5).setPriority(3));
-				//a.addConstraint(new CFaceNoEdgeToEdgeIntersection(a, graph.getSpatialIndexEdge()).setPriority(1));
-			}
-			for(AEdge a : t.aEdges) {
-				a.addConstraint(new CEdgeGranularity(a, 2*res.getResolutionM(), true));
-				a.addConstraint(new CEdgeFaceSize(a).setImportance(6));
-				a.addConstraint(new CEdgeValidity(a));
-				a.addConstraint(new CEdgeTriangle(a));
-				//a.addConstraint(new CEdgeSize(a, resolution, resolution*0.6));
-				//a.addConstraint(new CEdgeNoSelfIntersection(a));
-				//a.addConstraint(new CEdgeToEdgeIntersection(a, graph.getSpatialIndexEdge()));
-			}
-		}
-	};
 
 
 	private static HashMap<String,Collection<Point>> getPoints() {
