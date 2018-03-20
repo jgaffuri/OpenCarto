06b8ab65d21a12e4b6fc5cfd3098d98130adc083
julien Gaffuri <julien.gaffuri@gmail.com>
2018-03-20 15:46:30 +0100

    a

--- src/main/java/org/opencarto/gisco/MainGISCOGene100k.java
+++ src/main/java/org/opencarto/gisco/MainGISCOGene100k.java
@@ -45,13 +45,13 @@ public class MainGISCOGene100k {
 
 		GraphBuilder.LOGGER.setLevel(Level.WARN);
 
+		//TODO area preservation (gibraltar/san marino)
 		//TODO move narrow gap removal at unit level
 		//TODO control point in area
 
 		//TODO gene for web mapping applications
 		//TODO implement narrow corridor removal
 		//TODO removal of large elongated faces/holes: face size constraint: take into account shape - use erosion? use width evaluation method?
-
 		//TODO face collapse algorithm
 		//TODO edge size constraint: fix it!
 		//TODO in graph: connect polygon geometry coordinates to edge & node coordinates?
