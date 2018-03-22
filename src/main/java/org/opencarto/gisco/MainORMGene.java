06fd48eb957f3fe962b2d2c51f5be52f83be7eba
julien Gaffuri <julien.gaffuri@gmail.com>
2018-03-22 21:36:21 +0100

    a

--- src/main/java/org/opencarto/gisco/MainORMGene.java
+++ src/main/java/org/opencarto/gisco/MainORMGene.java
@@ -34,49 +34,13 @@ public class MainORMGene {
 	public static void main(String[] args) {
 		System.out.println("Start");
 
-		//focus on sweden. Focus first on tracks
 		//https://www.openrailwaymap.org/
 		//https://wiki.openstreetmap.org/wiki/OpenRailwayMap/Tagging
 
-		//is it possible to filter OSM based on tagging to obtain something comparable to ERM? -> Yes.
-		//filtering: ambiguity with "abandonned". Choice: remove it. Tramway/subway. Choice: remove it and focus on train.
-		/*
-"railway"!='construction'
-AND "railway"!='dismantled'
-AND "railway"!='elevator'
-AND "railway"!='funicular'
-AND "railway"!='historic'
-AND "railway"!='historic_path'
-AND "railway"!='historical'
-AND "railway" != 'miniature'
-AND "railway"!='planned'
-AND "railway" != 'platform'
-AND "railway" != 'platform_edge'
-AND "railway" != 'proposed'
-AND "railway" != 'razed'
-AND "railway" != 'turntable'
-AND "railway" != 'abandoned'
-AND "railway" != 'tram'
-AND "railway" != 'subway'
-		 */
-		//Conclusion: filtering analysis based on tags. Accept imperfection! Analyse it. Make choices and approximations. OSM has a descriptive approach, while we have functionnal questions.
-
-		//Is it possible to use some tags to select tracks composing main lines ? Approach: examine other tags and make classification
-		//service? usage? maxspeed-highspeed? electrified-voltage? gauge? railway:traffic_mode? railway:preferred_direction-railway:bidirectional? name-description-ref?
-		//use service! - keep only tracks with service==null (+ voltage!=null ?)
-
 		//target: 1:50k -> Resolution 0.2mm -> 10m
-
-		//TODO define specs based on ORM model and generalisation process in mind (at least ERM specs should be covered)
-		//specs for input dataset (1:5k): tracks selected, with proper attributes, well structured. basic ETL process.
 		//specs for generalised dataset (1:50k)
 		//   main railway lines + railway areas + stations (points and surfaces)
 		//   leveling crossing (points)
-
-		//make generalisation on graph
-		//collapse too short faces/edges
-		//algorithm to compute average of two lines, based on curvelinear abscissa
-
 
 		LOGGER.info("Load input tracks");
 		String basePath = "/home/juju/Bureau/gisco_rail/";
@@ -146,40 +110,10 @@ AND "railway" != 'subway'
 		//TODO compute nodes centrality?
 		//TODO narrow face collapse algorithm - with triangulation? - collapse cases when only 2 limit sections?
 		//TODO generate areas of service lines
+		//collapse too short faces/edges
+		//algorithm to compute average of two lines, based on curvelinear abscissa
 
 		System.out.println("End");
 	}
-
-	/*
-ERM specifications - railways
-
---- Railway station
-
-type - railway station, joint railway station, halt, marshalling yard, intermodal rail transport, terminal
-name
-railway_station_identifier
-use - cargo/freight, passenger, general
-
---- Railway section
-
-name
-railway_code
-existence - operational, abandoned/disused, under construction
-category - main line, branch line
-use - cargo/freight, passenger, general
-nb_tracks - single, double, multiple, juxtaposition
-gauge_cm
-gauge - normal, braod, narrow
-power_source - electrified track, overhead electrified, non-electrified
-speed_class - conventionnal, upgraded high speed (<250), dedicated high speed (>250)
-level - -9,-3,-2,-1,0,1,2,3,9
-seasonality - all year, seasonal
-TEN - part of, not part of
-length_m
-
---- Railway network link
-No attribute
-
-	 */
 
 }
