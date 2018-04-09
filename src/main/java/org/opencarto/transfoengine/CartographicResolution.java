c60b2803ea7c31a3664aa4a12fd070407e6e9876
julien Gaffuri <julien.gaffuri@gmail.com>
2018-04-09 20:37:14 +0200

    a

--- src/main/java/org/opencarto/transfoengine/CartographicResolution.java
+++ src/main/java/org/opencarto/transfoengine/CartographicResolution.java
@@ -35,6 +35,7 @@ public class CartographicResolution {
 	 * @param scaleDenominator The scale denominator. Ex: 1e6 for 1:1M scale.
 	 */
 	public CartographicResolution(double scaleDenominator, CRSType type) {
+
 		//resolution is 0.1mm map. 0.1mm at 1:1M -> 1e-4*1e6 = 1e2 = 100m
 		if(type == CRSType.CARTO) resolutionM = scaleDenominator*1e-4;
 		else resolutionM = scaleDenominator*1e-4 / ProjectionUtil.ED;
