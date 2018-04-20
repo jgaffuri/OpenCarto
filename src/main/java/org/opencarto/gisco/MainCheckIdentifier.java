3e0db752f0d235b291019d711a661aee6917b772
julien Gaffuri <julien.gaffuri@gmail.com>
2018-04-20 11:12:17 +0200

    a

---
+++ src/main/java/org/opencarto/gisco/MainCheckIdentifier.java
@@ -0,0 +1,26 @@
+package org.opencarto.gisco;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+import org.apache.log4j.Logger;
+import org.opencarto.datamodel.Feature;
+import org.opencarto.io.SHPUtil;
+import org.opencarto.util.FeatureUtil;
+
+public class MainCheckIdentifier {
+	private final static Logger LOGGER = Logger.getLogger(MainCheckIdentifier.class.getName());
+
+	public static void main(String[] args) {
+		LOGGER.info("Start");
+
+		LOGGER.info("Load data");
+		Collection<Feature> units = SHPUtil.loadSHP("/home/juju/Bureau/nuts_gene_data/nutsplus/NUTS_PLUS_01M_1904.shp").fs;
+		LOGGER.info("Check id");
+		HashMap<String, Integer> ids = FeatureUtil.checkIdentfier(units, "id");
+		System.out.println(ids);
+
+		LOGGER.info("End");
+	}
+
+}
