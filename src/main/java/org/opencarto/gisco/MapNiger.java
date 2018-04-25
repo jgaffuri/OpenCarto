31d13b0f0a10504aea3c0ed38f4b5a6713a69d71
julien Gaffuri <julien.gaffuri@gmail.com>
2018-04-25 11:56:15 +0200

    a

---
+++ src/main/java/org/opencarto/gisco/MapNiger.java
@@ -0,0 +1,39 @@
+package org.opencarto.gisco;
+
+import java.util.Collection;
+
+import org.apache.log4j.Logger;
+import org.opencarto.datamodel.Feature;
+import org.opencarto.io.SHPUtil;
+import org.opencarto.transfoengine.tesselationGeneralisation.TesselationQuality;
+import org.opencarto.util.JTSGeomUtil;
+
+import com.vividsolutions.jts.geom.Envelope;
+
+public class MapNiger {
+	private final static Logger LOGGER = Logger.getLogger(MapNiger.class.getName());
+
+	public static void main(String[] args) {
+		System.out.println("Start");
+
+		String basePath = "/home/juju/Bureau/niger/data/";
+
+		LOGGER.info("Load data");
+		String inFile = basePath+"commune_niger.shp";
+		//String inFile = basePath+"test/testQ.shp";
+		Collection<Feature> units = SHPUtil.loadSHP(inFile).fs;
+		for(Feature f : units) f.id = ""+f.getProperties().get("CODECOMMUN");
+
+		LOGGER.info("Fix quality");
+		double eps = 1e-9;
+		units = TesselationQuality.fixQuality(units, new Envelope(-180+eps, 180-eps, -90+eps, 90-eps), 1e-7, 3000000, 15000);
+
+		LOGGER.info("Save");
+		for(Feature f : units) f.setGeom(JTSGeomUtil.toMulti(f.getGeom()));
+		SHPUtil.saveSHP(units, basePath+"commune_niger_fix.shp", SHPUtil.getCRS(inFile));
+		//SHPUtil.saveSHP(fs, basePath+"test/", "testQ_clean.shp", SHPUtil.getCRS(inFile));
+
+		System.out.println("End");
+	}
+
+}
