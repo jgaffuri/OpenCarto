59bae9b8b76ce2717411a78be1035d238967d0b6
julien Gaffuri <julien.gaffuri@gmail.com>
2018-05-17 14:11:45 +0200

    a

--- src/main/java/org/opencarto/algo/graph/stroke/StrokeAnalysis.java
+++ src/main/java/org/opencarto/algo/graph/stroke/StrokeAnalysis.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package org.opencarto.algo.graph;
+package org.opencarto.algo.graph.stroke;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -18,8 +18,6 @@ import org.opencarto.datamodel.graph.Edge;
 import org.opencarto.datamodel.graph.Graph;
 import org.opencarto.datamodel.graph.Node;
 
-import com.vividsolutions.jts.algorithm.Angle;
-import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 
 /**
@@ -111,39 +109,6 @@ public class StrokeAnalysis {
 		}
 	}
 
-	public class StrokeConnectionSalienceComputation{
-		double computeSalience(StrokeConnection sc) {
-			//compute deflation angle indicator
-			double sal = getDeflationIndicator(sc.n, sc.e1, sc.e2);
-			//TODO compute salience also based on attributes of feature + other? length?
-			Feature f1 = (Feature) sc.e1.obj;
-			Feature f2 = (Feature) sc.e2.obj;
-			return sal;
-		};
-		//between 0 (worst case) to 1 (perfect, no deflation)
-		final double getDeflationIndicator(Node n, Edge e1, Edge e2) {
-			Coordinate c = n.getC();
-			Coordinate c1 = getCoordinateForDeflation(e1,n);
-			Coordinate c2 = getCoordinateForDeflation(e2,n);
-			double ang = Angle.angleBetween(c1, c, c2);
-			//ang between 0 and Pi
-			if(ang<0 || ang>Math.PI)
-				LOGGER.warn("Unexpected deflection angle value around "+c+". Should be within [0,Pi]. "+ang);
-			return ang / Math.PI;
-		}
-
-		final Coordinate getCoordinateForDeflation(Edge e, Node n) {
-			Coordinate c = null;
-			Coordinate[] cs = e.getCoords();
-			if(n.getC().distance(cs[0]) == 0)
-				c = cs[1];
-			else if(n.getC().distance(cs[cs.length-1]) == 0)
-				c = cs[cs.length-2];
-			else
-				LOGGER.warn("Could not getCoordinateForDeflation around "+n.getC());
-			return c;
-		}
-	}
 
 
 	//get all possible connections, which have a deflection angle smaller than a max value
