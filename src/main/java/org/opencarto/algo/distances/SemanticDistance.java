1e33cacdfb0ac2673acb2b5c3d479ca34bd6f281
julien Gaffuri <julien.gaffuri@gmail.com>
2018-05-18 10:59:02 +0200

    a

--- src/main/java/org/opencarto/algo/distances/SemanticDistance.java
+++ src/main/java/org/opencarto/algo/distances/SemanticDistance.java
@@ -13,7 +13,8 @@ public class SemanticDistance implements Distance<Feature> {
 
 	@Override
 	public double get(Feature f1, Feature f2) {
-		// TODO Auto-generated method stub
+		
+		
 		return 0;
 	}
 
