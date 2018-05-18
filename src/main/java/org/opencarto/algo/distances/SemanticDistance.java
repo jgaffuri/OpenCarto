df43766f9ff75ff33d81b2f21f102d0a26f37643
julien Gaffuri <julien.gaffuri@gmail.com>
2018-05-18 11:06:26 +0200

    a

--- src/main/java/org/opencarto/algo/distances/SemanticDistance.java
+++ src/main/java/org/opencarto/algo/distances/SemanticDistance.java
@@ -3,6 +3,9 @@
  */
 package org.opencarto.algo.distances;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 import org.opencarto.datamodel.Feature;
 
 /**
@@ -10,16 +13,28 @@ import org.opencarto.datamodel.Feature;
  *
  */
 public class SemanticDistance implements Distance<Feature> {
-	
+
 	/* (non-Javadoc)
 	 * @see org.opencarto.algo.distances.Distance#get(java.lang.Object, java.lang.Object)
 	 */
 	@Override
 	public double get(Feature f1, Feature f2) {
 		//compare the attribute + values of the two features
-		
-		
-		return 0;
+
+		//get all attribute keys
+		Collection<String> keys = new HashSet<>();
+		keys.addAll(f1.getProperties().keySet());
+		keys.addAll(f2.getProperties().keySet());
+
+		if(keys.size() == 0) return 0;
+
+		int nbCommon = 0;
+		for(String key : keys) {
+			Object v1 = f1.get(key), v2 = f2.get(key);
+			if((v1==null && v2==null)||v1.equals(v2)) nbCommon++;
+		}
+
+		return 1.0-nbCommon/keys.size();
 	}
 
 }
