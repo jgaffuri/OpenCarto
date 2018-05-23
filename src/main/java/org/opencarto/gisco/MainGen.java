205c6a5167765524abf2bfaa8f07b480c0dc60a0
julien Gaffuri <julien.gaffuri@gmail.com>
2018-05-23 10:00:17 +0200

    a

--- src/main/java/org/opencarto/gisco/MainGen.java
+++ src/main/java/org/opencarto/gisco/MainGen.java
@@ -10,6 +10,7 @@ import org.opencarto.datamodel.Feature;
 import org.opencarto.io.SHPUtil;
 import org.opencarto.transfoengine.tesselationGeneralisation.TesselationGeneralisation;
 import org.opencarto.transfoengine.tesselationGeneralisation.TesselationQuality;
+import org.opencarto.util.FeatureUtil;
 import org.opencarto.util.ProjectionUtil.CRSType;
 
 /**
@@ -26,10 +27,15 @@ public class MainGen {
 		String in = basePath+"SETTLEMENT_A_100K.shp";
 		String pattern = "SETTLEMENT_A_";
 		int roundNb = 5;
+		int maxCoordinatesNumber = 50000;
+		int objMaxCoordinateNumber = 50000;
 
 		CRSType crsType = SHPUtil.getCRSType(in);
 
-		TesselationQuality.checkQuality(SHPUtil.loadSHP(in).fs, 1e-6, basePath + "qc.csv", true, 30000000, 150000, true);
+		//LOGGER.info("Check quality");
+		//TesselationQuality.checkQuality(SHPUtil.loadSHP(in).fs, 1e-6, basePath + "qc.csv", true, maxCoordinatesNumber, objMaxCoordinateNumber, true);
+		//LOGGER.info("Check identifier");
+		//FeatureUtil.checkIdentfier(SHPUtil.loadSHP(in).fs, "ID");
 
 		//for(double s : new double[]{1,3,10,20,60}) {
 		for(double s : new double[]{1}) {
@@ -38,11 +44,11 @@ public class MainGen {
 			LOGGER.info("Load data for "+((int)s)+"M generalisation");
 			Collection<Feature> units = SHPUtil.loadSHP(in).fs;
 
-			//LOGGER.info("Fix quality");
-			//units = TesselationQuality.fixQuality(units, null, 1e-7, 30000000, 150000);
+			LOGGER.info("Fix quality");
+			units = TesselationQuality.fixQuality(units, null, 1e-7, maxCoordinatesNumber, objMaxCoordinateNumber);
 
 			LOGGER.info("Launch generalisation for "+((int)s)+"M");
-			units = TesselationGeneralisation.runGeneralisation(units, null, crsType, scaleDenominator, roundNb, 10000000, 10000);
+			units = TesselationGeneralisation.runGeneralisation(units, null, crsType, scaleDenominator, roundNb, maxCoordinatesNumber, objMaxCoordinateNumber);
 
 			LOGGER.info("Save output data");
 			SHPUtil.saveSHP(units, basePath + pattern+((int)s)+"M.shp", SHPUtil.getCRS(in));
