1a7febdfacea36fbde44ba4a13c2b4993f861644
julien Gaffuri <julien.gaffuri@gmail.com>
2018-05-23 09:12:21 +0200

    a

--- src/main/java/org/opencarto/gisco/MainGen.java
+++ src/main/java/org/opencarto/gisco/MainGen.java
@@ -25,28 +25,29 @@ public class MainGen {
 		String basePath = "/home/juju/Bureau/nuts_gene_data/sett/";
 		String in = basePath+"SETTLEMENT_A_100K.shp";
 		String pattern = "SETTLEMENT_A_";
+		int roundNb = 5;
 
-		TesselationQuality.checkQuality(SHPUtil.loadSHP(in).fs, 1e-6, basePath + "qc.csv", true, 30000000, 150000, false);
-/*
+		CRSType crsType = SHPUtil.getCRSType(in);
+
+		TesselationQuality.checkQuality(SHPUtil.loadSHP(in).fs, 1e-6, basePath + "qc.csv", true, 30000000, 150000, true);
+
 		//for(double s : new double[]{1,3,10,20,60}) {
 		for(double s : new double[]{1}) {
 			double scaleDenominator = s*1e6;
 
 			LOGGER.info("Load data for "+((int)s)+"M generalisation");
 			Collection<Feature> units = SHPUtil.loadSHP(in).fs;
-			CRSType crsType = SHPUtil.getCRSType(in);
 
 			//LOGGER.info("Fix quality");
 			//units = TesselationQuality.fixQuality(units, null, 1e-7, 30000000, 150000);
 
 			LOGGER.info("Launch generalisation for "+((int)s)+"M");
-			int roundNb = 5;
 			units = TesselationGeneralisation.runGeneralisation(units, null, crsType, scaleDenominator, roundNb, 10000000, 10000);
 
 			LOGGER.info("Save output data");
 			SHPUtil.saveSHP(units, basePath + pattern+((int)s)+"M.shp", SHPUtil.getCRS(in));
 		}
-*/
+
 		LOGGER.info("End");
 	}
 
