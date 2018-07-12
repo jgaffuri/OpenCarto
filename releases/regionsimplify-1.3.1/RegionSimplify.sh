89fe96dde53f2643a9e433fae21296648753ce20
julien Gaffuri <julien.gaffuri@gmail.com>
2018-07-12 10:57:32 +0200

    script extension

--- releases/regionsimplify-1.3.1/RegionSimplify.sh
+++ releases/regionsimplify-1.3.1/RegionSimplify.sh
@@ -3,9 +3,9 @@
 
 mkdir -p test_out
 
-for dataset in "bangladesh" "chile" "china_mainland"
+for dataset in "bangladesh" "chile" "china_mainland" "indonesia" "panama" "philippines"
 do
-	for scaleM in "5"
+	for scaleM in "5" "10"
 	do
     	echo "Generalisation for "$dataset" - 1:"$scaleM"000000"
 		java -jar RegionSimplify.jar -i "test_data/"$dataset".shp" -o "test_out/"$dataset"-"$scaleM"M.shp" -s $scaleM"000000"
