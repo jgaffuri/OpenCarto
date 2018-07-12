e576402866999e2bb5446eaebf2c2b3bc84f7e95
julien Gaffuri <julien.gaffuri@gmail.com>
2018-07-12 10:56:25 +0200

    a

--- releases/regionsimplify-1.3.1/RegionSimplify.sh
+++ releases/regionsimplify-1.3.1/RegionSimplify.sh
@@ -8,6 +8,6 @@ do
 	for scaleM in "5"
 	do
     	echo "Generalisation for "$dataset" - 1:"$scaleM"000000"
-		java -jar RegionSimplify.jar -i test_data/$dataset.shp -o test_out/$dataset-$scaleMM.shp -s $scaleM000000
+		java -jar RegionSimplify.jar -i "test_data/"$dataset".shp" -o "test_out/"$dataset"-"$scaleM"M.shp" -s $scaleM"000000"
 	done
 done
