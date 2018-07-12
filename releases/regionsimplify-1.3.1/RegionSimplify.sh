684ba3a16c5a4e2a41132e45a7f725860e44f266
julien Gaffuri <julien.gaffuri@gmail.com>
2018-07-12 10:43:59 +0200

    script improvement

--- releases/regionsimplify-1.3.1/RegionSimplify.sh
+++ releases/regionsimplify-1.3.1/RegionSimplify.sh
@@ -1,6 +1,7 @@
-#java -jar RegionSimplify.jar -i test_data/bangladesh.shp
-java -jar RegionSimplify.jar -i test_data/chile.shp -o test_out/chile.shp -s 2000000
-#java -jar RegionSimplify.jar -i test_data/china_mainland.shp
-#java -jar RegionSimplify.jar -i test_data/indonesia.shp
-#java -jar RegionSimplify.jar -i test_data/panama.shp
-#java -jar RegionSimplify.jar -i test_data/philippines.shp
+#!/usr/bin/env bash
+
+for dataset in "bangladesh" "chile" "china_mainland"
+do
+    echo "Generalisation for - "$dataset
+	java -jar RegionSimplify.jar -i test_data/$dataset.shp -o test_out/$dataset.shp -s 2000000
+done
