bcb2200cc16df391a19cecc096db616447d3ef27
julien Gaffuri <julien.gaffuri@gmail.com>
2018-07-12 10:49:11 +0200

    improve script example

--- releases/regionsimplify-1.3.1/RegionSimplify.sh
+++ releases/regionsimplify-1.3.1/RegionSimplify.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+
+mkdir -p test_out
+
 for dataset in "bangladesh" "chile" "china_mainland"
 do
-    echo "Generalisation for - "$dataset
-	java -jar RegionSimplify.jar -i test_data/$dataset.shp -o test_out/$dataset.shp -s 2000000
+	for scaleM in "5"
+	do
+    	echo "Generalisation for "$dataset" - 1:"$scaleM"000000"
+		java -jar RegionSimplify.jar -i test_data/$dataset.shp -o test_out/$dataset-$scaleMM.shp -s $scaleM000000
+	done
 done
