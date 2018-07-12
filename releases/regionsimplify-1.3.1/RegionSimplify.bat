1bc706346688b3296d0a0441d4d47db416dece18
julien Gaffuri <julien.gaffuri@gmail.com>
2018-07-12 14:36:57 +0200

    update script

--- releases/regionsimplify-1.3.1/RegionSimplify.bat
+++ releases/regionsimplify-1.3.1/RegionSimplify.bat
@@ -1,1 +1,3 @@
-java -jar RegionSimplify.jar -i test_data/chile.shp
+mkdir test_out
+java -jar RegionSimplify.jar -i test_data/chile.shp -o test_out/chile-5M.shp -s 5000000
+java -jar RegionSimplify.jar -i test_data/chile.shp -o test_out/chile-10M.shp -s 10000000
