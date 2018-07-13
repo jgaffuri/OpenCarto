96dd2a0e92f6d7faffdc5f2dec3f42bf149f4c89
julien Gaffuri <julien.gaffuri@gmail.com>
2018-07-13 12:15:25 +0200

    a

--- releases/regionsimplify-1.3.1/RegionSimplify.bat
+++ releases/regionsimplify-1.3.1/RegionSimplify.bat
@@ -1,3 +1,3 @@
 mkdir test_out
-java -jar RegionSimplify.jar -i test_data/chile.shp -o test_out/chile-5M.shp -s 5000000
-java -jar RegionSimplify.jar -i test_data/chile.shp -o test_out/chile-10M.shp -s 10000000
+java -jar RegionSimplify.jar -i test_data/bangladesh.shp -o test_out/bangladesh-5M.shp -s 5000000
+java -jar RegionSimplify.jar -i test_data/bangladesh.shp -o test_out/bangladesh-10M.shp -s 10000000
