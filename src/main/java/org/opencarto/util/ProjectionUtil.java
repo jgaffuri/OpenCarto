3b44e28a477aa0b70a1761ac4770ddf1c6d300c6
jgaffuri <jgaf@protonmail.com>
2018-12-06 21:32:02 +0100

    distance computation from geographical coordinates

--- src/main/java/org/opencarto/util/ProjectionUtil.java
+++ src/main/java/org/opencarto/util/ProjectionUtil.java
@@ -11,16 +11,12 @@ import javax.measure.unit.Unit;
 import org.apache.log4j.Logger;
 import org.geotools.geometry.jts.JTS;
 import org.geotools.referencing.CRS;
-import org.geotools.referencing.GeodeticCalculator;
 import org.geotools.resources.CRSUtilities;
 import org.opencarto.datamodel.Feature;
 import org.opengis.referencing.ReferenceIdentifier;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
-import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.LineString;
-import com.vividsolutions.jts.geom.MultiLineString;
 
 /**
  * Basic conversion functions for mercator projection.
@@ -373,47 +369,5 @@ public class ProjectionUtil {
 		}
 	}
 	 */
-
-
-
-
-	public static double getDistanceKM(double slon, double slat, double dlon, double dlat){
-		GeodeticCalculator gc = new GeodeticCalculator();
-		gc.setStartingGeographicPoint(slon, slat);
-		gc.setDestinationGeographicPoint(dlon, dlat);
-		return gc.getOrthodromicDistance() * 0.001;
-	}
-
-	//in km
-	public static double getDistanceKM(Coordinate c1, Coordinate c2) {
-		return getDistanceKM(c1.x, c1.y ,c2.x, c2.y);
-	}
-
-	//in km
-	public static double getLengthGeoKM(Geometry g) {
-		if(g instanceof LineString) return getLengthGeoKM((LineString)g);
-		if(g instanceof MultiLineString) return getLengthGeoKM((MultiLineString)g);
-		System.err.println("getLengthGeo not implemented for geometry type "+g.getGeometryType());
-		return -1;
-	}
-	//in km
-	public static double getLengthGeoKM(MultiLineString mls){
-		double dist = 0;
-		for(int i=0; i<mls.getNumGeometries(); i++)
-			dist += getLengthGeoKM( (LineString) mls.getGeometryN(i) );
-		return dist;
-	}
-	//in km
-	public static double getLengthGeoKM(LineString ls){
-		Coordinate[] cs = ls.getCoordinates();
-		Coordinate c1=cs[0],c2;
-		double dist=0;
-		for(int i=1;i<cs.length;i++){
-			c2=cs[i];
-			dist+=getDistanceKM(c1.x, c1.y ,c2.x, c2.y);
-			c1=c2;
-		}
-		return dist;
-	}
 
 }
