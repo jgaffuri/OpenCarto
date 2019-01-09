5cbd2c39a05de47c1695a8122816fe050625289a
jgaffuri <jgaf@protonmail.com>
2019-01-09 09:21:34 +0100

    a

--- src/main/java/org/opencarto/gisco/MainGen.java
+++ src/main/java/org/opencarto/gisco/MainGen.java
@@ -22,9 +22,8 @@ public class MainGen {
 	public static void main(String[] args) {
 		LOGGER.info("Start");
 
-		String basePath = "/home/juju/Bureau/nuts_gene_data/sett/";
-		String in = basePath+"SETTLEMENT_A_100K.shp";
-		String pattern = "SETTLEMENT_A_";
+		String in = "/home/juju/Bureau/nuts_gene_data/sett/SETTLEMENT_A.shp";
+		String out = "/home/juju/Bureau/nuts_gene_data/sett/SETTLEMENT_A_1M.shp";
 		int roundNb = 5;
 		int maxCoordinatesNumber = 50000;
 		int objMaxCoordinateNumber = 15000;
@@ -36,22 +35,19 @@ public class MainGen {
 		//LOGGER.info("Check identifier");
 		//FeatureUtil.checkIdentfier(SHPUtil.loadSHP(in).fs, "ID");
 
-		//for(double s : new double[]{1,3,10,20,60}) {
-		for(double s : new double[]{1}) {
-			double scaleDenominator = s*1e6;
+		double scaleDenominator = 1.0*1e6;
 
-			LOGGER.info("Load data for "+((int)s)+"M generalisation");
-			Collection<Feature> units = SHPUtil.loadSHP(in).fs;
+		LOGGER.info("Load data from "+in);
+		Collection<Feature> units = SHPUtil.loadSHP(in).fs;
 
-			LOGGER.info("Fix quality");
-			units = TesselationQuality.fixQuality(units, null, 1e-7, maxCoordinatesNumber, objMaxCoordinateNumber, false);
+		LOGGER.info("Fix quality");
+		units = TesselationQuality.fixQuality(units, null, 1e-7, maxCoordinatesNumber, objMaxCoordinateNumber, false);
 
-			LOGGER.info("Launch generalisation for "+((int)s)+"M");
-			units = TesselationGeneralisation.runGeneralisation(units, null, crsType, scaleDenominator, roundNb, maxCoordinatesNumber, objMaxCoordinateNumber);
+		LOGGER.info("Launch generalisation");
+		units = TesselationGeneralisation.runGeneralisation(units, null, crsType, scaleDenominator, roundNb, maxCoordinatesNumber, objMaxCoordinateNumber);
 
-			LOGGER.info("Save output data");
-			SHPUtil.saveSHP(units, basePath + pattern+((int)s)+"M.shp", SHPUtil.getCRS(in));
-		}
+		LOGGER.info("Save output data in "+out);
+		SHPUtil.saveSHP(units, out, SHPUtil.getCRS(in));
 
 		LOGGER.info("End");
 	}
