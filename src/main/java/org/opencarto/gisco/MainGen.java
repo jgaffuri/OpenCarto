a154b266344f907b468310b59d1645faff84472f
jgaffuri <jgaf@protonmail.com>
2019-01-15 11:52:19 +0100

    a

--- src/main/java/org/opencarto/gisco/MainGen.java
+++ src/main/java/org/opencarto/gisco/MainGen.java
@@ -8,9 +8,8 @@ import java.util.Collection;
 import org.apache.log4j.Logger;
 import org.opencarto.datamodel.Feature;
 import org.opencarto.io.SHPUtil;
-import org.opencarto.transfoengine.tesselationGeneralisation.TesselationQuality;
-
-import com.vividsolutions.jts.geom.Envelope;
+import org.opencarto.transfoengine.tesselationGeneralisation.TesselationGeneralisation;
+import org.opencarto.util.ProjectionUtil.CRSType;
 
 /**
  * @author julien Gaffuri
@@ -23,13 +22,14 @@ public class MainGen {
 		LOGGER.info("Start");
 
 		String basePath = "/home/juju/Bureau/GLOBAL_ADMIN_AREAS/";
-		String in = basePath+"GLOBAL_ADMIN_AREAS.shp";
+		String in = basePath+"GLOBAL_ADMIN_AREAS_clean.shp";
 		String out = basePath+"GLOBAL_ADMIN_AREAS_1M.shp";
 		String idCol = "ID_";
 		boolean tracePartitionning = true;
 
 		int maxCoordinatesNumber = 1000000;
 		int objMaxCoordinateNumber = 1000;
+
 
 
 
@@ -51,17 +51,18 @@ public class MainGen {
 
 		//quality correction
 
+		/*
 		LOGGER.info("Fix quality");
 		double eps = 1e-9; Envelope env = new Envelope(-180+eps, 180-eps, -90+eps, 90-eps);
 		units = TesselationQuality.fixQuality(units, env, 1e-7, maxCoordinatesNumber, objMaxCoordinateNumber, tracePartitionning);
 
 		LOGGER.info("Save output data in "+out);
 		SHPUtil.saveSHP(units, basePath+"GLOBAL_ADMIN_AREAS_clean.shp", SHPUtil.getCRS(in));
-
+*/
 
 		//generalisation
 
-		/*
+		
 		LOGGER.info("Launch generalisation");
 		double scaleDenominator = 1.0*1e6;
 		int roundNb = 5;
@@ -70,7 +71,7 @@ public class MainGen {
 
 		LOGGER.info("Save output data in "+out);
 		SHPUtil.saveSHP(units, out, SHPUtil.getCRS(in));
-		 */
+		 
 
 
 		LOGGER.info("End");
