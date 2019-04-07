19a5f340dbf093c590ef9a0e7ff31d58f98a4a37
jgaffuri <jgaf@protonmail.com>
2019-04-07 21:02:52 +0200

    improve todo

--- src/main/java/org/opencarto/gisco/rail/MainRailwayEdgeMatching.java
+++ src/main/java/org/opencarto/gisco/rail/MainRailwayEdgeMatching.java
@@ -30,6 +30,12 @@ public class MainRailwayEdgeMatching {
 
 	public static void main(String[] args) throws Exception {
 
+		//TODO: get OSM, for comparision
+		//TODO: improve union: get attributes + simple geometries (not multilinestring like in italy)
+		//TODO: when section is similar to another one (min dist + hausdorf small), remove the one which is not from the country.
+		//TODO: remove sections not in their countries, far and not connected (?)
+		//TODO: decompose sections with extreme points of sections of other countries. Then link them. Remove similar sections (joining same nodes) from diff countries.
+
 		//resolution data
 		HashMap<String,Double> resolutions = new HashMap<String,Double>();
 		resolutions.put("LU", 1.5);
@@ -44,14 +50,14 @@ public class MainRailwayEdgeMatching {
 		resolutions.put("DE", 12.0);
 		resolutions.put("PT", 700.0);
 
-		System.out.println("Load input tracks");
+		System.out.println("Load input sections");
 		String basePath = "/home/juju/Bureau/gisco_rail/";
 		Filter f = CQL.toFilter( "CNTR = 'LU' OR CNTR = 'BE' OR CNTR = 'AT' OR CNTR = 'NL' OR CNTR = 'FR' OR CNTR = 'ES' OR CNTR = 'CH' OR CNTR = 'PL' OR CNTR = 'DE' OR CNTR = 'PT'" );
-		ArrayList<Feature> tracks = SHPUtil.loadSHP(basePath+"in/RailwayLink.shp", f).fs;
-		System.out.println(tracks.size());
+		ArrayList<Feature> secs = SHPUtil.loadSHP(basePath+"in/RailwayLink.shp", f).fs;
+		System.out.println(secs.size());
 
 		System.out.println("Ensure input geometries are simple");
-		for(Feature t : tracks) {
+		for(Feature t : secs) {
 			MultiLineString mls = (MultiLineString) t.getGeom();
 			if(mls.getNumGeometries() != 1) System.err.println("Input geometries should be simple linestrings. nb=" + mls.getNumGeometries() + " CNTR=" + t.getProperties().get("CNTR"));
 			t.setGeom( (LineString)mls.getGeometryN(0) );
@@ -59,7 +65,7 @@ public class MainRailwayEdgeMatching {
 
 		System.out.println("Build spatial index");
 		Quadtree si = new Quadtree();
-		for(Feature t : tracks) si.insert(t.getGeom().getEnvelopeInternal(), t);
+		for(Feature c : secs) si.insert(c.getGeom().getEnvelopeInternal(), c);
 
 		System.out.println("Get list of countries ordered by resolution");
 		ArrayList<String> cnts = new ArrayList<String>();
@@ -68,80 +74,81 @@ public class MainRailwayEdgeMatching {
 			@Override
 			public int compare(String cnt1, String ctn2) { return (int)(10000000 * (resolutions.get(ctn2) - resolutions.get(cnt1))); }
 		});
+		System.out.println(cnts);
 
-		System.out.println("Set EM tag");
-		for(Feature t : tracks) t.getProperties().put("EM", "No");
+		System.out.println("Initialise EM tag");
+		for(Feature s : secs) s.getProperties().put("EM", "no");
 
-		System.out.println("Go through countries, starting with the one with the largest resolution");
+		System.out.println("Go through countries, starting with the one with the most detailled resolution");
 		for(String cnt : cnts) {
-			//get all tracks of the cnt
-			ArrayList<Feature> tracksCnt = new ArrayList<Feature>();
-			for(Feature t : tracks) if(t.getProperties().get("CNTR").equals(cnt)) tracksCnt.add(t);
+			//get all sections of the cnt
+			ArrayList<Feature> secCnt = new ArrayList<Feature>();
+			for(Feature s : secs) if(s.getProperties().get("CNTR").equals(cnt)) secCnt.add(s);
 
 			double res = resolutions.get(cnt);
-			System.out.println(cnt + " - res=" + res + " - nb=" + tracksCnt.size());
+			System.out.println(cnt + " - res=" + res + " - nb=" + secCnt.size());
 
-			for(Feature t : tracksCnt) {
+			for(Feature s : secCnt) {
 
-				//get all tracks that are 'nearby'
-				Envelope env = t.getGeom().getEnvelopeInternal(); env.expandBy(res*1.01);
+				//get all sections that are 'nearby'
+				Envelope env = s.getGeom().getEnvelopeInternal(); env.expandBy(res*1.01);
 
 				List<?> ts_ = si.query(env); //TODO order?
-				for(Object t2 : ts_) {
-					Feature t_ = (Feature) t2;
-					if(t == t_) continue;
-					if(t.getGeom().isEmpty()) continue;
+				for(Object s2 : ts_) {
+					Feature s_ = (Feature) s2;
+					if(s == s_) continue;
+					if(s.getGeom().isEmpty()) continue;
 
-					if(t_.getProperties().get("CNTR").equals(cnt)) continue;
-					if(res < resolutions.get(t_.getProperties().get("CNTR"))) continue;
-					if(areConnected( (LineString)t.getGeom(), (LineString)t_.getGeom())) continue;
-					if(! t_.getGeom().getEnvelopeInternal().intersects(env)) continue;
-					if(t.getGeom().distance(t_.getGeom()) > res*1.01) continue;
+					if(s_.getProperties().get("CNTR").equals(cnt)) continue;
+					if(res < resolutions.get(s_.getProperties().get("CNTR"))) continue;
+					if(areConnected( (LineString)s.getGeom(), (LineString)s_.getGeom())) continue;
+					if(! s_.getGeom().getEnvelopeInternal().intersects(env)) continue;
+					if(s.getGeom().distance(s_.getGeom()) > res*1.01) continue;
 
 					//tag the section
-					t.getProperties().put("EM", "Involved");
+					s.getProperties().put("EM", "involved");
 
 					//remove t geometry part with t_
-					Geometry buff = t_.getGeom().buffer(res);
-					Geometry gDiff = t.getGeom().difference(buff);
+					Geometry buff = s_.getGeom().buffer(res);
+					Geometry gDiff = s.getGeom().difference(buff);
 
 					//if nothing left, remove it
 					if(gDiff.isEmpty()) {
-						si.remove(t.getGeom().getEnvelopeInternal(), t);
-						t.setGeom(gDiff);
-						tracks.remove(t);
+						si.remove(s.getGeom().getEnvelopeInternal(), s);
+						s.setGeom(gDiff);
+						secs.remove(s);
 						continue;
 					}
 
 					if(!(gDiff instanceof LineString)) continue; //TODO handle that case !
 
 					//if t geometry has changed
-					if(gDiff.getLength() != t.getGeom().getLength()) {
+					if(gDiff.getLength() != s.getGeom().getLength()) {
 						//update geometry
-						si.remove(t.getGeom().getEnvelopeInternal(), t);
-						t.setGeom((LineString)gDiff);
-						si.insert(t.getGeom().getEnvelopeInternal(), t);
-						t.getProperties().put("EM", "changed");
+						si.remove(s.getGeom().getEnvelopeInternal(), s);
+						s.setGeom((LineString)gDiff);
+						si.insert(s.getGeom().getEnvelopeInternal(), s);
+						s.getProperties().put("EM", "changed");
 					}
 
 					//connect lines
 					//TODO when tg_ geom is changed, former connections should be preserved! !
-					LineString tg_ = connectLineStringsTip( (LineString)t_.getGeom(), (LineString)t.getGeom(), 1.5*res );
+					LineString tg_ = connectLineStringsTip( (LineString)s_.getGeom(), (LineString)s.getGeom(), 1.5*res );
 
 					//nothing to connect
 					if(tg_ == null) continue;
 
 					//update t_ geometry
-					si.remove(t_.getGeom().getEnvelopeInternal(), t_);
-					t_.setGeom((LineString)tg_);
-					si.insert(t_.getGeom().getEnvelopeInternal(), t_);
-					t_.getProperties().put("EM", "changed");
+					si.remove(s_.getGeom().getEnvelopeInternal(), s_);
+					s_.setGeom((LineString)tg_);
+					si.insert(s_.getGeom().getEnvelopeInternal(), s_);
+					s_.getProperties().put("EM", "changed");
 				}
 			}
 		}
 
-		System.out.println("Save output " + tracks.size());
-		SHPUtil.saveSHP(tracks, basePath+"out/EM/RailwayLinkEM.shp", SHPUtil.getCRS(basePath+"in/RailwayLink.shp"));
+		System.out.println("Save output " + secs.size());
+		SHPUtil.saveSHP(secs, basePath+"out/EM/RailwayLinkEM.shp", SHPUtil.getCRS(basePath+"in/RailwayLink.shp"));
 
 		System.out.println("End");
 	}
