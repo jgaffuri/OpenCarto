4af3ac09a4f5da87acbb22da3995e9d22f0e3c0a
jgaffuri <jgaf@protonmail.com>
2019-04-30 12:07:15 +0200

    a

--- src/main/java/org/opencarto/gisco/rail/MainCompareRailwayOSM.java
+++ src/main/java/org/opencarto/gisco/rail/MainCompareRailwayOSM.java
@@ -12,8 +12,7 @@ public class MainCompareRailwayOSM {
 	public MainCompareRailwayOSM() {
 
 		//load OSM
-		String fil = "RAILWAY <> 'construction' AND RAILWAY <> 'dismantled' AND RAILWAY <> 'elevator' AND RAILWAY <> 'funicular' AND RAILWAY <> 'historic' AND RAILWAY <> 'historic_path' AND RAILWAY <> 'historical' AND RAILWAY  <>  'miniature' AND RAILWAY <> 'planned' AND RAILWAY  <>  'platform' AND RAILWAY  <>  'platform_edge' AND RAILWAY  <>  'proposed' AND RAILWAY  <>  'razed' AND RAILWAY  <>  'turntable' AND RAILWAY  <>  'abandoned' AND RAILWAY  <>  'tram' AND RAILWAY  <>  'subway' AND SERVICE IS NULL";
-
+		//filter
 
 		//load railway
 		//compare them
