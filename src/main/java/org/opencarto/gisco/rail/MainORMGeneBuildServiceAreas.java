59233260933aaf5b1cc3310958bed86ba7e3c31f
jgaffuri <jgaf@protonmail.com>
2019-05-07 14:11:25 +0200

    a

--- src/main/java/org/opencarto/gisco/rail/MainORMGeneBuildServiceAreas.java
+++ src/main/java/org/opencarto/gisco/rail/MainORMGeneBuildServiceAreas.java
@@ -3,18 +3,6 @@
  */
 package org.opencarto.gisco.rail;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
-import org.apache.log4j.Logger;
-import org.locationtech.jts.geom.Geometry;
-import org.opencarto.algo.aggregation.BufferAggregation;
-import org.opencarto.datamodel.Feature;
-import org.opencarto.io.SHPUtil;
-import org.opencarto.util.FeatureUtil;
-import org.opencarto.util.JTSGeomUtil;
-import org.opencarto.util.ProjectionUtil;
-
 /**
  * 
  * Some tests on ORM generalisation
@@ -23,41 +11,11 @@ import org.opencarto.util.ProjectionUtil;
  *
  */
 public class MainORMGeneBuildServiceAreas {
-	public final static Logger LOGGER = Logger.getLogger(MainORMGeneBuildServiceAreas.class.getName());
 
 	public static void main(String[] args) {
 		System.out.println("Start");
-		int quad = 5;
 
-		LOGGER.info("Load input tracks");
-		String basePath = "/home/juju/Bureau/gisco_rail/";
-		ArrayList<Feature> tracks = SHPUtil.loadSHP(basePath+"orm/shp_SE/orm_tracks.shp").fs;
-		//ArrayList<Feature> tracks = SHPUtil.loadSHP(basePath+"orm/shp_SE/lines_LAEA.shp", epsg).fs;
-		System.out.println(tracks.size()+"   "+FeatureUtil.getVerticesNumber(tracks));
-
-		LOGGER.info("Glue buffer out");
-		double res = 10; //for 1:50k
-		Geometry glue = new BufferAggregation(res, -1, quad, -1, false).aggregateGeometries(FeatureUtil.getGeometries(tracks));
-		LOGGER.info("Glue buffer in");
-		glue = glue.buffer(-res);
-
-		LOGGER.info("Buffers for service areas");
-		double areaParamM = 7;
-		Geometry sArea = glue.buffer(-areaParamM,quad).buffer(areaParamM*1.001,quad);
-		LOGGER.info("Difference for double tracks");
-		Geometry doubleTrack = glue.difference(sArea);
-		glue = null;
-
-		LOGGER.info("Decomposition into polygons + filter by size");
-		double sizeSel = res*res*5;
-		Collection<?> areas = JTSGeomUtil.getPolygonGeometries(sArea, sizeSel); sArea = null;
-		Collection<?> doubleTracks = JTSGeomUtil.getPolygonGeometries(doubleTrack, sizeSel); doubleTrack = null;
-
-		LOGGER.info("   nbAreas="+areas.size() + "   nbDoubleTracks=" + doubleTracks.size());
-
-		LOGGER.info("Save");
-		SHPUtil.saveGeomsSHP((Collection<Geometry>) areas, basePath+"out/service_areas.shp", ProjectionUtil.getETRS89_LAEA_CRS());
-		SHPUtil.saveGeomsSHP((Collection<Geometry>) doubleTracks, basePath+"out/double_tracks.shp", ProjectionUtil.getETRS89_LAEA_CRS());
+		
 
 		System.out.println("End");
 	}
--- src/main/java/org/opencarto/gisco/rail/MainRailwayGeneralisation.java
+++ src/main/java/org/opencarto/gisco/rail/MainRailwayGeneralisation.java
@@ -4,29 +4,58 @@
 package org.opencarto.gisco.rail;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
+import org.apache.log4j.Logger;
+import org.locationtech.jts.geom.Geometry;
+import org.opencarto.algo.aggregation.BufferAggregation;
 import org.opencarto.datamodel.Feature;
 import org.opencarto.io.SHPUtil;
+import org.opencarto.util.FeatureUtil;
+import org.opencarto.util.JTSGeomUtil;
+import org.opencarto.util.ProjectionUtil;
 
 /**
  * @author julien Gaffuri
  *
  */
 public class MainRailwayGeneralisation {
+	public final static Logger LOGGER = Logger.getLogger(MainRailwayGeneralisation.class.getName());
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("Load input sections");
+		int quad = 5;
+		double res = 10; //for 1:50k
+		double areaBufferParamM = 7;
+
+		LOGGER.info("Load input sections");
 		String basePath = "/home/juju/Bureau/gisco_rail/";
-		ArrayList<Feature> secs = SHPUtil.loadSHP(basePath+"out/EM/RailwayLinkEM.shp").fs;
-		System.out.println(secs.size());
+		ArrayList<Feature> sections = SHPUtil.loadSHP(basePath+"out/EM/RailwayLinkEM.shp").fs;
+		System.out.println(sections.size()+"   "+FeatureUtil.getVerticesNumber(sections));
 
-		
-		
-		
-		
+		LOGGER.info("Glue buffer out");
+		Geometry glue = new BufferAggregation(res, -1, quad, -1, false).aggregateGeometries(FeatureUtil.getGeometries(sections));
+		LOGGER.info("Glue buffer in");
+		glue = glue.buffer(-res);
 
+		LOGGER.info("Buffers for service areas");
+		Geometry sArea = glue.buffer(-areaBufferParamM, quad).buffer(areaBufferParamM*1.001, quad);
+		LOGGER.info("Difference for double tracks");
+		Geometry doubleTrack = glue.difference(sArea);
+		glue = null;
+
+		LOGGER.info("Decomposition into polygons + filter by size");
+		double sizeSel = res*res*5;
+		Collection<?> areas = JTSGeomUtil.getPolygonGeometries(sArea, sizeSel); sArea = null;
+		Collection<?> doubleTracks = JTSGeomUtil.getPolygonGeometries(doubleTrack, sizeSel); doubleTrack = null;
+
+		LOGGER.info("   nbAreas = "+areas.size() + "   nbDoubleTracks = " + doubleTracks.size());
+
+		LOGGER.info("Save");
+		SHPUtil.saveGeomsSHP((Collection<Geometry>) areas, basePath+"out/service_areas.shp", ProjectionUtil.getETRS89_LAEA_CRS());
+		SHPUtil.saveGeomsSHP((Collection<Geometry>) doubleTracks, basePath+"out/double_tracks.shp", ProjectionUtil.getETRS89_LAEA_CRS());
 
 		System.out.println("End");
 	}
+
 
 }
