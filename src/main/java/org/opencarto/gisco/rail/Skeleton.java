2b6a5c678fc49823309c7f116b9a7da7032de463
jgaffuri <jgaf@protonmail.com>
2019-06-15 10:25:40 +0200

    a

---
+++ src/main/java/org/opencarto/gisco/rail/Skeleton.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.opencarto.gisco.rail;
+
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Polygon;
+import org.opencarto.datamodel.graph.Graph;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class Skeleton {
+
+
+	public static Graph get(Polygon p, Coordinate[] c) {
+		
+		
+		
+		return null;
+	}
+
+}
