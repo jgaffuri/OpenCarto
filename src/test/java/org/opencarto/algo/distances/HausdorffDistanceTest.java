2e3da31be970b3a15ac91d52172be8308ae15980
jgaffuri <julien.gaffuri@gmail.com>
2019-09-17 08:35:43 +0200

    remove dependency to jtsgeomutils - use wkt reader instead

--- src/test/java/org/opencarto/algo/distances/HausdorffDistanceTest.java
+++ src/test/java/org/opencarto/algo/distances/HausdorffDistanceTest.java
@@ -3,7 +3,7 @@ package org.opencarto.algo.distances;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.locationtech.jts.geom.Coordinate;
-import org.opencarto.util.JTSGeomUtil;
+import org.locationtech.jts.io.WKTReader;
 
 import junit.framework.TestCase;
 
@@ -13,20 +13,26 @@ import junit.framework.TestCase;
  */
 public class HausdorffDistanceTest extends TestCase {
 
+	private WKTReader wr = new WKTReader();
+
 	public HausdorffDistanceTest(String name) {
 		super(name);
 	}
 
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(HausdorffDistanceTest.class);
+	}
+
 	public void testNull() throws Exception {
 		Logger.getLogger(HausdorffDistance.class.getName()).setLevel(Level.OFF);
-		HausdorffDistance hd = new HausdorffDistance(null, JTSGeomUtil.createLineString(0,0, 100,0));
+		HausdorffDistance hd = new HausdorffDistance(null, wr.read("LINESTRING(0 0, 100 0)"));
 		assertNull(hd.getC0());
 		assertNull(hd.getC1());
 		assertTrue(Double.isNaN(hd.getDistance()));
 	}
 	public void testEmpty() throws Exception {
 		Logger.getLogger(HausdorffDistance.class.getName()).setLevel(Level.OFF);
-		HausdorffDistance hd = new HausdorffDistance(JTSGeomUtil.createLineString(), JTSGeomUtil.createLineString(0,0, 100,0));
+		HausdorffDistance hd = new HausdorffDistance(wr.read("LINESTRING EMPTY"), wr.read("LINESTRING(0 0, 100 0)"));
 		assertNull(hd.getC0());
 		assertNull(hd.getC1());
 		assertTrue(Double.isNaN(hd.getDistance()));
@@ -34,16 +40,16 @@ public class HausdorffDistanceTest extends TestCase {
 
 	public void test1() throws Exception {
 		HausdorffDistance hd = new HausdorffDistance(
-				JTSGeomUtil.createLineString(0,0, 100,0, 200,20),
-				JTSGeomUtil.createLineString(0,0, 100,0, 200,20)
+				wr.read("LINESTRING(0 0, 100 0, 200 20)"),
+				wr.read("LINESTRING(0 0, 100 0, 200 20)")
 				);
 		assertEquals(hd.getDistance(), 0.0);
 	}
 
 	public void test2() throws Exception {
 		HausdorffDistance hd = new HausdorffDistance(
-				JTSGeomUtil.createLineString(0,10, 100,10, 200,30),
-				JTSGeomUtil.createLineString(0,0, 100,0, 200,20)
+				wr.read("LINESTRING(0 10, 100 10, 200 30)"),
+				wr.read("LINESTRING(0 0, 100 0, 200 20)")
 				);
 		assertEquals(hd.getDistance(), 10.0);
 		assertEquals(hd.getC0().distance(new Coordinate(0, 10)), 0.0);
@@ -52,8 +58,8 @@ public class HausdorffDistanceTest extends TestCase {
 
 	public void test3() throws Exception {
 		HausdorffDistance hd = new HausdorffDistance(
-				JTSGeomUtil.createLineString(0,0, 100,0, 200,20),
-				JTSGeomUtil.createLineString(0,20, 100,20)
+				wr.read("LINESTRING(0 0, 100 0, 200 20)"),
+				wr.read("LINESTRING(0 20, 100 20)")
 				);
 		assertEquals(hd.getDistance(), 100.0);
 		assertEquals(hd.getC0().distance(new Coordinate(200, 20)), 0.0);
@@ -62,8 +68,8 @@ public class HausdorffDistanceTest extends TestCase {
 
 	public void test4() throws Exception {
 		HausdorffDistance hd = new HausdorffDistance(
-				JTSGeomUtil.createLineString(0,20, 100,20),
-				JTSGeomUtil.createLineString(0,0, 100,0, 200,20)
+				wr.read("LINESTRING(0 20, 100 20)"),
+				wr.read("LINESTRING(0 0, 100 0, 200 20)")
 				);
 		assertEquals(hd.getDistance(), 100.0);
 		assertEquals(hd.getC0().distance(new Coordinate(100, 20)), 0.0);
