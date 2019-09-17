af194c5e59f5ef2d10880b2f5302d2b3e5d5a590
jgaffuri <julien.gaffuri@gmail.com>
2019-09-17 15:02:34 +0200

    new tests

--- src/test/java/org/opencarto/algo/distances/HausdorffDistanceTest.java
+++ src/test/java/org/opencarto/algo/distances/HausdorffDistanceTest.java
@@ -15,16 +15,15 @@ import junit.framework.TestCase;
 public class HausdorffDistanceTest extends TestCase {
 	private final WKTReader wr = new WKTReader();
 
-	public HausdorffDistanceTest(String name) {
-		super(name);
-	}
+	public HausdorffDistanceTest(String name) { super(name); }
 
+	/*
 	public static void main(String[] args) {
 		junit.textui.TestRunner.run(HausdorffDistanceTest.class);
 	}
+	 */
 
 
-	
 	public void testNull() throws Exception {
 		Logger.getLogger(HausdorffDistance.class.getName()).setLevel(Level.OFF);
 		HausdorffDistance hd = new HausdorffDistance(null, wr.read("LINESTRING(0 0, 100 0)"));
@@ -59,13 +58,23 @@ public class HausdorffDistanceTest extends TestCase {
 	public void test2() throws Exception {
 		runTest(wr.read("LINESTRING(0 10, 100 10, 200 30)"), wr.read("LINESTRING(0 0, 100 0, 200 20)"), 10.0, new Coordinate(0, 10), new Coordinate(0, 0));
 	}
-
 	public void test3() throws Exception {
 		runTest(wr.read("LINESTRING(0 0, 100 0, 200 20)"), wr.read("LINESTRING(0 20, 100 20)"), 100.0, new Coordinate(200, 20), new Coordinate(100, 20));
 	}
-
 	public void test4() throws Exception {
 		runTest(wr.read("LINESTRING(0 20, 100 20)"), wr.read("LINESTRING(0 0, 100 0, 200 20)"), 100.0, new Coordinate(100, 20), new Coordinate(200, 20));
+	}
+	public void test5() throws Exception {
+		runTest(wr.read("LINESTRING (0 0, 2 1)"), wr.read("LINESTRING (0 0, 2 0)"), 1.0, new Coordinate(2, 1), new Coordinate(2, 0));
+	}
+	public void test6() throws Exception {
+		runTest(wr.read("LINESTRING (0 0, 2 0)"), wr.read("LINESTRING (0 1, 1 2, 2 1)"), 2.0, new Coordinate(1, 0), new Coordinate(1, 2));
+	}
+	public void test7() throws Exception {
+		runTest(wr.read("LINESTRING (0 0, 2 0)"), wr.read("MULTIPOINT (0 1, 1 0, 2 1)"), 1.0, new Coordinate(0, 0), new Coordinate(0, 1));
+	}
+	public void test8() throws Exception {
+		runTest(wr.read("LINESTRING (130 0, 0 0, 0 150)"), wr.read("LINESTRING (10 10, 10 150, 130 10)"), 14.142135623730951, new Coordinate(0, 0), new Coordinate(10, 10));
 	}
 
 }
