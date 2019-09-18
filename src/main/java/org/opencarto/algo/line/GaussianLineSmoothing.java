717a91600f3995a0f3d1967bca624fe079640036
jgaffuri <julien.gaffuri@gmail.com>
2019-09-18 16:09:37 +0200

    documentation

--- src/main/java/org/opencarto/algo/line/GaussianLineSmoothing.java
+++ src/main/java/org/opencarto/algo/line/GaussianLineSmoothing.java
@@ -5,7 +5,7 @@ import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.LineString;
 
 /**
- * Apply a gaussian smoothing to a line.
+ * Line gaussian smoothing.
  * 
  * @author julien Gaffuri
  *
@@ -13,12 +13,28 @@ import org.locationtech.jts.geom.LineString;
 public class GaussianLineSmoothing {
 	public static final Logger LOGGER = Logger.getLogger(GaussianLineSmoothing.class.getName());
 
-	public static LineString get(LineString ls, double sigmaM){ return get(ls, sigmaM, -1); }
-	public static LineString get(LineString ls, double sigmaM, double resolution){
-		if(ls.getCoordinates().length <= 2) return (LineString) ls.copy();
+	/**
+	 * @param line
+	 * @param sigmaM
+	 * @return
+	 */
+	public static LineString get(LineString line, double sigmaM){ return get(line, sigmaM, -1); }
 
-		boolean isClosed = ls.isClosed();
-		double length = ls.getLength();
+	/**
+	 * Line gaussian smoothing.
+	 * The position of each point is the average position of its neighbors, weighted by a gaussian kernel.
+	 * For non-closed lines, the initial and final points are preserved.
+	 * 
+	 * @param line The input line
+	 * @param sigmaM The standard deviation of the gaussian kernel. The larger, the more smoothed.
+	 * @param resolution The target resolution of the geometry. This parameter is used to filter the final geometry.
+	 * @return
+	 */
+	public static LineString get(LineString line, double sigmaM, double resolution){
+		if(line.getCoordinates().length <= 2) return (LineString) line.copy();
+
+		boolean isClosed = line.isClosed();
+		double length = line.getLength();
 		double densifiedResolution = sigmaM/3;
 
 		//handle extreme cases
@@ -27,22 +43,22 @@ public class GaussianLineSmoothing {
 			if(isClosed){
 				//return tiny triangle
 				//TODO do better? return center point?
-				return ls.getFactory().createLineString(new Coordinate[]{ ls.getCoordinateN(0), ls.getCoordinateN(1), ls.getCoordinateN(ls.getNumPoints()-2), ls.getCoordinateN(ls.getNumPoints()-1) });
+				return line.getFactory().createLineString(new Coordinate[]{ line.getCoordinateN(0), line.getCoordinateN(1), line.getCoordinateN(line.getNumPoints()-2), line.getCoordinateN(line.getNumPoints()-1) });
 			} else {
 				//return segment
-				return ls.getFactory().createLineString(new Coordinate[]{ ls.getCoordinateN(0), ls.getCoordinateN(ls.getNumPoints()-1) });
+				return line.getFactory().createLineString(new Coordinate[]{ line.getCoordinateN(0), line.getCoordinateN(line.getNumPoints()-1) });
 			}
 		}
 
 		//compute densified line
-		Coordinate[] densifiedCoords = LittleThumblingDensifier.densify(ls, densifiedResolution).getCoordinates();
+		Coordinate[] densifiedCoords = LittleThumblingDensifier.densify(line, densifiedResolution).getCoordinates();
 
 		//build ouput line structure
 		int nb = (int) (length/densifiedResolution);
 		Coordinate[] out = new Coordinate[nb+1];
 
 		//prepare gaussian coefficients
-		int n = 7*3; //it should be E(7*sigma/densifiedResolution) which is 7*3;
+		int n = 7*3; //it should be: E(7*sigma/densifiedResolution), which is 7*3;
 		double gcs[] = new double[n+1];
 		{
 			double a = sigmaM*Math.sqrt(2*Math.PI);
@@ -65,6 +81,7 @@ public class GaussianLineSmoothing {
 				double xq, yq;
 				if(q<0) {
 					if(isClosed) {
+						//make loop to get the right point
 						q = q%nb; if(q<0) q+=nb;
 						Coordinate c = densifiedCoords[q];
 						xq = c.x;
@@ -78,6 +95,7 @@ public class GaussianLineSmoothing {
 					}
 				} else if (q>nb) {
 					if(isClosed) {
+						//make loop to get the right point
 						q = q%nb; if(q==0) q=nb;
 						Coordinate c = densifiedCoords[q];
 						xq = c.x;
@@ -101,18 +119,22 @@ public class GaussianLineSmoothing {
 				x += xq*gc;
 				y += yq*gc;
 			}
+			//assign smoothed position of point i
 			out[i] = new Coordinate(x*densifiedResolution, y*densifiedResolution);
 		}
 
 		//handle start and end points
 		if(isClosed) {
+			//ensure start and end locations are the same
 			out[nb] = out[0];
 		} else {
+			//ensure start and end points are at the same position as the initial geometry
 			out[0] = densifiedCoords[0];
 			out[nb] = densifiedCoords[densifiedCoords.length-1];
 		}
 
-		LineString lsOut = ls.getFactory().createLineString(out);
+		//prepare final line, applying some filtering
+		LineString lsOut = line.getFactory().createLineString(out);
 		if(resolution<0) resolution = densifiedResolution /3;
 		lsOut = (LineString) DouglasPeuckerRamerFilter.get( lsOut , resolution);
 
